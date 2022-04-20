ef42959ab504087c5bc9b2a9db1de9a8d65e21ff
Tim Cosgrove <tim.cosgrove@agile6.com>
2022-04-20 15:56:03 -0700

    Revert to 4 spaces.

--- src/components/node/landing_page/index.tsx
+++ src/components/node/landing_page/index.tsx
@@ -2,20 +2,23 @@ import Link from 'next/link'
 
 // Output a landing_page node as a link to itself, plus its teaser text.
 const BenefitsHubLink = ({ node }) => {
-  if (node) {
-    return (
-      <>
-        <p className="vads-u-margin--0">
-          <strong>
-            <Link href={node.path?.alias ? node.path?.alias : ' '} passHref>
-              <a>{node.field_home_page_hub_label}</a>
-            </Link>
-          </strong>
-        </p>
-        <p className="vads-u-margin--0">{node.field_teaser_text}</p>
-      </>
-    )
-  }
+    if (node) {
+        return (
+            <>
+                <p className="vads-u-margin--0">
+                    <strong>
+                        <Link
+                            href={node.path?.alias ? node.path?.alias : ' '}
+                            passHref
+                        >
+                            <a>{node.field_home_page_hub_label}</a>
+                        </Link>
+                    </strong>
+                </p>
+                <p className="vads-u-margin--0">{node.field_teaser_text}</p>
+            </>
+        )
+    }
 }
 
 // This should not be the default; we will have a full display eventually.
--- src/pages/demo/partials/benefitHubsLinks.tsx
+++ src/pages/demo/partials/benefitHubsLinks.tsx
@@ -3,40 +3,40 @@ import BenefitsHubLinks from '@/components/partials/benefitHubsLinks'
 import { DrupalJsonApiParams } from 'drupal-jsonapi-params'
 
 const DemoPage = ({ nodes }) => {
-  return nodes.map((node) => (
-    <>
-      (on <strong>{node.title}</strong>)
-      <BenefitsHubLinks
-        key={node.uuid}
-        nodes={node.field_related_benefit_hubs}
-      />
-      <hr />
-    </>
-  ))
+    return nodes.map((node) => (
+        <>
+            (on <strong>{node.title}</strong>)
+            <BenefitsHubLinks
+                key={node.uuid}
+                nodes={node.field_related_benefit_hubs}
+            />
+            <hr />
+        </>
+    ))
 }
 export default DemoPage
 
 export async function getStaticProps(context) {
-  const params = new DrupalJsonApiParams()
-  params
-    .addFilter('status', '1')
-    .addFilter('field_related_benefit_hubs', null, 'IS NOT NULL')
-    .addInclude(['field_related_benefit_hubs'])
+    const params = new DrupalJsonApiParams()
+    params
+        .addFilter('status', '1')
+        .addFilter('field_related_benefit_hubs', null, 'IS NOT NULL')
+        .addInclude(['field_related_benefit_hubs'])
 
-  const nodes = await drupalClient.getResourceCollectionFromContext(
-    'node--support_resources_detail_page',
-    context,
-    {
-      params: {
-        include: 'field_related_benefit_hubs',
-        sort: '-created',
-        'filter[status][value]': '1',
-      },
+    const nodes = await drupalClient.getResourceCollectionFromContext(
+        'node--support_resources_detail_page',
+        context,
+        {
+            params: {
+                include: 'field_related_benefit_hubs',
+                sort: '-created',
+                'filter[status][value]': '1',
+            },
+        }
+    )
+    return {
+        props: {
+            nodes: nodes || null,
+        },
     }
-  )
-  return {
-    props: {
-      nodes: nodes || null,
-    },
-  }
 }
