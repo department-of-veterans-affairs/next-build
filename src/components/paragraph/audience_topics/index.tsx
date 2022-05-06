038fbcfda047bb387c13c6209acfb51bab74903d
Aaron <aaron.m.ponce@gmail.com>
2022-05-05 17:52:29 -0700

    VACMS-8609-paragraph-audience-topics

---
+++ src/components/paragraph/audience_topics/index.tsx
@@ -0,0 +1,40 @@
+import { v4 as uuidv4 } from 'uuid'
+import Link from 'next/link'
+import { getTagsList } from '@/utils/utils'
+
+const AudienceTopics = ({ paragraph }) => {
+  if (!paragraph) paragraph = []
+
+  const tags = getTagsList(paragraph)
+
+  return (
+    <>
+      {tags.length > 0 ? (
+        <div className="vads-u-padding-x--1 large-screen:vads-u-padding-x--0">
+          <div className="vads-u-padding-top--3 vads-u-padding-bottom--1p5 vads-u-border-top--1px vads-u-border-bottom--1px vads-u-border-color--gray-light vads-u-display--flex vads-u-align-items--flex-start vads-u-flex-direction--row">
+            <strong className="vads-u-margin-bottom--2 medium-screen:vads-u-margin-bottom--0">
+              Tags:
+            </strong>
+            <div className="vads-u-display--flex vads-u-flex-wrap--wrap">
+              {tags.map((tag) => (
+                <div key={uuidv4()}>
+                  {tag ? (
+                    <div className="vads-u-margin-right--1 vads-u-margin-bottom--1 medium-screen:vads-u-margin-bottom--0">
+                      <Link href={`/${tag.path.alias}/${tag.name}`}>
+                        <a className="vads-u-margin-bottom--1p5 usa-button-secondary vads-u-font-size--sm vads-u-border--1px vads-u-border-color--primary vads-u-padding--0p25 vads-u-padding-x--0p5 vads-u-margin-left--1p5 vads-u-text-decoration--none vads-u-color--base">
+                          {tag.name}
+                        </a>
+                      </Link>
+                    </div>
+                  ) : null}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
+  )
+}
+
+export default AudienceTopics
