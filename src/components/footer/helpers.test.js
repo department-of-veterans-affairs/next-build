24493bcf27d73edd72bfae65ed9b1029c187c632
Nathan Douglas <github@darkdell.net>
2022-05-13 10:41:22 -0400

    Moar tests.

--- src/components/footer/helpers.test.js
+++ src/components/footer/helpers.test.js
@@ -1,68 +1,95 @@
 import { faker, render, screen, toHaveAttribute } from 'test-utils'
-import { renderColumns } from './helpers'
+import { createLinkGroups, generateLinkItems, renderColumns } from './helpers'
 
-test('renderColumns() renders an empty div from an empty list of columns', async () => {
-  const columns = []
-  render(renderColumns(columns))
-  expect(document.querySelector('div')).toBeEmptyDOMElement()
-})
-
-test('renderColumns() renders a single column from a list of a single column', async () => {
-  const links = Array(5)
+const getLinks = (count = 5) =>
+  Array(count)
     .fill()
     .map(() => ({
       title: faker.lorem.sentence(3),
       href: faker.internet.url(),
     }))
-  const columns = [{ links }]
-  render(renderColumns(columns))
+
+const getColumns = (count = 5, numLinks = 5) =>
+  Array(count)
+    .fill()
+    .map((_, index) => ({
+      columns: index,
+      links: getLinks(numLinks),
+    }))
+
+test('renderColumns() renders an empty div from an empty list of columns', () => {
+  render(renderColumns(getColumns(0)))
+  expect(document.querySelector('div')).toBeEmptyDOMElement()
+})
+
+test('renderColumns() renders a single column from a list of a single column', () => {
+  render(renderColumns(getColumns(1, 5)))
   expect(screen.getByRole('list')).toHaveAttribute('class', 'va-footer-links')
 })
 
-/**
-export const renderColumns = (columnLinks) => {
-    const render = columnLinks.map((column) => {
-      return (
-        <>
-          <ul className="va-footer-links">
-            {column.links.map((link) => {
-              return (
-                <li key={link.title}>
-                  <span className="va-footer-link-text">
-                    <a href={link.href}>{link.title}</a>
-                  </span>
-                </li>
-              )
-            })}
-          </ul>
-        </>
-      )
-    })
-    return render
-  }
-  
-  export const generateLinkItems = (allColumns) => {
-    const linkColumns = Object.keys(allColumns).map((key) => {
-      return {
-        columns: key,
-        links: allColumns[key],
-      }
-    })
-    return renderColumns(linkColumns)
-  }
-  
-  export const createLinkGroups = (links) => {
-    const allGroups = links.reduce((acc, link) => {
-      if (!acc[link.column]) {
-        acc[link.column] = []
-      }
-      acc[link.column].push({
-        href: link.href,
-        title: link.title,
+test('renderColumns() renders three columns from a list of three columns', () => {
+  render(renderColumns(getColumns(3, 5)))
+  const renderedColumns = screen.getAllByRole('list')
+  expect(renderedColumns).toHaveLength(3)
+  renderedColumns.forEach((renderedColumn) => {
+    expect(renderedColumn).toHaveAttribute('class', 'va-footer-links')
+    expect(renderedColumn.children).toHaveLength(5)
+  })
+})
+
+test('generateLinkItems() renders columns from a list', () => {
+  const allColumns = {}
+  const columns = getColumns(1, 3).forEach((column) => {
+    allColumns[faker.lorem.words(1)] = column.links
+  })
+  render(generateLinkItems(allColumns))
+  const renderedColumns = screen.getAllByRole('list')
+  expect(renderedColumns).toHaveLength(1)
+  renderedColumns.forEach((renderedColumn) => {
+    expect(renderedColumn).toHaveAttribute('class', 'va-footer-links')
+    expect(renderedColumn.children).toHaveLength(3)
+  })
+})
+
+test('generateLinkItems() renders columns from a list with multiple columns', () => {
+  const allColumns = {}
+  Array(3)
+    .fill()
+    .forEach(() => {
+      const key = faker.lorem.words(1)
+      allColumns[key] = []
+      getColumns(1, 3).forEach((column) => {
+        allColumns[key] = column.links
       })
-      return acc
-    }, {})
-    return generateLinkItems(allGroups)
-  }
-  
-  */
+    })
+  render(generateLinkItems(allColumns))
+  const renderedColumns = screen.getAllByRole('list')
+  expect(renderedColumns).toHaveLength(3)
+  renderedColumns.forEach((renderedColumn) => {
+    expect(renderedColumn).toHaveAttribute('class', 'va-footer-links')
+    expect(renderedColumn.children).toHaveLength(3)
+  })
+})
+
+test('createLinkGroups() renders columns from a list of links', () => {
+  const columnNames = Array(3)
+    .fill()
+    .map(() => faker.lorem.words(1))
+  const links = Array(20)
+    .fill()
+    .map(() => {
+      const columnName = faker.helpers.arrayElement(columnNames)
+      const link = {}
+      link.column = columnName
+      link.href = faker.internet.url()
+      link.title = faker.lorem.sentence(5)
+      return link
+    })
+  render(createLinkGroups(links))
+  const renderedColumns = screen.getAllByRole('list')
+  expect(renderedColumns).toHaveLength(3)
+  renderedColumns.forEach((renderedColumn) => {
+    expect(renderedColumn).toHaveAttribute('class', 'va-footer-links')
+    expect(renderedColumn.children.length).toBeGreaterThanOrEqual(1)
+  })
+})
