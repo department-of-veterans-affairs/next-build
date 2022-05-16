6375b9996bf489cd7b8fa53ac7b5bcf3a43b7594
Penny Lischer <penny.lischer@agile6.com>
2022-05-16 13:52:14 -0700

    VACMS-8614 Component: paragraph--email_contact

---
+++ src/components/paragraph/email_contact/index.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import { recordEvent } from '@/utils/recordEvent'
+
+function isRequestValid(paragraph: any) {
+  return (
+    paragraph.field_email_label !== null &&
+    paragraph.field_email_address !== null
+  )
+}
+
+const EmailContact = ({ paragraph }): JSX.Element => {
+  if (!paragraph || !isRequestValid(paragraph)) return
+
+  const EMAIL_CONTACT_EVENT = {
+    event: 'nav-linkslist',
+    'links-list-header': `${encodeURIComponent(paragraph.field_email_address)}`,
+    'links-list-section-header': 'Need more help?',
+  }
+
+  return (
+    <div key={paragraph.id}>
+      <strong>{paragraph.field_email_label}: </strong>
+      <Link href={'mailto:' + paragraph.field_email_address} passHref>
+        <a
+          onClick={() => recordEvent(EMAIL_CONTACT_EVENT)}
+          rel="noreferrer noopener"
+        >
+          {paragraph.field_email_address}
+        </a>
+      </Link>
+    </div>
+  )
+}
+
+export default EmailContact
