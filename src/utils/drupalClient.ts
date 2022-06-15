9f015437dcaf067e60898d10f5560d48037a002f
Aaron <aaron.m.ponce@gmail.com>
2022-06-14 17:21:32 -0700

    Remove Experimental from Drupal client

--- src/utils/drupalClient.ts
+++ src/utils/drupalClient.ts
@@ -1,4 +1,4 @@
-import { Experiment_DrupalClient as DrupalClient } from 'next-drupal'
+import { DrupalClient } from 'next-drupal'
 import crossFetch from 'cross-fetch'
 import { SocksProxyAgent } from 'socks-proxy-agent'
 import { addCAs } from 'syswide-cas'
@@ -8,7 +8,7 @@ export const useProxy = baseUrl.includes('cms.va.gov')
 if (useProxy) {
   addCAs('certs/VA-Internal-S2-RCA1-v1.pem')
 }
-const fetcher = (input: RequestInfo, init?: RequestInit) => {
+export const fetcher = (input: RequestInfo, init?: RequestInit) => {
   const agent = new SocksProxyAgent('socks://127.0.0.1:2001')
   const options = {
     agent: useProxy ? agent : null,
