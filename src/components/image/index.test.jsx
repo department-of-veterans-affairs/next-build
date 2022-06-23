e08e445ad9f811ea08e5e86b1e777c081486fff6
Nathan Douglas <github@darkdell.net>
2022-06-23 13:37:15 -0400

    Fix my crap formatting.

--- src/components/image/index.test.jsx
+++ src/components/image/index.test.jsx
@@ -2,7 +2,8 @@ import { axe, faker, render, screen, waitFor } from 'test-utils'
 import Image from '.'
 import mediaImageIndividual from './mediaImageIndividual.json'
 
-const getUrl = (data) => data.included.filter((obj) => obj.type == 'file--file')[0].attributes.uri.url
+const getUrl = (data) =>
+  data.included.filter((obj) => obj.type == 'file--file')[0].attributes.uri.url
 
 const getAltText = (data) => data.data.relationships.image.data.meta.alt
 const getWidth = (data) => data.data.relationships.image.data.meta.width
@@ -13,7 +14,9 @@ describe('Image Component', () => {
     const originalError = console.error
     console.error = jest.fn()
     // eslint-disable-next-line
-    expect(() => render(<Image />)).toThrow(/image is missing required "src" property/i)
+    expect(() => render(<Image />)).toThrow(
+      /image is missing required "src" property/i
+    )
     console.error = originalError
   })
   test('throws an error if no height/width properties are passed in', async () => {
@@ -21,13 +24,15 @@ describe('Image Component', () => {
     console.error = jest.fn()
     let url = getUrl(mediaImageIndividual)
     // eslint-disable-next-line
-    expect(() => render(<Image src={url} />)).toThrow(/image with src "([^"]*)" must use "width" and "height" properties/i)
+    expect(() => render(<Image src={url} />)).toThrow(
+      /image with src "([^"]*)" must use "width" and "height" properties/i
+    )
     console.error = originalError
   })
   test('triggers an accessibility error if no alt text is passed in', async () => {
     let url = getUrl(mediaImageIndividual)
     // eslint-disable-next-line
-    const { container } = render(<Image src={url} layout={"fill"} />)
+    const { container } = render(<Image src={url} layout={'fill'} />)
     await waitFor(async () =>
       expect(await axe(container)).toEqual(
         expect.objectContaining({
@@ -54,13 +59,16 @@ describe('Image Component', () => {
     // eslint-disable-next-line
     const { container } = render(<Image {...props} />)
     await waitFor(async () => expect(await axe(container)).toHaveNoViolations())
-    let imgElement;
+    let imgElement
 
     // Loading element
     imgElement = document.querySelector('img[alt=""]')
     expect(imgElement).toHaveAttribute('alt', '')
     expect(imgElement).toHaveAttribute('aria-hidden', 'true')
-    expect(imgElement).toHaveAttribute('src', expect.stringContaining('data:image/svg+xml'))
+    expect(imgElement).toHaveAttribute(
+      'src',
+      expect.stringContaining('data:image/svg+xml')
+    )
     expect(imgElement).toHaveAttribute('style', expect.any(String))
 
     // Actual element
