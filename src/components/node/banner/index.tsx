1fb240d037c694970db5f15d0809b72551072211
Penny Lischer <penny.lischer@agile6.com>
2022-06-27 14:13:43 -0700

    VACMS-8654 Removed reference to 'web-components/react-bindings' and replaced with 'component-library/dist/react-bindings'

--- src/components/node/banner/index.tsx
+++ src/components/node/banner/index.tsx
@@ -1,17 +1,16 @@
-import { VaBanner } from '@department-of-veterans-affairs/web-components/react-bindings'
+import { VaBanner } from '@department-of-veterans-affairs/component-library/dist/react-bindings'
 import { isEmpty } from 'lodash'
 
 const BannerComponent = ({ node }): JSX.Element => {
   if (isEmpty(node)) return
 
-  console.log('node: ', node.title)
   return (
     <VaBanner
       role="va-banner"
-      headline={node.title}
       showClose={node.field_dismissible_option != 'perm'}
+      headline={node.title}
       type={node.field_alert_type}
-      visible="true"
+      visible={true}
       windowSession={node.field_dismissible_option == 'dismiss-session'}
     >
       <div dangerouslySetInnerHTML={{ __html: node.body.processed }} />
--- src/components/node/promo_banner/index.tsx
+++ src/components/node/promo_banner/index.tsx
@@ -1,4 +1,4 @@
-import { VaPromoBanner } from '@department-of-veterans-affairs/web-components/react-bindings'
+import { VaPromoBanner } from '@department-of-veterans-affairs/component-library/dist/react-bindings'
 import { isEmpty } from 'lodash'
 
 const PromoBannerComponent = ({ node }): JSX.Element => {
@@ -7,7 +7,6 @@ const PromoBannerComponent = ({ node }): JSX.Element => {
   return (
     <VaPromoBanner
       role="va-promoBanner"
-      text={node.title}
       href={node.field_link.uri}
       type={node.field_promo_type}
     >
