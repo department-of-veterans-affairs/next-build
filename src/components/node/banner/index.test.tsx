77734088d45c94a87be312327092f252acc2bff2
Penny Lischer <penny.lischer@agile6.com>
2022-06-28 11:32:32 -0700

    VACMS-8654 Updates to Banner tests. Added id to Banner components to allow for close to function properly. Added defineCustomElements in _app to allow for external styles to be loaded for web components.

--- src/components/node/banner/index.test.tsx
+++ src/components/node/banner/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from 'test-utils'
 import Banner from '@/components/node/banner'
 import mock_banner from './nodeBanner.json'
 
