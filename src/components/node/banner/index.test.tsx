4ec9b2de5d806443b1e924144b148d8e2013618a
Penny Lischer <penny.lischer@agile6.com>
2022-07-26 15:54:15 -0700

    VACMS-9911 Update the layout unit tests to get to 100% coverage

--- src/components/node/banner/index.test.tsx
+++ src/components/node/banner/index.test.tsx
@@ -5,7 +5,7 @@ import mock_banner from './nodeBanner.json'
 describe('<Banner> component renders', () => {
   test('with valid data', () => {
     render(<Banner node={mock_banner} />)
-    expect(screen.queryByText(/This is the body/)).toBeInTheDocument()
+    expect(screen.queryByText(/This is the banner body/)).toBeInTheDocument()
     expect(screen.getByRole('va-banner')).toHaveAttribute(
       'headline',
       'COVID-19 vaccines at VA'
@@ -16,6 +16,6 @@ describe('<Banner> component renders', () => {
 describe('<Banner> component does not render', () => {
   test('without node data', () => {
     render(<Banner node={[]} />)
-    expect(screen.queryByText(/This is the body/)).not.toBeInTheDocument()
+    expect(screen.queryByText(/This is the banner body/)).not.toBeInTheDocument()
   })
 })
