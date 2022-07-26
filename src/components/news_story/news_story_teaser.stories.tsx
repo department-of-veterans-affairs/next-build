ce1c4e0fa6ce988cf06aa4292fbbd16a060c0f8f
Tanner Heffner <tanner.heffner@agile6.com>
2022-07-26 15:15:03 -0700

    VACMS-9896 ts fixes for storybook, correct media data structure

--- src/components/news_story/news_story_teaser.stories.tsx
+++ src/components/news_story/news_story_teaser.stories.tsx
@@ -2,6 +2,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react'
 import React from 'react'
 
 import { NewsStoryTeaser } from '@/components/news_story'
+import mediaImage from '@/components/media/media_example.json'
 
 export default {
   title: 'News Story/Teaser',
@@ -19,6 +20,5 @@ Teaser.args = {
   introText:
     'When a hospital has a host of great doctors, honoring just two every year is challenging.',
   link: '#',
-  image:
-    'http://content-build-medc0xjkxm4jmpzxl3tfbcs7qcddsivh.ci.cms.va.gov/sites/default/files/styles/2_1_large/public/2019-05/doctor-year2019-decker-480_0.jpg',
+  image: mediaImage,
 }
