fe281582c48eecaaf1d92c560f40995a44bc3bfd
Tanner Heffner <tanner.heffner@agile6.com>
2022-07-26 15:15:03 -0700

    VACMS-9896 ts fixes for storybook, correct media data structure

--- src/components/news_story/news_story_full.stories.tsx
+++ src/components/news_story/news_story_full.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import { NewsStoryFull } from './index'
+import mediaImage from '@/components/media/media_example.json'
 
 export default {
   title: 'News Story/Full',
@@ -15,11 +16,14 @@ const Template: ComponentStory<typeof NewsStoryFull> = (args) => (
 export const Full = Template.bind({})
 Full.args = {
   title: 'We honor outstanding doctors\n',
-  image: 'http://content-build-medc0xjkxm4jmpzxl3tfbcs7qcddsivh.ci.cms.va.gov/sites/default/files/styles/2_1_medium_thumbnail/public/2019-05/doctor-year2019-decker-480_0.jpg',
-  caption: '"Caring for a single patient and solving that one patient\'s illness is our honor and privilege as health care providers." - Dr. Brooke Decker',
+  image: mediaImage,
+  caption:
+    '"Caring for a single patient and solving that one patient\'s illness is our honor and privilege as health care providers." - Dr. Brooke Decker',
   author: 'Keith Gottschalk',
-  introText: 'When a hospital has a host of great doctors, honoring just two every year is challenging.',
-  bodyContent: 'VA Pittsburgh\'s Outstanding Physicians of the Year for 2019, Drs. Brooke Decker and Aref M. Rahman, stand out for expertly filling critical medical facility roles while fostering a collaborative spirit among staff in caring for Veterans.\n' +
+  introText:
+    'When a hospital has a host of great doctors, honoring just two every year is challenging.',
+  bodyContent:
+    "VA Pittsburgh's Outstanding Physicians of the Year for 2019, Drs. Brooke Decker and Aref M. Rahman, stand out for expertly filling critical medical facility roles while fostering a collaborative spirit among staff in caring for Veterans.\n" +
     '\n' +
     'Dr. Decker, director of Infection Prevention, sees herself as a problem solver.\n' +
     '\n' +
@@ -27,7 +31,7 @@ Full.args = {
     '\n' +
     'Recent examples include ensuring hand sanitizers are plentiful and optimally located to support hand hygiene; safe disposal of needles and syringes; and developing the safest way to shut down and perform maintenance on an air handler in a patient-care area that is always open.\n' +
     '\n' +
-    'Dr. Rahman, a cardiologist, has been director of the Cardiac Cath Lab for the past six years. He credits his dedicated and skilled team for the lab\'s success, but he says one other factor plays a big role in that success.\n' +
+    "Dr. Rahman, a cardiologist, has been director of the Cardiac Cath Lab for the past six years. He credits his dedicated and skilled team for the lab's success, but he says one other factor plays a big role in that success.\n" +
     '\n' +
     '"The icing on top is the staff\'s eagerness to help our Veterans," said Rahman. "All hospital environments can be intense and demanding and come with general and specific challenges. But having co-workers whom one can count on makes my job at VA an invaluable opportunity."\n' +
     '\n' +
@@ -39,19 +43,19 @@ Full.args = {
     '\n' +
     '"Caring for a single patient and solving that one patient\'s illness is our honor and privilege as health care providers," said Decker. "Preventing 10 patients from getting sick is even better, and that\'s what we strive to do in Infection Prevention."\n' +
     '\n' +
-    'Rahman said he\'s been fascinated since childhood with how the human body works.\n' +
+    "Rahman said he's been fascinated since childhood with how the human body works.\n" +
     '\n' +
     '"I would read all kinds of articles about new discoveries in medicine and I found myself being drawn more and more toward what is literally the \'heart\' of our bodies," he said. "Cardiology is not just a career choice, but a calling, and I think it found me."\n' +
     '\n' +
-    'Both doctors said the best part of working at VA is listening to Veterans\' stories.\n' +
+    "Both doctors said the best part of working at VA is listening to Veterans' stories.\n" +
     '\n' +
     '"I love sitting down at the bedside and hearing about someone\'s family, pets, adventures," said Decker. "I\'ve met some truly great people working at VA."\n' +
     '\n' +
-    'Rahman said Veterans\' stories make every day at work interesting.\n' +
+    "Rahman said Veterans' stories make every day at work interesting.\n" +
     '\n' +
     '"As I interact with my patients, I get to hear a lot of fascinating stories of their experiences all over the world," said Rahman. "It\'s never a dull day at work."\n' +
     '\n' +
-    'Decker and Rahman were formally recognized as Outstanding Physicians of the Year on April 26 during the medical staff\'s quarterly meeting at University Drive.',
+    "Decker and Rahman were formally recognized as Outstanding Physicians of the Year on April 26 during the medical staff's quarterly meeting at University Drive.",
   date: 'May 14, 2019',
   socialLinks: '',
   listing: '/pittsburgh-health-care/stories',
