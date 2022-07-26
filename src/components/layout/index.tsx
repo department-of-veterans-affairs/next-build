d2cee9c63cb6cc067de09d2cd157d4d16aaa83a9
Penny Lischer <penny.lischer@agile6.com>
2022-07-26 15:54:15 -0700

    VACMS-9911 Update the layout unit tests to get to 100% coverage

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -22,9 +22,10 @@ export default function Layout({ children, props }: LayoutProps) {
 
       <main>{children}</main>
 
-      {props?.footerData && !isEmpty(props?.footerData) && (
-        <Footer links={props?.footerData} />
-      )}
+
+      {props?.footerData && !isEmpty(props?.footerData) &&
+          <Footer links={props?.footerData} />
+      }
     </>
   )
 }
