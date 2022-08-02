296f472103ec33a883d262a26419121852f774dc
Tanner Heffner <tanner.heffner@agile6.com>
2022-08-02 13:02:42 -0700

    VACMS-9990 update image url for storybook/tests

--- src/components/staffProfile/staffProfile.test.tsx
+++ src/components/staffProfile/staffProfile.test.tsx
@@ -5,7 +5,7 @@ const props = {
   id: '7783e76f-5aca-4d14-9f5e-fb00cc11e4da',
   name: 'Mr William Smathers',
   thumbnail: {
-    url: 'foo',
+    url: 'https://content-build-medc0xjkxm4jmpzxl3tfbcs7qcddsivh.ci.cms.va.gov/sites/default/files/2019-08/William_W_Smathers.jpg',
     alt: 'William W Smathers Headshot',
     title: 'William W Smathers',
     width: 110,
