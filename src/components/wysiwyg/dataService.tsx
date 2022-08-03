6ad592e0d18e3b715147eb2f5365c7eeb50ecb28
Aaron <aaron.m.ponce@gmail.com>
2022-08-03 14:21:44 -0700

    remove viewmode from wysiwig

--- src/components/wysiwyg/dataService.tsx
+++ src/components/wysiwyg/dataService.tsx
@@ -3,22 +3,15 @@ import { ParagraphResourceType, ParagraphWysiwyg } from '@/types/paragraph'
 import { drupalToVaPath, phoneLinks } from '@/utils/helpers'
 import Wysiwyg from '@/components/wysiwyg'
 
-export const wysiwygDataService = (
-  entity: ParagraphWysiwyg,
-  viewMode?: string
-) => {
+export const wysiwygDataService = (entity: ParagraphWysiwyg) => {
   const data = [entity.field_wysiwyg?.processed]
   const filters = [phoneLinks, drupalToVaPath]
   const filteredData = filters.reduce((d, f) => d.filter(f), data)
-  switch (viewMode) {
-    default:
-      return {
-        id: entity.id,
-        html: filteredData || filteredData[0] || '',
-      }
+  return {
+    id: entity.id,
+    html: filteredData || filteredData[0] || '',
   }
 }
-
 export const Meta: EntityMetaInfo = {
   resource: ParagraphResourceType.Wysiwyg,
   component: Wysiwyg,
