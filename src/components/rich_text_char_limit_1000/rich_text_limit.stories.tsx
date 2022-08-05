916bbb8c5626c75114254e55f41a721fc685b409
Penny Lischer <penny.lischer@agile6.com>
2022-08-05 13:28:22 -0700

    VACMS-10084 Component Refactor: Wysiwyg

--- src/components/rich_text_char_limit_1000/rich_text_limit.stories.tsx
+++ src/components/rich_text_char_limit_1000/rich_text_limit.stories.tsx
@@ -13,7 +13,7 @@ const Template: ComponentStory<typeof RichTextCharLimit1000> = (args) => (
 
 export const Example = Template.bind({})
 Example.args = {
-  id: 1,
+  id: 'abc123',
   html: [
     '<p>You may be eligible for VA disability benefits if you meet both of the requirements listed below.</p>\n<p><strong>Both of these must be true. You: </strong></p>\n<ul>\n<li>Have an illness we believe is caused by contact with Agent Orange (called a presumptive disease), <strong>and</strong></li>\n<li>Served in a location or job that put you in contact with Agent Orange (called having a presumption of contact) </li>\n</ul>\n<p>Read the full eligibility requirements for presumptive diseases and presumption of contact below.</p>\n',
   ],
