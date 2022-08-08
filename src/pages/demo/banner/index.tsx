da8b0caa9872bedb0606e182f0933cc89a2e5f90
Aaron <aaron.m.ponce@gmail.com>
2022-08-08 14:35:01 -0700

    fix demo page

--- src/pages/demo/banner/index.tsx
+++ src/pages/demo/banner/index.tsx
@@ -1,22 +1,23 @@
 import { drupalClient } from '@/utils/drupalClient'
 import { GetStaticPropsContext, GetStaticPropsResult } from 'next'
 import { DrupalJsonApiParams } from 'drupal-jsonapi-params'
-import { NodeBanner, NodePromoBanner, NodeResourceType } from '@/types/node'
+import { NodePromoBanner, NodeResourceType } from '@/types/node'
 import Container from '@/components/container'
-import { Banner, BannerProps } from '@/components/banner'
+import { Banner } from '@/components/banner'
 import PromoBanner from '@/components/node/promo_banner'
-import { generalEntityDataService } from '@/lib/delegators/generalEntityDataService'
-import { transformBannerDataService } from '@/components/banner/dataService'
+import { getGlobalElements } from '@/lib/context/getGlobalElements'
 interface BannerPageProps {
-  banner: BannerProps
-  promoBanners: NodePromoBanner[]
+  promoBanners?: NodePromoBanner[]
+  bannerData?: any
+  props?: any
 }
 
-const BannerPage = ({ banner, promoBanners }: BannerPageProps) => {
+const BannerPage = (props, { promoBanners }: BannerPageProps) => {
+  const { bannerData } = props.props
   return (
     <>
       <Container className="container">
-        {banner ? <Banner {...transformBannerDataService({ banner })} /> : null}
+        {bannerData ? <Banner {...bannerData} /> : null}
 
         {/*Maintenance banner*/}
         <div
@@ -44,22 +45,17 @@ export async function getStaticProps(
 ): Promise<GetStaticPropsResult<BannerPageProps>> {
   const params = new DrupalJsonApiParams()
 
-  const banners = await drupalClient.getResourceCollectionFromContext<
-    NodeBanner[]
-  >(NodeResourceType.Banner, context, {
-    params: params.getQueryObject(),
-  })
-
   const promoBanners = await drupalClient.getResourceCollectionFromContext<
     NodePromoBanner[]
   >(NodeResourceType.PromoBanner, context, {
     params: params.getQueryObject(),
   })
-  const banner = generalEntityDataService(banners)
+
   return {
     props: {
-      banner,
       promoBanners,
+      ...(await getGlobalElements(context)),
     },
+    // will be passed to the page component as promoBanners,
   }
 }
