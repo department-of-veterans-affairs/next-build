24cff39a8b2cf39d95dd14dbae1e0bd8e731edd5
Aaron <aaron.m.ponce@gmail.com>
2022-08-09 09:39:39 -0700

    Banner storybook and remove unused test json

---
+++ src/components/banner/banner.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Banner } from './index'
+
+export default {
+  title: 'Node/Banner',
+  component: Banner,
+} as ComponentMeta<typeof Banner>
+
+const Template: ComponentStory<typeof Banner> = (args) => <Banner {...args} />
+
+export const Example = Template.bind({})
+Example.args = {
+  id: 'ccd9d30f-78f9-4358-80d7-191f99b18d43',
+  title: 'COVID-19 vaccines at VA',
+  path: '/va-pittsburgh-health-care/vamc-banner-alert/2021-01-08/help-limit-the-spread-of-covid-19-and-other',
+  body: 'COVID-19 vaccines at VA',
+  alertType: 'information',
+  dismiss: true,
+}
--- src/components/banner/index.test.tsx
+++ src/components/banner/index.test.tsx
@@ -1,15 +1,16 @@
 import { render, screen } from 'test-utils'
 import { Banner } from '@/components/banner'
 
+const banner = {
+  id: 'ccd9d30f-78f9-4358-80d7-191f99b18d43',
+  title: 'COVID-19 vaccines at VA',
+  path: '/va-pittsburgh-health-care/vamc-banner-alert/2021-01-08/help-limit-the-spread-of-covid-19-and-other',
+  body: 'This is the banner body',
+  alertType: 'information',
+  dismiss: true,
+}
+
 describe('<Banner> component renders', () => {
-  const banner = {
-    id: 'ccd9d30f-78f9-4358-80d7-191f99b18d43',
-    title: 'COVID-19 vaccines at VA',
-    path: '/va-pittsburgh-health-care/vamc-banner-alert/2021-01-08/help-limit-the-spread-of-covid-19-and-other',
-    body: 'This is the banner body',
-    alertType: 'information',
-    dismiss: true,
-  }
   test('with valid data', () => {
     render(<Banner {...banner} />)
     expect(screen.queryByText(/This is the banner body/)).toBeInTheDocument()
@@ -21,15 +22,8 @@ describe('<Banner> component renders', () => {
 })
 
 describe('<Banner> component does not render', () => {
-  const banner = {
-    id: 'ccd9d30f-78f9-4358-80d7-191f99b18d43',
-    title: 'COVID-19 vaccines at VA',
-    path: '/va-pittsburgh-health-care/vamc-banner-alert/2021-01-08/help-limit-the-spread-of-covid-19-and-other',
-    body: null,
-    alertType: 'information',
-    dismiss: true,
-  }
   test('without node data', () => {
+    banner.body = null
     render(<Banner {...banner} />)
     expect(
       screen.queryByText(/This is the banner body/)
