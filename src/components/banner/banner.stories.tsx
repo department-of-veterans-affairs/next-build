0cc3e9ca3e74d4ca3f2fe194d9f2b521588c9e18
Aaron <aaron.m.ponce@gmail.com>
2022-08-09 12:32:42 -0700

    link to existing storybook

--- src/components/banner/banner.stories.tsx
+++ src/components/banner/banner.stories.tsx
@@ -14,7 +14,7 @@ Example.args = {
   id: 'ccd9d30f-78f9-4358-80d7-191f99b18d43',
   title: 'COVID-19 vaccines at VA',
   path: '/va-pittsburgh-health-care/vamc-banner-alert/2021-01-08/help-limit-the-spread-of-covid-19-and-other',
-  body: 'COVID-19 vaccines at VA',
+  body: 'The banner component is part of the VSP Design System Storybook located <a href=https://design.va.gov/storybook/?path=/docs/components-va-banner--default>here</a>',
   alertType: 'information',
   dismiss: true,
 }
