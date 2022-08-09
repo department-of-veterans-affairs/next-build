63a0e9b480e477e227efa6f0ae62adc99079870a
Aaron <aaron.m.ponce@gmail.com>
2022-08-09 09:46:11 -0700

    fix layout test

--- src/components/layout/index.test.tsx
+++ src/components/layout/index.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from 'test-utils'
 import '@testing-library/jest-dom'
 import Layout from './index'
-import mock_banner from '../banner/nodeBanner.json'
 
 const children = <div></div>
 const footerData = {
@@ -10,6 +9,14 @@ const footerData = {
   order: 1,
   target: '',
   title: 'Homeless Veterans',
+}
+const banner = {
+  id: 'ccd9d30f-78f9-4358-80d7-191f99b18d43',
+  title: 'COVID-19 vaccines at VA',
+  path: '/va-pittsburgh-health-care/vamc-banner-alert/2021-01-08/help-limit-the-spread-of-covid-19-and-other',
+  body: 'This is the banner body',
+  alertType: 'information',
+  dismiss: true,
 }
 
 describe('<Layout> renders', () => {
@@ -31,7 +38,7 @@ describe('<Layout> renders', () => {
   })
 
   test.skip('<Banner> when bannerData and footerData exist', () => {
-    const props = { bannerData: [mock_banner], footerData: [footerData] }
+    const props = { bannerData: [banner], footerData: [footerData] }
 
     render(<Layout props={props}>{children}</Layout>)
     expect(screen.getByRole('va-banner')).toHaveAttribute(
