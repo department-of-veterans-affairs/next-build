305241872c73e3728ad14b722e5d6b7d486b87f1
Penny Lischer <penny.lischer@agile6.com>
2022-08-09 17:03:44 -0700

    VACMS-10014 Added back image.stories.tsx

---
+++ src/components/image/image.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import Image from './index'
+
+export default {
+  title: 'Components/Image',
+  component: Image,
+} as ComponentMeta<typeof Image>
+
+const Template: ComponentStory<typeof Image> = (args) => <Image {...args} />
+
+export const Example = Template.bind({})
+Example.args = {
+  src: 'http://placekitten.com/200/300',
+  alt: 'an example image',
+  title: 'The grayest box of them all',
+  height: '300',
+  width: '200',
+}
