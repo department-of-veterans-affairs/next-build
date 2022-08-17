42edcf0f95453c5a545c509a275ef39f3506a893
Penny Lischer <penny.lischer@agile6.com>
2022-08-17 13:53:36 -0700

    VACMS-10260 reverted drupalClient.ts changes

--- src/lib/utils/drupalClient.ts
+++ src/lib/utils/drupalClient.ts
@@ -2,59 +2,19 @@ import { DrupalClient } from 'next-drupal'
 import crossFetch from 'cross-fetch'
 import { SocksProxyAgent } from 'socks-proxy-agent'
 import { addCAs } from 'syswide-cas'
-import * as fs from 'fs'
 
-const https = require('https')
-
-interface TLSSecretData {
-  cert: string
-}
-
-export const agent = new SocksProxyAgent('socks://127.0.0.1:2001')
-export const CERTS_DIRECTORY = 'certs/'
-export const baseUrl = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL
+export const baseUrl = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL || 'cms.va.gov'
 export const useProxy = baseUrl.includes('cms.va.gov')
-
-export const loadCertificate = async (): Promise<TLSSecretData> => {
-  const certs = await fs.promises.readdir(CERTS_DIRECTORY)
-  const certificateAuthority = certs.find((c) => c.includes('.pem'))
-  https.globalAgent.options.ca = []
-
-  if (!certificateAuthority) {
-    fs.readdir(CERTS_DIRECTORY, (err, files) => {
-      if (err) {
-        console.log(err)
-      } else {
-        files.forEach((file) => {
-          const cert = fs.readFileSync(CERTS_DIRECTORY + file)
-          https.globalAgent.options.ca.push(cert)
-        })
-      }
-    })
-  }
-
-  if (fs.existsSync(CERTS_DIRECTORY)) {
-    return addCAs(CERTS_DIRECTORY || certificateAuthority)
-  }
+if (useProxy) {
+  addCAs('certs/VA-Internal-S2-RCA1-v1.pem')
 }
-
 export const fetcher = (input: RequestInfo, init?: RequestInit) => {
-  if (useProxy) {
-    loadCertificate()
-      .then(() => {
-        console.log('Loaded certificate')
-      })
-      .catch((err) => {
-        console.log('Error loading certificate', err)
-      })
-      .finally(() => {
-        console.log('Finished loading certificate')
-      })
-  }
+  const agent = new SocksProxyAgent('socks://127.0.0.1:2001')
   const options = {
     agent: useProxy ? agent : null,
     ...init,
   }
+
   return crossFetch(input, {
     ...options,
   })
