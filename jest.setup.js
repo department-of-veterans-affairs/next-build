e6c08ad8a38955738e3d7a3cdb9a344faea1b458
Tim Cosgrove <tim.cosgrove@agile6.com>
2022-08-16 19:50:03 -0700

    Configure testing to all set NODE_ENV to test.

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,15 @@
 import '@testing-library/jest-dom/extend-expect'
 import 'jest-axe/extend-expect'
 import nock, { back as nockBack } from 'nock'
+import { loadEnvConfig } from '@next/env'
 
 nockBack.setMode('record')
 global.setImmediate = jest.useRealTimers
+
+// Load Environment vars to Jest.
+const loadEnv = async () => {
+  const projectDir = process.cwd()
+  loadEnvConfig(projectDir)
+}
+
+loadEnv()
