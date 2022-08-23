d874b2afe997b8f710f2a94e3337c3803b7e6c92
Tim Cosgrove <tim.cosgrove@agile6.com>
2022-08-23 09:35:14 -0700

    Removing loadEnv to see if it interferes with swc.

--- jest.setup.js
+++ jest.setup.js
@@ -1,15 +1,6 @@
 import '@testing-library/jest-dom/extend-expect'
 import 'jest-axe/extend-expect'
 import { back as nockBack } from 'nock'
-import { loadEnvConfig } from '@next/env'
 
 nockBack.setMode('record')
 global.setImmediate = jest.useRealTimers
-
-// Load Environment vars to Jest.
-const loadEnv = async () => {
-  const projectDir = process.cwd()
-  loadEnvConfig(projectDir)
-}
-
-loadEnv()
