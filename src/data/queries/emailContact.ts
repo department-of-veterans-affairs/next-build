b3cbc02455a602337039c393ee24878ac2db609b
Tanner Heffner <tanner.heffner@agile6.com>
2022-09-26 11:59:59 -0700

    run yarn format

--- src/data/queries/emailContact.ts
+++ src/data/queries/emailContact.ts
@@ -3,9 +3,10 @@ import { ParagraphEmailContact } from '@/types/dataTypes/drupal/paragraph'
 import { QueryFormatter } from 'next-drupal-query'
 import { EmailContactType } from '@/types/index'
 
-export const formatter: QueryFormatter<ParagraphEmailContact, EmailContactType> = (
-  entity: ParagraphEmailContact
-) => {
+export const formatter: QueryFormatter<
+  ParagraphEmailContact,
+  EmailContactType
+> = (entity: ParagraphEmailContact) => {
   return {
     id: entity.id,
     label: entity.field_email_label || null,
