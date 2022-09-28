f5db9e83d915e93a84424ceb9562959f20e54c64
Tanner Heffner <tannerjheffner@gmail.com>
2022-09-28 08:52:52 -0700

    VACMS-10725 refactor email contact to use queries (#108)
    * VACMS-10725 refactor email contact to use queries
    
    * run yarn format

---
+++ src/data/queries/emailContact.ts
@@ -0,0 +1,15 @@
+// Define the query params for fetching paragraph--email_contact.
+import { ParagraphEmailContact } from '@/types/dataTypes/drupal/paragraph'
+import { QueryFormatter } from 'next-drupal-query'
+import { EmailContactType } from '@/types/index'
+
+export const formatter: QueryFormatter<
+  ParagraphEmailContact,
+  EmailContactType
+> = (entity: ParagraphEmailContact) => {
+  return {
+    id: entity.id,
+    label: entity.field_email_label || null,
+    address: entity.field_email_address || null,
+  }
+}
