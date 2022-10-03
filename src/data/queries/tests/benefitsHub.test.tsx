c431791a7eac524de0e3153047536df58c8fc344
Tanner Heffner <tannerjheffner@gmail.com>
2022-10-03 13:06:15 -0700

    VACMS-0000 add tests for refactored queries (#111)
    * VACMS-10262 refactor wysiwyg and rich text char limit 1000
    
    * VACMS-0000 add tests for new queries
    
    * VACMS-0000 add eslint for ts-ignore
    
    * Prettier error.
    
    Co-authored-by: Tim Cosgrove <tim.cosgrove@agile6.com>

---
+++ src/data/queries/tests/benefitsHub.test.tsx
@@ -0,0 +1,30 @@
+import { NodeLandingPage } from '@/types/dataTypes/drupal/node'
+import { queries } from '@/data/queries'
+import mockData from '@/mocks/benefitHubs.mock.json'
+
+// field_related_office is causing issues here, I believe because the referenced node is unpublished (node/38439)
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const nodeBenefitsHubMock: NodeLandingPage[] = mockData
+
+describe('node--news_story formatData', () => {
+  let windowSpy
+
+  beforeEach(() => {
+    windowSpy = jest.spyOn(window, 'window', 'get')
+  })
+
+  afterEach(() => {
+    windowSpy.mockRestore()
+  })
+
+  test('outputs formatted data', () => {
+    windowSpy.mockImplementation(() => undefined)
+
+    expect(
+      nodeBenefitsHubMock.map((mock) => {
+        return queries.formatData('node--landing_page', mock)
+      })
+    ).toMatchSnapshot()
+  })
+})
---
+++ src/data/queries/tests/wysiwyg.test.tsx
@@ -0,0 +1,27 @@
+import { queries } from '@/data/queries'
+import { ParagraphWysiwyg } from '@/types/dataTypes/drupal/paragraph'
+import mockData from '@/mocks/wysiwyg.mock.json'
+
+const wysiwygMock: ParagraphWysiwyg[] = mockData
+
+describe('paragraph--wysiwyg formatData', () => {
+  let windowSpy
+
+  beforeEach(() => {
+    windowSpy = jest.spyOn(window, 'window', 'get')
+  })
+
+  afterEach(() => {
+    windowSpy.mockRestore()
+  })
+
+  test('outputs formatted data', () => {
+    windowSpy.mockImplementation(() => undefined)
+
+    expect(
+      wysiwygMock.map((mock) => {
+        return queries.formatData('paragraph--wysiwyg', mock)
+      })
+    ).toMatchSnapshot()
+  })
+})
