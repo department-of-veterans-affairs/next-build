50fc7610b2ca1e506c82ab89d8c3fd18c5db40e5
Tanner Heffner <tanner.heffner@agile6.com>
2023-08-03 09:57:28 -0700

    prettier check failed... prettier write... writes?

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,9 +4,11 @@ module.exports = {
 
   // IDE may warn about a duplicate key, but need to be separate for correct env handling.
   // Lint and format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js|jsx)': (filenames) => [
-    `yarn lint --fix ${filenames.join(' ')}`,
-    `yarn prettier ${filenames.join(' ')} --check`,
+  '*.(ts|tsx|js|jsx)': (filenames) => [
+    `yarn lint --fix --file ${filenames
+      .map((file) => file.split(process.cwd())[1])
+      .join(' --file ')}`,
+    `yarn prettier ${filenames.join(' ')} --write`,
   ],
 
   // Run unit tests relating to modified files.
