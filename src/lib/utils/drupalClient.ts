797fe9d619e65978e03b3d67461bb3c20a28fed6
Tanner Heffner <tanner.heffner@agile6.com>
2023-08-11 14:04:45 -0700

    change certs in tugboat to match cms repo, remove local from allowedhost for proxy

--- src/lib/utils/drupalClient.ts
+++ src/lib/utils/drupalClient.ts
@@ -7,15 +7,17 @@ export const baseUrl = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL || 'cms.va.gov'
 const url = new URL(baseUrl)
 const host = url.host
 
-const allowedHosts = ['cms.va.gov', 'va-gov-cms.ddev.site']
+const allowedHosts = ['cms.va.gov']
 
 // tugboat env doesn't need the SOCKS proxy. APP_ENV set in tugboat
 export const useProxy =
   allowedHosts.includes(host) && process.env.APP_ENV != 'tugboat'
 
 export const fetcher = async (input: RequestInfo, init?: RequestInit) => {
-  const syswideCas = await import('syswide-cas')
-  syswideCas.addCAs('certs/VA-Internal-S2-RCA-combined.pem')
+  if (useProxy) {
+    const syswideCas = await import('syswide-cas')
+    syswideCas.addCAs('certs/VA-Internal-S2-RCA-combined.pem')
+  }
 
   const agent = new SocksProxyAgent('socks://127.0.0.1:2001')
   const options = {
