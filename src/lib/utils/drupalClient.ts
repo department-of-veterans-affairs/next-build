86886a40a9e767c39304f470a5c1b607bc10d286
Tanner Heffner <tanner.heffner@agile6.com>
2023-08-14 14:10:30 -0700

    add anchor and escape .

--- src/lib/utils/drupalClient.ts
+++ src/lib/utils/drupalClient.ts
@@ -9,7 +9,7 @@ const host = url.host
 
 // tugboat env doesn't need the SOCKS proxy. APP_ENV set in tugboat
 export const useProxy =
-  host.match('cms.va.gov') && process.env.APP_ENV != 'tugboat'
+  host.match(/cms\.va\.gov$/) && process.env.APP_ENV != 'tugboat'
 
 export const fetcher = async (input: RequestInfo, init?: RequestInit) => {
   if (useProxy) {
