c76aef9b0a7fd9750864e359f28c376cd7473fa1
Tanner Heffner <tanner.heffner@agile6.com>
2023-09-19 14:43:08 -0700

    remove workerthreads

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
     // This is experimental but can
     // be enabled to allow parallel threads
     // with nextjs automatic static generation
-    workerThreads: false,
+    // workerThreads: false,
     cpus: 2,
   },
 }
