82bdf4bc0231c6b07e8c2262f547b872e8841a9d
Ryan Koch <ryan.koch.0213@gmail.com>
2023-09-20 17:32:55 -0500

    Fetches subsequent pages in parallel rather than series

--- src/lib/utils/staticPaths.ts
+++ src/lib/utils/staticPaths.ts
@@ -1,42 +1,59 @@
-import { GetStaticPathsContext } from 'next'
 import { drupalClient } from '@/lib/utils/drupalClient'
 import { getAllPagedListingPaths } from '@/lib/utils/listingPages'
 import RESOURCE_TYPES from '@/lib/constants/resourceTypes'
 import { JsonApiResponse, JsonApiResourceWithPath } from 'next-drupal'
 
-async function appendAllSubsequentPages(currentPage: JsonApiResponse) {
-  if (!currentPage) {
-    return []
-  }
-
-  const currentPageData = drupalClient.deserialize(
-    currentPage
-  ) as JsonApiResourceWithPath[]
-  const nextPageLink = currentPage.links?.next
-  const nextPageUrl =
-    typeof nextPageLink === 'string' ? nextPageLink : nextPageLink?.href
-  if (nextPageUrl) {
-    try {
-      const nextPageResponse = await drupalClient.fetch(nextPageUrl)
-      const nextPage = await nextPageResponse.json()
-      return [...currentPageData, ...(await appendAllSubsequentPages(nextPage))]
-    } catch (err) {
-      return currentPageData
-    }
-  } else {
-    return currentPageData
-  }
-}
-
 async function getAllResourcesByResourceType(resourceType: string) {
+  const pageSize = 50 //must be <= 50 due to JSON:API limit
+  const params = {
+    // Note: we only need `path`, but we can't put it first:
+    // See:
+    //  https://next-drupal.org/guides/page-limit
+    //  https://dsva.slack.com/archives/C01SR56755H/p1695244241079879?thread_ts=1695070010.697129&cid=C01SR56755H
+    [`fields[${resourceType}]`]: 'title,path',
+    'page[limit]': pageSize,
+  }
+
+  // 1. Fetch first page.
   const firstPage = await drupalClient.getResourceCollection<JsonApiResponse>(
     resourceType,
     {
+      params,
       deserialize: false,
     }
   )
 
-  return appendAllSubsequentPages(firstPage)
+  // 2. Determine number of pages to fetch.
+  const totalResourceCount = firstPage.meta.count
+  const firstPageData = drupalClient.deserialize(
+    firstPage
+  ) as JsonApiResourceWithPath[]
+  const pageCount = Math.ceil(totalResourceCount / pageSize)
+
+  // 3. If more pages, fetch them in parallel.
+  // Note: If we used JSON:API `next` links, we'd have to fetch in series.
+  if (pageCount <= 1) {
+    return firstPageData
+  }
+  const subsequentPageData = await Promise.all(
+    Array.from({
+      length: pageCount - 1,
+    }).map((_, i) => {
+      const pageNum = i + 2
+      return drupalClient.getResourceCollection<JsonApiResourceWithPath[]>(
+        resourceType,
+        {
+          params: {
+            ...params,
+            'page[offset]': (pageNum - 1) * pageSize,
+          },
+        }
+      )
+    })
+  )
+
+  // 4. Glue all pages together.
+  return [firstPageData, ...subsequentPageData].flat()
 }
 
 export async function getStaticPathsByResourceType(
