f41822cd9b3e65a5fdafc16c560b428bc9309892
Josh Mills <61624970+jtmst@users.noreply.github.com>
2023-09-25 11:34:43 -0400

    Add APP_ENV flag for envs (#176)
    Co-authored-by: Tanner Heffner <tannerjheffner@gmail.com>
    Co-authored-by: Tanner Heffner <tanner.heffner@agile6.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const dotenv = require('dotenv')
+const dotenvExpand = require('dotenv-expand')
+
+const myEnv = dotenv.config({
+  path: `envs/.env.${process.env.APP_ENV || 'local'}`,
+})
+
+dotenvExpand.expand(myEnv)
+
 /**
  * @type {import('next').NextConfig}
  */
+
 const nextConfig = {
   images: {
     domains: [
