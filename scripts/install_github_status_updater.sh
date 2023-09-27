5fc7ae1605f3f3815cc1feb8f195497f34329889
Tanner Heffner <tanner.heffner@agile6.com>
2023-09-27 12:54:10 -0700

    change file that gsu saves to

--- scripts/install_github_status_updater.sh
+++ scripts/install_github_status_updater.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e;
-destination="${1:-./bin}";
+destination="${1:-\/bin}";
 version="${2:-0.5.0}";
 executable_path="${destination}/github-status-updater";
 curl --location "https://github.com/cloudposse/github-status-updater/releases/download/${version}/github-status-updater_linux_amd64" --output "${executable_path}";
