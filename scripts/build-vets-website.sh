b229bd04daa6b8e118eb83b3eb98e3a76d1aa116
Tanner Heffner <tanner.heffner@agile6.com>
2023-10-03 15:06:42 -0700

    try to use a diff node version for vets-website

--- scripts/build-vets-website.sh
+++ scripts/build-vets-website.sh
@@ -1,14 +1,11 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
+#!/bin/sh
 cd ../vets-website
 
 echo "Node $(node -v)"
 echo "NPM $(npm -v)"
 echo "Yarn $(yarn -v)"
 
-yarn install
 export NODE_EXTRA_CA_CERTS=/etc/ssl/certs/ca-certificates.crt
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=TRUE
+yarn install
 yarn build
