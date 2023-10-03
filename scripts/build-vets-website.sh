6a1cddfb6c078b2dd2a962771e2e9b5b75a35902
Tanner Heffner <tanner.heffner@agile6.com>
2023-10-03 15:10:23 -0700

    try nvm stuff

--- scripts/build-vets-website.sh
+++ scripts/build-vets-website.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd ../vets-website
 
+nvm use 14.15.1
+
 echo "Node $(node -v)"
 echo "NPM $(npm -v)"
 echo "Yarn $(yarn -v)"
@@ -9,3 +11,5 @@ export NODE_EXTRA_CA_CERTS=/etc/ssl/certs/ca-certificates.crt
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=TRUE
 yarn install
 yarn build
+
+nvm use 18.18.0
