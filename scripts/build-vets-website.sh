a6aa3461ac93d0d98c195dc6001302d0c6aa308f
Tanner Heffner <tanner.heffner@agile6.com>
2023-10-03 15:12:34 -0700

    add nvm bit to vets-web script

--- scripts/build-vets-website.sh
+++ scripts/build-vets-website.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 cd ../vets-website
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm use 14.15.1
 
 echo "Node $(node -v)"
