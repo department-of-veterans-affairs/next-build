a38244370b856deffe0fd7468c5ae7542b3c5172
Tanner Heffner <tanner.heffner@agile6.com>
2023-10-03 14:55:34 -0700

    take 3

--- scripts/build-vets-website.sh
+++ scripts/build-vets-website.sh
@@ -1,16 +1,14 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-source ~/.bashrc
 
-pushd "../vets-website"
+cd ../vets-website
 
 echo "Node $(node -v)"
 echo "NPM $(npm -v)"
 echo "Yarn $(yarn -v)"
 
 yarn install
-export NODE_EXTRA_CA_CERTS=/etc/pki/tls/certs/ca-bundle.crt
+export NODE_EXTRA_CA_CERTS=/etc/ssl/certs/ca-certificates.crt
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=TRUE
 yarn build
-popd
