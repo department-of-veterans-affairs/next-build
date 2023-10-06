fe937d697975e3e60cda0275764da5b79b4185e1
Tanner Heffner <tanner.heffner@agile6.com>
2023-10-06 16:02:20 -0700

    clean up header queries

--- src/data/queries/headerFooter.ts
+++ src/data/queries/headerFooter.ts
@@ -7,14 +7,13 @@ import {
 import { queries } from '.'
 import { drupalClient } from '@/lib/drupal/drupalClient'
 import { Menu, HeaderMegaMenu } from '@/types/dataTypes/drupal/menu'
-import { HeaderFooterData, MegaMenuPromoColumn } from '@/types/index'
+import { HeaderFooterData } from '@/types/index'
 import { buildHeaderFooterData } from '@/lib/utils/headerFooter'
 
 export type RawHeaderFooterData = {
   footerColumns: Menu
   footerBottomRail: Menu
   headerMegaMenu: HeaderMegaMenu
-  promoBlocks: MegaMenuPromoColumn[]
 }
 
 // Define the query params for fetching footer menu data.
@@ -22,67 +21,54 @@ export const params: QueryParams<null> = () => {
   return queries.getParams().addFields('menu_items', ['title,url'])
 }
 
-// Define the query params for fetching header megamenu data.
+// Define the query params for fetching header megamenu data. Include referenced promo block data, if present
 export const megaMenuParams: QueryParams<null> = () => {
-  return queries.getParams().addInclude(['field_promo_reference'])
-}
-
-export const promoBlockParams: QueryParams<null> = () => {
   return queries
     .getParams()
-    .addInclude(['field_image', 'field_image.media', 'field_promo_link'])
+    .addInclude([
+      'field_promo_reference',
+      'field_promo_reference.field_image',
+      'field_promo_reference.field_image.image',
+      'field_promo_reference.field_promo_link',
+    ])
 }
 
 // Define the option types for the data loader.
 type DataOpts = QueryOpts<{
   params?: object
   megaMenuParams?: object
-  promoBlockParams?: object
 }>
 
 export const data: QueryData<DataOpts, RawHeaderFooterData> = async (opts) => {
   // Gather data from the different menus for the headerFooter data object
-  const footerColumns = await drupalClient.getMenu('va-gov-footer', opts.params)
-  const footerBottomRail = await drupalClient.getMenu(
-    'footer-bottom-rail',
-    opts.params
-  )
+  const footerColumns = await drupalClient.getMenu('va-gov-footer', {
+    params: params().getQueryObject(),
+  })
+  const footerBottomRail = await drupalClient.getMenu('footer-bottom-rail', {
+    params: params().getQueryObject(),
+  })
   const headerMegaMenu: HeaderMegaMenu = await drupalClient.getMenu(
     'header-megamenu',
-    opts.megaMenuParams
+    {
+      params: megaMenuParams().getQueryObject(),
+    }
   )
-
-  // promo blocks only exist on certain items in the menu
-  const blocksToGather = headerMegaMenu.items.filter(
-    (item) => item.field_promo_reference !== null || undefined
-  )
-  const promoBlocks = []
-
-  // query actual block data from reference
-  for (const block of blocksToGather) {
-    const promo = await queries.getData('block_content--promo', {
-      id: block.field_promo_reference.id,
-    })
-    promoBlocks.push(promo)
-  }
 
   return {
     footerColumns,
     footerBottomRail,
     headerMegaMenu,
-    promoBlocks,
   }
 }
 
 export const formatter: QueryFormatter<
   RawHeaderFooterData,
   HeaderFooterData
-> = ({ footerColumns, footerBottomRail, headerMegaMenu, promoBlocks }) => {
+> = ({ footerColumns, footerBottomRail, headerMegaMenu }) => {
   const { footerData, megaMenuData } = buildHeaderFooterData({
     footerBottomRail,
     footerColumns,
     headerMegaMenu,
-    promoBlocks,
   })
 
   // Data assembled into shape front-end widget expects
--- src/lib/utils/headerFooter.test.ts
+++ src/lib/utils/headerFooter.test.ts
@@ -6,13 +6,11 @@ describe('headerFooter', () => {
     const headerMegaMenu = { items: [], tree: [] }
     const footerBottomRail = { items: [], tree: [] }
     const footerColumns = { items: [], tree: [] }
-    const promoBlocks = []
 
     const result = buildHeaderFooterData({
       footerBottomRail,
       footerColumns,
       headerMegaMenu,
-      promoBlocks,
     })
 
     expect(result.footerData).toBeInstanceOf(Array<FooterLink>)
