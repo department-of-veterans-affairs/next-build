e518ff1d3c89b3cbd2068a8e35e4da71ebc1b8a1
Josh Mills <61624970+jtmst@users.noreply.github.com>
2023-10-12 14:20:35 -0400

    skip link added (#194)

---
+++ src/lib/utils/handleSkipLink.ts
@@ -0,0 +1,25 @@
+import { MouseEvent } from 'react'
+
+export const handleSkipLink = (e: MouseEvent<HTMLAnchorElement>) => {
+  e.preventDefault()
+  const contentDiv = document.getElementById('content')
+  const firstH1 = contentDiv && contentDiv.querySelector('h1')
+  if (firstH1) {
+    firstH1.setAttribute('tabindex', '-1')
+    firstH1.focus()
+
+    const h1Position = firstH1.getBoundingClientRect().top + window.scrollY
+    window.scrollTo({
+      top: h1Position,
+    })
+
+    // Cleanup
+    firstH1.addEventListener(
+      'blur',
+      () => {
+        firstH1.removeAttribute('tabindex')
+      },
+      { once: true }
+    )
+  }
+}
