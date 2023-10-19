c4e83f004d764683f7abddab2a5f9c2966cd2ea1
Tanner Heffner <tanner.heffner@agile6.com>
2023-10-19 16:45:16 -0700

    update cypress tests

---
+++ cypress/e2e/newsStory.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+context('News Story', () => {
+  it('News Story page renders with navigation back to parent story list', () => {
+    cy.visit('/butler-health-care/stories/its-flu-shot-time/')
+
+    cy.get('#news-stories-listing-link').click()
+    cy.location('pathname').should('equal', '/butler-health-care/stories/')
+  })
+
+  it('Should render without a11y errors', () => {
+    cy.visit('/butler-health-care/stories/its-flu-shot-time/')
+
+    cy.testA11y()
+  })
+})
---
+++ cypress/e2e/storyListing.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+context('Story Listing', () => {
+  it('Story Listing page renders with stories that can be navigated to', () => {
+    cy.visit('/butler-health-care/stories')
+
+    cy.get('.usa-unstyled-list a').first().click()
+    cy.location('pathname').should('include', '/butler-health-care/stories/')
+  })
+
+  it('Story Listing pages should be paginated if there are more than 10 stories', () => {
+    cy.visit('/eastern-oklahoma-health-care/stories')
+    // .shadow() required to check shadow DOM content
+    cy.get('va-pagination').shadow().contains('2').click()
+    cy.location('pathname').should('include', '/page-2/')
+
+    cy.get('va-pagination').shadow().contains('Next').click()
+    cy.location('pathname').should('include', '/page-3/')
+  })
+
+  it('Should render a message if there are no stories', () => {
+    cy.visit('/portland-health-care/stories')
+    cy.get('.usa-unstyled-list').should('have.text', 'No stories at this time.')
+  })
+
+  it('Should render without a11y errors', () => {
+    cy.visit('/butler-health-care/stories')
+
+    cy.testA11y()
+  })
+})
