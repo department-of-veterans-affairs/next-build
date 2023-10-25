d777a0bab08b3a55f4fcf6a884f0e916b0c890a2
Tanner Heffner <tanner.heffner@agile6.com>
2023-10-25 15:45:29 -0700

    adjust / on test paths

--- cypress/e2e/newsStory.cy.js
+++ cypress/e2e/newsStory.cy.js
@@ -2,14 +2,14 @@
 
 context('News Story', () => {
   it('News Story page renders with navigation back to parent story list', () => {
-    cy.visit('/butler-health-care/stories/its-flu-shot-time/')
+    cy.visit('butler-health-care/stories/its-flu-shot-time/')
 
     cy.get('#news-stories-listing-link').click()
     cy.location('pathname').should('equal', '/butler-health-care/stories/')
   })
 
   it.skip('Should render without a11y errors', () => {
-    cy.visit('/butler-health-care/stories/its-flu-shot-time/')
+    cy.visit('butler-health-care/stories/its-flu-shot-time/')
 
     cy.testA11y()
   })
--- cypress/e2e/storyListing.cy.js
+++ cypress/e2e/storyListing.cy.js
@@ -2,14 +2,14 @@
 
 context('Story Listing', () => {
   it('Story Listing page renders with stories that can be navigated to', () => {
-    cy.visit('/butler-health-care/stories')
+    cy.visit('butler-health-care/stories')
 
     cy.get('.usa-unstyled-list a').first().click()
     cy.location('pathname').should('include', '/butler-health-care/stories/')
   })
 
   it('Story Listing pages should be paginated if there are more than 10 stories', () => {
-    cy.visit('/eastern-oklahoma-health-care/stories')
+    cy.visit('eastern-oklahoma-health-care/stories')
     // .shadow() required to check shadow DOM content
     cy.get('va-pagination').shadow().contains('2').click()
     cy.location('pathname').should('include', '/page-2/')
@@ -19,12 +19,12 @@ context('Story Listing', () => {
   })
 
   it('Should render a message if there are no stories', () => {
-    cy.visit('/portland-health-care/stories')
+    cy.visit('portland-health-care/stories')
     cy.get('.usa-unstyled-list').should('have.text', 'No stories at this time.')
   })
 
   it.skip('Should render without a11y errors', () => {
-    cy.visit('/butler-health-care/stories')
+    cy.visit('butler-health-care/stories')
 
     cy.testA11y()
   })
