3549527de8ae317b720996484ac8654a7bdd83c5
Tanner Heffner <tanner.heffner@agile6.com>
2023-10-31 10:14:46 -0700

    removed commented lines

--- cypress/e2e/newsStory.cy.js
+++ cypress/e2e/newsStory.cy.js
@@ -1,12 +1,6 @@
 /// <reference types="cypress" />
-// import { features, vamcEhr } from './mocks'
-context('News Story', () => {
-  // beforeEach(() => {
-  //   cy.intercept('/data/cms/vamc-ehr*', vamcEhr).as('facilityData')
-  //   cy.intercept('/v0/feature_toggles*', features).as('features')
-  //   cy.intercept('/js-report/api/*', {}).as('sentry')
-  // })
 
+context('News Story', () => {
   it('News Story page renders with navigation back to parent story list', () => {
     cy.visit('/butler-health-care/stories/its-flu-shot-time/')
 
