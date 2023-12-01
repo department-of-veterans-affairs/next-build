5a405b7f475f681e7abc7a12983eadecec091a8f
Tanner Heffner <tanner.heffner@agile6.com>
2023-11-30 17:57:20 -0800

    adjust playwright workflow, ci flag

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
   staticPageGenerationTimeout: 180, //arbitrary; 60 is default but it's too small
   experimental: {
     // so tugboat doesn't go crazy
-    cpus: 3,
+    cpus: process.env.CI ? 3 : null,
   },
 }
 module.exports = nextConfig
