c8b8d69fe30a38a821df769d8d141c57708ccb4f
Tanner Heffner <tanner.heffner@agile6.com>
2023-11-30 16:52:25 -0800

    limit cpus to 3

--- next.config.js
+++ next.config.js
@@ -19,5 +19,9 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   staticPageGenerationTimeout: 180, //arbitrary; 60 is default but it's too small
+  experimental: {
+    // so tugboat doesn't go crazy
+    cpus: 3,
+  },
 }
 module.exports = nextConfig
