4dda7303b4ebcf40351179307dc5a606c9254f28
Tanner Heffner <tanner.heffner@agile6.com>
2023-12-01 10:15:26 -0800

    switch from ioredis to node_redis

--- src/lib/utils/redisCache.ts
+++ src/lib/utils/redisCache.ts
@@ -1,14 +1,46 @@
+/* eslint-disable no-console */
 import { DataCache } from 'next-drupal'
-import Redis from 'ioredis'
+import { createClient, RedisClientType } from 'redis'
 
-// connects to a running redis server, defaults to 127.0.0.1:6379
-const redis = new Redis(process.env.REDIS_URL)
+let redisClient: RedisClientType
+let isReady: boolean
 
-export const redisCache: DataCache = {
+async function getCache(): Promise<RedisClientType> {
+  if (!isReady) {
+    // connects to a running redis server, defaults to 127.0.0.1:6379
+    redisClient = createClient({
+      url: process.env.REDIS_URL,
+    })
+    redisClient.on('error', (err) => console.error(`Redis Error: ${err}`))
+    redisClient.on('connect', () => console.info('Redis connected'))
+    redisClient.on('reconnecting', () => console.info('Redis reconnecting'))
+    redisClient.on('ready', () => {
+      isReady = true
+      console.info('Redis ready!')
+    })
+    await redisClient.connect()
+  }
+  return redisClient
+}
+
+getCache()
+  .then((connection) => {
+    redisClient = connection
+  })
+  .catch((err) => {
+    console.error({ err }, 'Failed to connect to Redis')
+  })
+
+// This is the cache wrapper that drupalClient can use
+const redisCache: DataCache = {
   async set(key, value) {
-    return await redis.set(key, value)
+    const cache = await getCache()
+    return await cache.set(key, value)
   },
   async get(key) {
-    return await redis.get(key)
+    const cache = await getCache()
+    return await cache.get(key)
   },
 }
+
+export { getCache, redisCache }
