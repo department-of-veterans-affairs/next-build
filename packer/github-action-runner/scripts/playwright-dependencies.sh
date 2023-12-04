09acdfe4dff942220b567ca1e607bcbbfbff2250
eric oliver <eric.oliver@agile6.com>
2023-12-04 15:12:57 -0800

    Ensure workflow triggers on Pull Request and fix playwright dep install.

--- packer/github-action-runner/scripts/playwright-dependencies.sh
+++ packer/github-action-runner/scripts/playwright-dependencies.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Playwright dependencies:
+set -e
+set -o pipefail
 
-apt-get install -y libatk1.0-0 \                
-             libatk-bridge2.0-0 \                          
-             libxkbcommon0 \                               
-             libatspi2.0-0 \                               
-             libxcomposite1 \                              
-             libxdamage1 \                                 
-             libxfixes3 \                                  
-             libxrandr2 \                                  
-             libgbm1 \                                     
-             libasound2   
+apt update
+apt install -y \
+    batk1.0-0 \
+    libatk-bridge2.0-0 \
+    libxkbcommon0 \
+    libatspi2.0-0 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm1 \
+    libasound2
