e846d918ee0438fd3e3cd3e88b4b2ad852df18f9
Tanner Heffner <tanner.heffner@agile6.com>
2023-12-06 15:19:06 -0800

    try bumping nav timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,36 +4,36 @@ export default defineConfig({
   testDir: './playwright/tests',
   outputDir: './playwright/test-results',
   reporter: [['html', { outputFolder: './playwright/test-report' }]],
-  fullyParallel: true,
+  fullyParallel: process.env.CI ? false : true,
+
+  // limit the number of workers running. start with 1, and if your tests pass reliably, consider increasing to
+  // speed up runs. the new % option may be a good choice, e.g.  workers: '50%'
+  // workers: 1,
 
   // Fail the build on CI if you accidentally left test.only in the source code.
   forbidOnly: !!process.env.CI,
 
-  // limit the number of workers running. start with 1, and if your tests pass reliably, consider increasing to
-  // speed up runs. the new % option may be a good choice, e.g.  workers: '50%'
-  workers: 1,
-
   // Retry on CI only.
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
 
   use: {
-    // set this to a large number, to account for pages occasionally loading more slowly in your pipeline
-    // than they do locally
-    actionTimeout: 12000,
-
     baseURL: process.env.SITE_URL || 'http://localhost:8001',
 
     // Whether to ignore HTTPS errors during navigation.
     ignoreHTTPSErrors: true,
 
+    // browser context options
     contextOptions: {
       ignoreHTTPSErrors: true,
     },
 
+    // doubling for CI to resolve urls
+    navigationTimeout: process.env.CI ? 60000 : 30000,
+
     // Collect trace when retrying the failed test.
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    video: 'on-first-retry',
     viewport: { width: 1280, height: 720 },
   },
 
@@ -42,11 +42,13 @@ export default defineConfig({
     {
       name: 'main',
       testIgnore: /.a11y.spec.js/,
+      dependencies: ['setup'],
     },
     // Sitemap a11y test only
     {
       name: 'a11y',
       testMatch: /.a11y.spec.js/,
+      dependencies: ['setup'],
     },
   ],
 })
