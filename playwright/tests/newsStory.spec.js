3f20c5a992c807fe40cc37fe5465ee7c7ffa5bc2
Tanner Heffner <tanner.heffner@agile6.com>
2023-12-07 08:53:11 -0800

    try blocking certain requests

--- playwright/tests/newsStory.spec.js
+++ playwright/tests/newsStory.spec.js
@@ -1,6 +1,13 @@
 const { test, expect } = require('../utils/next-test')
 
 test.describe('News Story', () => {
+  test.beforeEach(async ({ context }) => {
+    // Block requests for gtm, localhost services
+    await context.route(/gtm.js*/, (route) => route.abort())
+    await context.route(/:3000*/, (route) => route.abort())
+    await context.route(/:3001*/, (route) => route.abort())
+  })
+
   test('News Story page renders with navigation back to parent story list', async ({
     page,
   }) => {
