01998bfde8b37f8a27cbdddcb44202a259cf7c99
Tanner Heffner <tanner.heffner@agile6.com>
2023-12-12 11:36:52 -0800

    double awaits across the sky

--- src/lib/utils/redisCache.ts
+++ src/lib/utils/redisCache.ts
@@ -24,10 +24,11 @@ export function redisCache(
 ): DataCache {
   return {
     async set(key, value) {
-      return (await client).set(key, value)
+      // Need to double await here to wait for the client, then wait for the set.
+      return await (await client).set(key, value)
     },
     async get(key) {
-      // Need to double await here to wait for the client, then wait for the get
+      // Need to double await here to wait for the client, then wait for the get.
       return await (await client).get(key)
     },
   }
