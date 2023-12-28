74b29ff3ff6be96a1dc84711d0ddf3b11c637de8
Tanner Heffner <tanner.heffner@agile6.com>
2023-12-28 14:31:21 -0800

    remove unneeded webpack mod

--- next.config.js
+++ next.config.js
@@ -10,24 +10,6 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   staticPageGenerationTimeout: 180, //arbitrary; 60 is default but it's too small
-
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve = {
-        ...config.resolve,
-        fallback: {
-          // redis can't be used client side
-          // see https://github.com/getsentry/sentry-javascript/issues/6548
-          // note: this repo isn't using sentry but the issue explains the problem
-          net: false,
-          dns: false,
-          tls: false,
-          assert: false,
-          fs: false,
-        },
-      }
-    }
-    return config
-  },
 }
+
 module.exports = nextConfig
