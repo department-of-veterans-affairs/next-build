437939ab18dbe34ad6895e79e450c633bc1a217b
Ryan Koch <6863534+ryguyk@users.noreply.github.com>
2024-01-12 12:31:47 -0600

    VACMS-16638: Static-Data-File Generation (KISS) (#335)
    Co-authored-by: Tanner Heffner <tannerjheffner@gmail.com>

--- src/lib/constants/pageSizes.ts
+++ src/lib/constants/pageSizes.ts
@@ -1,9 +1,6 @@
-import {
-  RESOURCE_TYPES,
-  ADDITIONAL_RESOURCE_TYPES,
-} from '@/lib/constants/resourceTypes'
+import { RESOURCE_TYPES } from '@/lib/constants/resourceTypes'
 
 export const PAGE_SIZES = {
   [RESOURCE_TYPES.STORY_LISTING]: 10,
-  [ADDITIONAL_RESOURCE_TYPES.STATIC_PATHS]: 50, //must be <= 50 due to JSON:API limit
+  MAX: 50, //50 is JSON:API limit. Use this for fetching as many as possible at a time.
 } as const
