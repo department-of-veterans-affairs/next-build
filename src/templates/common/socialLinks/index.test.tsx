ac77c6036e533c0360001322d3daec4d54c55a36
jtmst <josh.troy.mills@gmail.com>
2024-01-18 10:57:49 -0500

    Social Links Refactor

--- src/templates/common/socialLinks/index.test.tsx
+++ src/templates/common/socialLinks/index.test.tsx
@@ -1,33 +1,49 @@
 import { render, screen } from '@testing-library/react'
 import { SocialLinks } from '@/templates/common/socialLinks/index'
 
-const socialLinks = {
+const socialLinksProps = {
   title: 'We honor outstanding doctors',
   path: '/pittsburgh-health-care/stories/we-honor-outstanding-doctors',
 }
 
-test('<SocialLinks> component renders', () => {
-  const { container } = render(<SocialLinks {...socialLinks} />)
+test('<SocialLinks> component renders standard links', () => {
+  render(<SocialLinks {...socialLinksProps} />)
 
-  const aEl = container.querySelectorAll('a')
-  const iEl = container.querySelectorAll('i')
-  expect(aEl[0]).toHaveAttribute(
+  expect(screen.getByText(/Share on Facebook/)).toHaveAttribute(
     'href',
     'https://www.facebook.com/sharer/sharer.php?href=/pittsburgh-health-care/stories/we-honor-outstanding-doctors'
   )
-  expect(iEl[0]).toHaveAttribute(
-    'class',
-    'va-c-social-icon fab fa-facebook vads-u-margin-right--0p5'
-  )
-  expect(screen.queryByText(/Share on Facebook/)).toBeInTheDocument()
-
-  expect(aEl[1]).toHaveAttribute(
+  expect(screen.getByText(/Share on Twitter/)).toHaveAttribute(
     'href',
     'https://twitter.com/intent/tweet?text=We honor outstanding doctors&url=/pittsburgh-health-care/stories/we-honor-outstanding-doctors'
   )
-  expect(iEl[1]).toHaveAttribute(
-    'class',
-    'va-c-social-icon fab fa-twitter vads-u-margin-right--0p5'
-  )
-  expect(screen.queryByText(/Share on Twitter/)).toBeInTheDocument()
+})
+
+test('<SocialLinks> component renders VBA links', () => {
+  const vbaProps = {
+    ...socialLinksProps,
+    regionNickname: 'Pittsburgh',
+    fieldNews: {
+      uri: 'https://news-link.com',
+      title: 'VBA News',
+    },
+  }
+
+  render(<SocialLinks {...vbaProps} />)
+  const vbaLink = screen.getByText('VBA News').closest('a')
+  expect(vbaLink).toHaveAttribute('href', 'https://news-link.com')
+})
+
+test('<SocialLinks> renders correct links based on props', () => {
+  const eventProps = {
+    ...socialLinksProps,
+    description: 'Event Description',
+    address: 'Event Address',
+    dateObject: { endValue: 123, value: 456 },
+  }
+
+  render(<SocialLinks {...eventProps} />)
+
+  // Check for event-specific link
+  expect(screen.getByText(/Add to Calendar/)).toBeInTheDocument()
 })
