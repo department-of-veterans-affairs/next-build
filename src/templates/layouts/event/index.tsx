4473b23f0f9a1c681a90ef3cac7242645627d116
jtmst <josh.troy.mills@gmail.com>
2024-01-19 12:22:37 -0500

    fix for event effect error

--- src/templates/layouts/event/index.tsx
+++ src/templates/layouts/event/index.tsx
@@ -46,7 +46,7 @@ export const Event = ({
     // Calculate the most recent date when the component mounts
     const recentDate = deriveMostRecentDate(formattedDates)
     setMostRecentDate(recentDate)
-  }, [formattedDates])
+  }, [datetimeRange])
 
   const toggleRecurringEvents = () => {
     setShowRecurringEvents((prevState) => !prevState)
