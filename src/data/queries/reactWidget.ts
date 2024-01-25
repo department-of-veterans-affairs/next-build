c166dd600a9f972b113646201904fac28ece7bbc
Ryan Koch <6863534+ryguyk@users.noreply.github.com>
2024-01-25 15:02:03 -0600

    VACMS-8636: Adds component for rendering react-widget paragraph. (#359)

---
+++ src/data/queries/reactWidget.ts
@@ -0,0 +1,23 @@
+import { ParagraphReactWidget } from '@/types/drupal/paragraph'
+import { QueryFormatter } from 'next-drupal-query'
+import { ReactWidget } from '@/types/formatted/reactWidget'
+
+export const formatter: QueryFormatter<ParagraphReactWidget, ReactWidget> = (
+  entity: ParagraphReactWidget
+) => {
+  return {
+    entityId: entity.drupal_internal__id,
+    widgetType: entity.field_widget_type,
+    ctaWidget: entity.field_cta_widget,
+    loadingMessage: entity.field_loading_message,
+    timeout: entity.field_timeout,
+    errorMessage: entity.field_error_message.processed,
+    defaultLink: entity.field_default_link
+      ? {
+          url: entity.field_default_link.url,
+          title: entity.field_default_link.title,
+        }
+      : null,
+    buttonFormat: entity.field_button_format,
+  }
+}
---
+++ src/templates/components/reactWidget/index.tsx
@@ -0,0 +1,127 @@
+import { ReactWidget as FormattedReactWidget } from '@/types/formatted/reactWidget'
+
+const CtaWidget = ({ entityId, widgetType }) => {
+  return (
+    <div
+      data-template="paragraphs/react_widget"
+      data-entity-id={entityId}
+      className="cta-widget"
+      data-widget-type="cta"
+      data-app-id={widgetType}
+    ></div>
+  )
+}
+
+const LoadingIndicator = ({ loadingMessage, slowLoadingMessage }) => {
+  return (
+    <div className="loading-indicator-container">
+      <div
+        aria-label={loadingMessage}
+        aria-valuetext="Loading your application..."
+        className="loading-indicator"
+        role="progressbar"
+      ></div>
+      <span className="loading-indicator-message loading-indicator-message--normal">
+        {loadingMessage}
+      </span>
+      <span
+        className="loading-indicator-message loading-indicator-message--slow vads-u-display--none"
+        aria-hidden="true"
+      >
+        {slowLoadingMessage}
+      </span>
+    </div>
+  )
+}
+
+const StaticWidgetContent = ({ defaultLink, buttonFormat }) => {
+  return (
+    <span
+      className="static-widget-content vads-u-display--none"
+      aria-hidden="true"
+    >
+      {defaultLink && (
+        <a
+          href={defaultLink.url}
+          className={buttonFormat ? 'usa-button-primary va-button-primary' : ''}
+        >
+          {defaultLink.title}
+        </a>
+      )}
+    </span>
+  )
+}
+
+const ErrorMessage = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+
+  return (
+    <div
+      className="usa-alert usa-alert-error sip-application-error vads-u-display--none"
+      aria-hidden="true"
+    >
+      <div className="usa-alert-body">{errorMessage}</div>
+    </div>
+  )
+}
+
+const DefaultWidget = ({
+  entityId,
+  widgetType,
+  timeout,
+  loadingMessage,
+  slowLoadingMessage,
+  errorMessage,
+  defaultLink,
+  buttonFormat,
+}) => {
+  return (
+    <div
+      data-template="paragraphs/react_widget"
+      data-entity-id={entityId}
+      data-widget-type={widgetType}
+      data-widget-timeout={timeout}
+    >
+      <LoadingIndicator
+        loadingMessage={loadingMessage}
+        slowLoadingMessage={slowLoadingMessage}
+      />
+      <StaticWidgetContent
+        defaultLink={defaultLink}
+        buttonFormat={buttonFormat}
+      />
+      <ErrorMessage errorMessage={errorMessage} />
+    </div>
+  )
+}
+
+export const ReactWidget = ({
+  entityId,
+  ctaWidget = false,
+  widgetType,
+  loadingMessage = 'Loading...',
+  slowLoadingMessage = 'Sorry, this is taking longer than expected.',
+  timeout = 20,
+  errorMessage,
+  defaultLink,
+  buttonFormat,
+}: FormattedReactWidget) => {
+  if (ctaWidget) {
+    return <CtaWidget entityId={entityId} widgetType={widgetType} />
+  } else {
+    return (
+      <DefaultWidget
+        entityId={entityId}
+        widgetType={widgetType}
+        loadingMessage={loadingMessage}
+        slowLoadingMessage={slowLoadingMessage}
+        timeout={timeout}
+        errorMessage={errorMessage}
+        defaultLink={defaultLink}
+        buttonFormat={buttonFormat}
+      />
+    )
+  }
+}
