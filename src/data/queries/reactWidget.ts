eb2e1a137f9cf6b9d11b8e869711c4789bc3de54
Ryan Koch <ryan.koch.0213@gmail.com>
2024-01-25 14:37:08 -0600

    Removes unnecessary optional chaining.

--- src/data/queries/reactWidget.ts
+++ src/data/queries/reactWidget.ts
@@ -6,18 +6,18 @@ export const formatter: QueryFormatter<ParagraphReactWidget, ReactWidget> = (
   entity: ParagraphReactWidget
 ) => {
   return {
-    entityId: entity?.drupal_internal__id,
-    ctaWidget: entity?.field_cta_widget,
-    widgetType: entity?.field_widget_type,
-    loadingMessage: entity?.field_loading_message,
-    timeout: entity?.field_timeout,
-    errorMessage: entity?.field_error_message.processed,
+    entityId: entity.drupal_internal__id,
+    ctaWidget: entity.field_cta_widget,
+    widgetType: entity.field_widget_type,
+    loadingMessage: entity.field_loading_message,
+    timeout: entity.field_timeout,
+    errorMessage: entity.field_error_message.processed,
     defaultLink: entity.field_default_link
       ? {
           url: entity.field_default_link.url,
           title: entity.field_default_link.title,
         }
       : null,
-    buttonFormat: entity?.field_button_format,
+    buttonFormat: entity.field_button_format,
   }
 }
