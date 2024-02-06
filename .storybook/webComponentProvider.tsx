615da5505652150cb7e9f2cd8c124af8cb69f427
Tanner Heffner <tanner.heffner@agile6.com>
2024-02-06 15:17:20 -0800

    fix vets-website header footer in storybook

--- .storybook/webComponentProvider.tsx
+++ .storybook/webComponentProvider.tsx
@@ -1,17 +1,19 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { defineCustomElements } from '@department-of-veterans-affairs/web-components/loader'
 
 const WebComponentProvider = ({ children }) => {
+  const [loaded, setLoaded] = useState(false)
   useEffect(() => {
     defineCustomElements()
+
+    // load vets-website assets for storybook components
+    const scriptTag = document.createElement('script')
+    scriptTag.src = '/generated/static-pages.entry.js'
+    scriptTag.addEventListener('load', () => setLoaded(true))
+    document.body.appendChild(scriptTag)
   }, [])
 
-  return (
-    <>
-      {children}
-      <script src="/generated/static-pages.entry.js"></script>
-    </>
-  )
+  return <>{children}</>
 }
 
 export default WebComponentProvider
