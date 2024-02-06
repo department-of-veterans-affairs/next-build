67493b8ed126493d2d239c3746dc2c07b85e4de0
Jeff Schmidt <jeff.schmidt@civicactions.com>
2024-02-06 14:18:09 -0600

    trying to get vets-website assets working

--- docker-run.sh
+++ docker-run.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-# Shell script invoked by next-build docker container (see docker/Dockerfile).
-# Env vars must be present to populate cmd params.
-# Secrets (remaining required params) must be present at ./envs/.env.${ENV}.
-
-APP_ENV=${ENV} yarn build:preview && yarn start \
-    --SITE_URL ${SITE_URL} \
-    --NEXT_PUBLIC_DRUPAL_BASE_URL ${NEXT_PUBLIC_DRUPAL_BASE_URL} \
-    --NEXT_IMAGE_DOMAIN ${NEXT_IMAGE_DOMAIN} \
-    --REDIS_URL ${REDIS_URL} \
+APP_ENV=$APP_ENV yarn build:preview && yarn start \
+    --SITE_URL $SITE_URL \
+    --NEXT_PUBLIC_DRUPAL_BASE_URL $NEXT_PUBLIC_DRUPAL_BASE_URL \
+    --NEXT_IMAGE_DOMAIN $NEXT_IMAGE_DOMAIN \
+    --REDIS_URL $REDIS_URL \
     --NEXT_PUBLIC_ASSETS_URL=./public/generated/
