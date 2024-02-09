5806503d07828dab8209a742b43a8b89e4898c8c
Jeff Schmidt <95797077+jschmidt-civicactions@users.noreply.github.com>
2024-02-09 15:37:35 -0600

    Initial pass at Dockerfile and supporting files (#370)

---
+++ docker-run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+APP_ENV=$APP_ENV yarn build:preview && yarn start \
+    --SITE_URL $SITE_URL \
+    --NEXT_PUBLIC_DRUPAL_BASE_URL $NEXT_PUBLIC_DRUPAL_BASE_URL \
+    --NEXT_IMAGE_DOMAIN $NEXT_IMAGE_DOMAIN \
+    --REDIS_URL $REDIS_URL \
+    --NEXT_PUBLIC_ASSETS_URL=/generated/
