498bc025eb769e6938e0db8eaf546408a2ba2cca
Tim Cosgrove <timcosgrove@users.noreply.github.com>
2024-02-12 12:10:30 -0800

    Broken link check workflow (#355)

--- scripts/check-broken-links.mjs
+++ scripts/check-broken-links.mjs
@@ -3,16 +3,58 @@ import { LinkChecker } from 'linkinator'
 import chalk from 'chalk'
 import {
   getSitemapLocations,
-  splitPagesIntoSegments,
+  splitPagesIntoBatches,
+  getPagesSlice,
 } from '../playwright/utils/getSitemapLocations.js'
 import fs from 'fs'
 
 const OPTIONS = {
   sitemapUrl: process.env.SITE_URL || 'http://www.va.gov/',
-  batchSize: process.env.BATCH_SIZE || 32,
+  // totalInstances is the number of instances to split work among.
+  totalInstances: process.env.TOTAL_INSTANCES || 1,
+  // instanceNumber is the specifc instance currently running the check.
+  // In this and totalInstances case, the defaults will check the entire set.
+  instanceNumber: process.env.INSTANCE_NUMBER || 1,
+  // batchSize is the number of pararllel link check processes to run.
+  batchSize: process.env.BATCH_SIZE || 20,
   verbose: process.env.VERBOSE || false,
-  skipImageLinks: process.env.SKIP_IMAGES || false,
 }
+
+// List of patterns to skip checking. Most of these are to avoid false positives
+// from servers that disallow bot traffic like ours.
+// These can be regular expressions; no quotes around them in that case.
+const LINKS_TO_SKIP = [
+  'www.googletagmanager.com',
+  'dap.digitalgov.gov/Universal-Federated-Analytics-Min.js',
+  'resource.digital.voice.va.gov',
+  'www.choicehotels.com',
+  'microsoft.com',
+  'redroof.com',
+  'motel6.com',
+  'vetcenter.va.gov',
+  /visn\d+.*?\.va\.gov/,
+  'instagram.com',
+  'caregiver.va.gov',
+  'desertpacific.va.gov',
+  'ptsd.va.gov',
+  'exclusions.oig.hhs.gov',
+  'patientportal.myhealth.va.gov',
+  'prod-va-gov-assets.s3-us-gov-west-1.amazonaws.com',
+  's3-us-gov-west-1.amazonaws.com/content.www.va.gov',
+  'va-ams.intelliworxit.com',
+  'epilepsy.va.gov',
+  'sciencedirect.com',
+  'twitter.com',
+  'volunteer.va.gov',
+  'womenshealth.va.gov',
+  'oefoif.va.gov',
+  'sci.va.gov',
+  'southeast.va.gov',
+  'va.gov/Geriatrics',
+  'va.gov/wholehealth',
+  'warrelatedillness.va.gov',
+  // process.env.SKIP_IMAGES ? '' : null
+]
 
 // Map of states and colors to use when logging link results.
 const LOGGER_MAP = {
@@ -25,16 +67,14 @@ const LOGGER_MAP = {
 // See: https://github.com/JustinBeckwith/linkinator?tab=readme-ov-file#linkinatorcheckoptions
 const LINKCHECKER_CONFIG = {
   // Links in this array will not be checked. Will report as SKIPPED.
-  linksToSkip: [
-    'https://www.googletagmanager.com/',
-    'https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js',
-    // process.env.SKIP_IMAGES ? '' : null
-  ],
-  timeout: 5000, // Fail a link if it doesn't resolve within 5s, otherwise linkinator will hang until it resolves.
+  linksToSkip: LINKS_TO_SKIP,
+  timeout: 10000, // Fail a link if it doesn't resolve, otherwise linkinator will hang until it resolves.
   urlRewriteExpressions: [
     // { pattern: '', replacement: '' }
   ],
   // recurse: true, // not recursing through links that are checked because we scan the full known sitemap
+  retryErrors: true,
+  retryErrorsCount: 3,
 }
 
 /**
@@ -50,7 +90,7 @@ const LINKCHECKER_CONFIG = {
  *
  * This will output dot notation to the terminal for each link as it is checked, plus
  * a list of broken links. A JSON file with the run's metrics is also output to
- * <repo root>/broken-link-report.json
+ * <repo root>/broken-links-report.json
  */
 async function checkBrokenLinks() {
   const start = Date.now()
@@ -73,16 +113,26 @@ async function checkBrokenLinks() {
 
       linksChecked.push(result)
 
-      if (result.state === 'BROKEN') {
+      // 403 errors returned to the crawler by and large work for web users.
+      if (result.state === 'BROKEN' && result.status !== 403) {
         brokenLinks.push(result)
       }
     })
+    .on('retry', (retryDetails) => {
+      console.log(
+        `Retrying ${retryDetails.url} ; status: ${retryDetails.status}`
+      )
+    })
 
   // Full array of sitemap defined URLs.
-  const paths = await getSitemapLocations(OPTIONS.sitemapUrl)
-  // Tiny array of paths for debugging this script.
-  // const paths = (await getSitemapLocations(OPTIONS.sitemapUrl)).slice(0, 100)
+  const allPaths = await getSitemapLocations(OPTIONS.sitemapUrl)
+  const paths = getPagesSlice(
+    allPaths,
+    OPTIONS.totalInstances,
+    OPTIONS.instanceNumber
+  )
   console.log(`Number of pages to check: ${chalk.yellow(paths.length)}`)
+  const initialPathCount = paths.length
 
   // Wow! That's probably a lot of pages. Split it into batches for efficiency.
   console.log(
@@ -90,25 +140,55 @@ async function checkBrokenLinks() {
       OPTIONS.batchSize
     )} batches for processing.`
   )
-  const batches = splitPagesIntoSegments(paths, OPTIONS.batchSize)
-
+  const batches = splitPagesIntoBatches(paths, OPTIONS.batchSize)
   // A fake counter for the illusion of sequential completion.
   let counter = 1
+
+  // Use this setTimeout loop to keep the event loop alive.
+  //
+  // During testing, we saw that the program would sometimes exit after the
+  // batch runs without executing the code below them. This was a 'clean' exit,
+  // which for Node happens when the event loop empties out.
+  //
+  // If we could figure out why the batch runs sometimes empty the event loop
+  // and thus trigger exit, this keep-alive loop wouldn't be necessary.
+  // @TODO this probably needs a second time-based condition, though GHA timeout
+  // can also prevent this from sitting forever.
+  let batchesComplete = false
+  function checkAndLoop() {
+    if (batchesComplete === true) {
+      console.log('Batches complete, exiting the loop.')
+    } else {
+      setTimeout(checkAndLoop, 5000)
+    }
+  }
+  checkAndLoop()
+
   // Request each batch at once. This takes a little bit of time depending on the size
   // of the sitemap. VA.gov builds a large one.
-  await Promise.all(
-    batches.map(async (batch) => {
-      for (const path of batch) {
-        // Where the actual link check happens, uses options defined above
-        await checker.check({ ...LINKCHECKER_CONFIG, path })
-      }
-      console.log(
-        chalk.yellow(`\n Batch #${counter} of ${OPTIONS.batchSize} complete.`)
-      )
-      counter++
-    })
-  )
-
+  try {
+    await Promise.all(
+      batches.map(async (batch) => {
+        // truncate the batch for testing purposes
+        for (const path of batch) {
+          // Where the actual link check happens, uses options defined above
+          //if (showLogs) console.log(`Batch ${index}: checking ${path}`)
+          try {
+            await checker.check({ ...LINKCHECKER_CONFIG, path })
+          } catch (error) {
+            console.log('check error: ', error)
+          }
+        }
+        console.log(
+          chalk.yellow(`\n Batch #${counter} of ${OPTIONS.batchSize} complete.`)
+        )
+        counter++
+      })
+    )
+  } catch (error) {
+    console.log(`Checking failed: `, error)
+  }
+  batchesComplete = true
   const end = Date.now()
 
   const time = new Date(end - start)
@@ -118,7 +198,7 @@ async function checkBrokenLinks() {
   // How many links did we scan?
   console.log(`\n All batches complete in ${chalk.yellow(formattedTime)}!`)
   console.log(
-    `Scanned total of ${chalk.yellow(
+    `Of an initial ${initialPathCount} pages, scanned total of ${chalk.yellow(
       linksChecked.length
     )} links on ${chalk.yellow(pagesChecked.length)} pages!`
   )
@@ -131,43 +211,52 @@ async function checkBrokenLinks() {
   const jsonReport = {
     metrics: {
       domain: OPTIONS.sitemapUrl,
+      initialPathCount: initialPathCount,
       pagesScanned: pagesChecked.length,
       linksChecked: linksChecked.length,
       brokenLinkCount: brokenLinks.length,
       time: formattedTime,
     },
-    brokenLinks: [],
+    brokenLinksByParent: {},
+    brokenLinksByLink: {},
   }
 
   if (brokenLinks.length > 0) {
     for (const brokenLink of brokenLinks) {
       const { url, status, parent } = brokenLink
 
-      // Output which links are broken on what pages.
-      console.log(`\n${chalk.red(url)}`)
-      console.log('  ', 'STATUS:', status)
-      console.log('  ', 'SOURCE:', parent)
-
-      // Trim item to just essentials for JSON output.
-      jsonReport.brokenLinks.push({
-        url,
-        status,
-        parent,
-      })
+      // Group broken links by parent.
+      if (parent !== undefined) {
+        jsonReport.brokenLinksByParent[parent] =
+          jsonReport.brokenLinksByParent[parent] || []
+        jsonReport.brokenLinksByParent[parent].push({
+          url,
+          status,
+        })
+      }
+      // Group broken links by link.
+      if (url !== undefined) {
+        jsonReport.brokenLinksByLink[url] =
+          jsonReport.brokenLinksByLink[url] || []
+        jsonReport.brokenLinksByLink[url].push({
+          parent,
+          status,
+        })
+      }
     }
   }
 
   // Write finished report to file.
   const json = JSON.stringify(jsonReport)
-  fs.writeFile('broken-link-report.json', json, (err) => {
+  fs.writeFile('broken-links-report.json', json, (err) => {
     if (err) {
       console.error(err)
     }
   })
 
-  return console.log(
+  console.log(
     `\n Report file written to: ${chalk.green(
-      process.cwd() + '/broken-link-report.json'
+      process.cwd() + '/broken-links-report.json'
     )}`
   )
 }
---
+++ scripts/create-combined-reports.mjs
@@ -0,0 +1,133 @@
+/* eslint-disable no-console */
+import fs from 'fs'
+import chalk from 'chalk'
+
+const createCombinedReports = () => {
+  // List any report files.
+  let combinedJson = {
+    metrics: {
+      initialPathCount: 0,
+      pagesScanned: 0,
+      linksChecked: 0,
+      brokenLinkCount: 0,
+    },
+    brokenLinksByParent: {},
+    brokenLinksByLink: {},
+  }
+  const files = fs
+    .readdirSync('.')
+    .filter((filename) => filename.match(/^broken-links-report-run.*?\.json/))
+  for (let i = 0; i < files.length; i++) {
+    const fileJson = JSON.parse(fs.readFileSync(files[i], 'utf8'))
+    // Add together the numbers. We drop the non-numeric metrics here.
+    for (const metric in fileJson.metrics) {
+      if (typeof fileJson.metrics[metric] == 'number') {
+        combinedJson.metrics[metric] += fileJson.metrics[metric]
+      }
+    }
+    // Iterate over source pages and then destination, making sure to not lose
+    // any data in the combination.
+    for (const parentLink in fileJson.brokenLinksByParent) {
+      if (!combinedJson.brokenLinksByParent[parentLink]) {
+        combinedJson.brokenLinksByParent[parentLink] = []
+      }
+      combinedJson.brokenLinksByParent[parentLink] =
+        combinedJson.brokenLinksByParent[parentLink].concat(
+          fileJson.brokenLinksByParent[parentLink]
+        )
+    }
+    for (const parentLink in fileJson.brokenLinksByLink) {
+      if (!combinedJson.brokenLinksByLink[parentLink]) {
+        combinedJson.brokenLinksByLink[parentLink] = []
+      }
+      combinedJson.brokenLinksByLink[parentLink] =
+        combinedJson.brokenLinksByLink[parentLink].concat(
+          fileJson.brokenLinksByLink[parentLink]
+        )
+    }
+  }
+  // Write finished report to file.
+  fs.writeFile(
+    'broken-links-report-combined.json',
+    JSON.stringify(combinedJson),
+    (err) => {
+      if (err) {
+        console.error(err)
+      }
+    }
+  )
+
+  console.log(
+    `\n Combined report file written to: ${chalk.green(
+      process.cwd() + '/broken-links-report-combined.json'
+    )}`
+  )
+
+  let markDownReport = ''
+
+  // Output a markdown report for easy readability.
+  markDownReport += `# VA.gov broken link report\n`
+  markDownReport += `Found ${combinedJson.metrics.brokenLinkCount} broken links on ${combinedJson.metrics.pagesScanned} pages.\n`
+  const dateTime = new Date().toString()
+  markDownReport += `Report generated: ${dateTime}\n\n`
+
+  // First group by source page
+  markDownReport += `## Broken links grouped by source page\n`
+  for (const parent of Object.keys(combinedJson.brokenLinksByParent)) {
+    markDownReport += `Source: ${parent}\n`
+    for (const child of combinedJson.brokenLinksByParent[parent]) {
+      markDownReport += `- ${child.url}, response code ${child.status}\n`
+    }
+    markDownReport += `\n`
+  }
+  markDownReport += `\n`
+
+  // Group by broken link.
+  markDownReport += `## Broken links grouped by destination\n`
+  markDownReport += `Each broken link and all the pages it appears on.\n\n`
+  for (const child of Object.keys(combinedJson.brokenLinksByLink)) {
+    markDownReport += `Broken destination: ${child}\n`
+    for (const parent of combinedJson.brokenLinksByLink[child]) {
+      markDownReport += `- ${parent.parent}\n`
+    }
+    markDownReport += `\n`
+  }
+
+  // Write markdown report to file
+  fs.writeFile('broken-links-report.md', markDownReport, (err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+
+  console.log(
+    `\n Report Markdown file written to: ${chalk.green(
+      process.cwd() + '/broken-links-report.md'
+    )}`
+  )
+
+  // Generate a CSV report
+  let csvReport = `Parent Link, Child Link, Error Code\n`
+  for (const parent of Object.keys(combinedJson.brokenLinksByParent)) {
+    const sanitizedParent = parent.replace(/,/g, ',,')
+    for (const child of combinedJson.brokenLinksByParent[parent]) {
+      const sanitizedChildUrl = child.url.replace(/,/g, ',,')
+      csvReport += `${sanitizedParent},${sanitizedChildUrl},${child.status}\n`
+    }
+  }
+
+  // Write markdown report to file
+  fs.writeFile('broken-links-report.csv', csvReport, (err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+
+  console.log(
+    `\n Report CSV file written to: ${chalk.green(
+      process.cwd() + '/broken-links-report.csv'
+    )}`
+  )
+}
+
+createCombinedReports()
