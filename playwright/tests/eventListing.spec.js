18a384e8ca4b672fd2ea1d56c44f9746ac72a0ee
Tanner Heffner <tanner.heffner@agile6.com>
2024-02-20 13:02:09 -0800

    update event listing playwright test

--- playwright/tests/eventListing.spec.js
+++ playwright/tests/eventListing.spec.js
@@ -4,9 +4,21 @@ test.describe('eventListing', () => {
   test('Event Listing page renders with events that can be navigated to', async ({
     page,
   }) => {
-    await page.goto('/butler-health-care/events')
+    await page.goto('/outreach-and-events/events/')
     await page.locator('.events a').first().click()
-    await expect(page).toHaveURL(/\/butler-health-care\/events\//)
+    await expect(page).toHaveURL(/\/outreach-and-events\/events\//)
+  })
+
+  test('Event Listing widget can switch between event types', async ({
+    page,
+  }) => {
+    await page.goto('/outreach-and-events/events/')
+    await page.getByLabel('Filter by').selectOption('past')
+    await page.getByLabel('Filter by').selectOption('upcoming')
+    await page.getByLabel('Filter by').selectOption('specific-date')
+
+    const specificDateLabel = page.getByLabel('Please tell us a date ')
+    await expect(specificDateLabel).toBeVisible()
   })
 
   test('Should render without a11y errors', async ({
