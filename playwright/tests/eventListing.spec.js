8e5403123bc80f1ad6e8c7160c5ac884d0241d5a
Tanner Heffner <tanner.heffner@agile6.com>
2024-02-23 09:51:09 -0800

    jk:

--- playwright/tests/eventListing.spec.js
+++ playwright/tests/eventListing.spec.js
@@ -4,15 +4,15 @@ test.describe('eventListing', () => {
   test('Event Listing page renders with events that can be navigated to', async ({
     page,
   }) => {
-    await page.goto('/butler-health-care/events/')
+    await page.goto('/outreach-and-events/events/')
     await page.locator('.events a').first().click()
-    await expect(page).toHaveURL(/\/butler-health-care\/events\//)
+    await expect(page).toHaveURL(/\/outreach-and-events\/events\//)
   })
 
   test('Event Listing widget changes form fields based on selection', async ({
     page,
   }) => {
-    await page.goto('/butler-health-care/events/')
+    await page.goto('/outreach-and-events/events/')
     await page.getByLabel('Filter by').selectOption('specific-date')
 
     const specificMonth = page.getByLabel(
