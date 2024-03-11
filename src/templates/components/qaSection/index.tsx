691d41941aadadd4b18b4c7a989c9037539e567c
jtmst <josh.troy.mills@gmail.com>
2024-03-11 18:50:13 -0400

    cleanup

--- src/templates/components/qaSection/index.tsx
+++ src/templates/components/qaSection/index.tsx
@@ -1,8 +1,8 @@
 import { QaSection as FormattedQaSection } from '@/types/formatted/qaSection'
 import { QaCollapsiblePanel } from '../qaCollapsiblePanel'
+import { QaParagraph as FormattedQaParagraph } from '@/types/formatted/qaParagraph'
 import { QaGroup as FormattedQaGroup } from '@/types/formatted/qaGroup'
 import { Paragraph } from '@/templates/components/paragraph'
-import { FormattedParagraph } from '@/data/queries'
 
 export function QaSection({
   header,
@@ -18,8 +18,12 @@ export function QaSection({
       {displayAccordion ? (
         <QaCollapsiblePanel questions={questions} />
       ) : (
-        questions.map((questionContent: FormattedParagraph, index) => (
-          <Paragraph key={questionContent?.id || index} {...questionContent} />
+        questions.map((questionContent: FormattedQaParagraph, index) => (
+          <Paragraph
+            key={questionContent?.id || index}
+            setHeaderh3={setHeaderh3}
+            {...questionContent}
+          />
         ))
       )}
     </div>
