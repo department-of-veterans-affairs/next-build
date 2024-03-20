679cbcb9484a893a4720cb32312fd0138fd78b0c
Tanner Heffner <tannerjheffner@gmail.com>
2024-03-20 08:20:11 -0700

    next-build content release workflow (#424)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.APP_ENV === 'prod'
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -11,6 +13,9 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   staticPageGenerationTimeout: 180, //arbitrary; 60 is default but it's too small
+  assetPrefix: isProd
+    ? 'https://s3.us-gov-west-1.amazonaws.com/next-content.www.va.gov/'
+    : undefined,
 }
 
 module.exports = nextConfig
