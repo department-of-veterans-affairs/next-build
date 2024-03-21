d429c1c85741a36ede347116622e024c00c0af28
Alex Finnarn <alex.finnarn@gmail.com>
2024-03-21 13:55:32 -0400

    try building proxy-fetcher

--- playwright/tests/a11y.spec.js
+++ playwright/tests/a11y.spec.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-console */
 import { test, expect } from '../utils/next-test'
-// import {
-//   getSitemapLocations,
-//   splitPagesIntoBatches,
-// } from '../utils/getSitemapLocations'
+import {
+  getSitemapLocations,
+  splitPagesIntoBatches,
+} from '../utils/getSitemapLocations'
 import AxeBuilder from '@axe-core/playwright'
-// import { getFetcher } from 'proxy-fetcher'
-import fetch from 'cross-fetch'
+// import fetch from 'cross-fetch'
 
 async function runA11yTestsForPages(pages, page, testInfo) {
   // let a11yFailures = []
@@ -98,7 +97,7 @@ test.describe(`Accessibility Tests`, async () => {
     const slim = pages.slice(0, 50) // for faster testing
     pageSegments = splitPagesIntoBatches(slim, BATCH_SIZE)
     const segment = process.env.SEGMENT_INDEX
-      ? pageSegments[process.env.SEGMENT_INDEX]
+      ? pageSegments[process.env.SEGMENT_INDEX - 1]
       : slim
 
     await runA11yTestsForPages(segment, page, testInfo)
@@ -106,49 +105,49 @@ test.describe(`Accessibility Tests`, async () => {
 })
 // }
 
-async function getSitemapLocations(baseUrl) {
-  // handle trailing slash
-  const base = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl
-  const mainSitemapUrl = `${base}/sitemap.xml`
-
-  const response = await fetch(mainSitemapUrl)
-
-  const xml = await response.text()
-  const locs = []
-
-  const urls = extractUrlsFromXML(xml)
-
-  for (const url of urls) {
-    // toplevel sitemap is an index of additional sitemaps
-    if (url.endsWith('.xml')) {
-      const response = await fetch(url)
-      const xml = await response.text()
-      const urls = extractUrlsFromXML(xml)
-      locs.push(urls)
-    } else {
-      locs.push(url)
-    }
-  }
-
-  return locs.flat()
-}
+// async function getSitemapLocations(baseUrl) {
+//   // handle trailing slash
+//   const base = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl
+//   const mainSitemapUrl = `${base}/sitemap.xml`
+//
+//   const response = await fetch(mainSitemapUrl)
+//
+//   const xml = await response.text()
+//   const locs = []
+//
+//   const urls = extractUrlsFromXML(xml)
+//
+//   for (const url of urls) {
+//     // toplevel sitemap is an index of additional sitemaps
+//     if (url.endsWith('.xml')) {
+//       const response = await fetch(url)
+//       const xml = await response.text()
+//       const urls = extractUrlsFromXML(xml)
+//       locs.push(urls)
+//     } else {
+//       locs.push(url)
+//     }
+//   }
+//
+//   return locs.flat()
+// }
 
 // VA.gov sitemaps have a lot of urls in them. Helper function for things that
 // may want to parallelize checking that list (broken links, a11y, etc.)
-function splitPagesIntoBatches(pages, batchCount) {
-  const batchSize = Math.ceil(pages.length / batchCount)
-  return new Array(Number(batchCount)).fill().map((_, index) => {
-    return pages.slice(index * batchSize, (index + 1) * batchSize)
-  })
-}
-
-function extractUrlsFromXML(xml) {
-  return [...xml.matchAll(new RegExp(`<loc>(.|\n)*?</loc>`, 'g'))].map(
-    ([loc]) => {
-      return loc
-        .replace('<loc>', '')
-        .replace('</loc>', '')
-        .replace(/^https:/, 'http:')
-    }
-  )
-}
+// function splitPagesIntoBatches(pages, batchCount) {
+//   const batchSize = Math.ceil(pages.length / batchCount)
+//   return new Array(Number(batchCount)).fill().map((_, index) => {
+//     return pages.slice(index * batchSize, (index + 1) * batchSize)
+//   })
+// }
+//
+// function extractUrlsFromXML(xml) {
+//   return [...xml.matchAll(new RegExp(`<loc>(.|\n)*?</loc>`, 'g'))].map(
+//     ([loc]) => {
+//       return loc
+//         .replace('<loc>', '')
+//         .replace('</loc>', '')
+//         .replace(/^https:/, 'http:')
+//     }
+//   )
+// }
