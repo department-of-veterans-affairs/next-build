25d465034308d2c38c675c91f8bc5191b07b5b1c
Tanner Heffner <tanner.heffner@agile6.com>
2024-03-28 15:57:01 -0700

    simple expects

--- playwright/tests/a11y.spec.js
+++ playwright/tests/a11y.spec.js
@@ -10,6 +10,7 @@ async function runA11yTestsForPages(pages, page, testInfo) {
   for (const pageUrl of pages) {
     await page.goto(pageUrl)
     console.log('testing page:', pageUrl)
+
     // @todo The shared "makeAxeBuilder" never reports errors for whatever reason so not using for now.
     // const accessibilityScanResults = await makeAxeBuilder({ page }).analyze()
     const accessibilityScanResults = await new AxeBuilder({ page })
@@ -19,11 +20,10 @@ async function runA11yTestsForPages(pages, page, testInfo) {
       // .exclude('footer')
       .analyze()
 
-    console.log('page violations:', accessibilityScanResults.violations)
+    // dummy expect so page has time to analyze, perhaps
+    await expect(page).toHaveURL(pageUrl)
 
-    // @todo Likely remove the check since we are testing many URLs and showing
-    // a segment fails does not help much.
-    expect(accessibilityScanResults.violations).toEqual([])
+    console.log('page violations:', accessibilityScanResults.violations)
 
     if (accessibilityScanResults.violations.length > 0) {
       const scanResults = {
@@ -44,6 +44,8 @@ async function runA11yTestsForPages(pages, page, testInfo) {
 
     fs.writeFileSync(`segment-${segmentNumber}.json`, trimmedScanResultsArray)
   }
+
+  return scanResultsArray
 }
 
 const totalSegments = process.env.TOTAL_SEGMENTS
@@ -81,7 +83,8 @@ test.describe(`Accessibility Tests`, async () => {
 
     console.log('number of pages in segment', segment.length)
 
-    await runA11yTestsForPages(segment, page, testInfo)
+    const results = await runA11yTestsForPages(segment, page, testInfo)
+    expect(results).toEqual([])
   })
 })
 
