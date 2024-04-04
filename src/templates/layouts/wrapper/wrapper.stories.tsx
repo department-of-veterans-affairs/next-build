02550a1fee0c08089a3d0854d69e05c5a3d0dae4
Tanner Heffner <tanner.heffner@agile6.com>
2024-04-04 16:07:30 -0700

    update storybook docs

--- src/templates/layouts/wrapper/wrapper.stories.tsx
+++ src/templates/layouts/wrapper/wrapper.stories.tsx
@@ -1,6 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { Wrapper } from '@/templates/layouts/wrapper'
-// import WebComponentProvider from '@/stories/webComponentProvider'
 import headerFooterMock from '@/mocks/headerFooter.mock.json'
 
 const meta: Meta<typeof Wrapper> = {
@@ -22,9 +21,6 @@ export const Example: Story = {
   parameters: {
     layout: 'fullscreen',
   },
-  // decorators: [
-  //   (Story) => <WebComponentProvider>{Story()}</WebComponentProvider>,
-  // ],
   render: () => (
     <Wrapper {...props}>
       <div style={{ height: '600px' }}></div>
