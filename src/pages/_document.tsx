be6e0ac3523c8e3a0c2aed8dcc80d54330926a0a
Tanner Heffner <tannerjheffner@gmail.com>
2024-04-08 20:59:08 -0700

    Fix styles (#488)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,10 +10,6 @@ const Document = () => {
       <Head>
         <meta charSet="utf-8" />
         <link
-          rel="stylesheet"
-          href="https://unpkg.com/@department-of-veterans-affairs/formation/dist/formation.min.css"
-        />
-        <link
           href={`${ASSETS_URL}static-pages.css`}
           data-entry-name="static-pages.css"
           rel="stylesheet"
