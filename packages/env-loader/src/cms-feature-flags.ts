508da90c70860fca77fa294578b6afafb4330bd5
nfpappas-oddball <npappas@oddball>
2024-06-07 11:31:58 -0400

    make actions checkout from my branch

--- packages/env-loader/src/cms-feature-flags.ts
+++ packages/env-loader/src/cms-feature-flags.ts
@@ -1,16 +1,15 @@
 import { getFetcher } from 'proxy-fetcher'
 import { EnvVars } from '.'
 
-// const removeTrailingSlash = (s: string): string =>
-//   s.endsWith('/') ? s.substring(0, s.length - 1) : s
+const removeTrailingSlash = (s: string): string =>
+  s.endsWith('/') ? s.substring(0, s.length - 1) : s
 
 export const getCmsFeatureFlags = async (
   drupalBaseUrl: string,
   debug: boolean = true
 ): Promise<EnvVars> => {
   const fetcher = getFetcher(drupalBaseUrl, debug)
-  //const featureFlagUrl = `${removeTrailingSlash(drupalBaseUrl)}/flags_list`
-  const featureFlagUrl = `${drupalBaseUrl}/flags_list`
+  const featureFlagUrl = `${removeTrailingSlash(drupalBaseUrl)}/flags_list`
   const response = await fetcher(featureFlagUrl)
   const json = await response.json()
   return json.data
