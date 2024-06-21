39a9e87518dc070ca845f79f3ce28e566c05b64b
Therisa Tran <146353301+ttran-Therisa@users.noreply.github.com>
2024-06-21 09:10:34 -0400

    PR: Debug Press Releases Listings (#556)

---
+++ src/templates/layouts/pressReleaseListing/pressReleaseListing.stories.ts
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { PressReleaseListing } from './index'
+import data from '@/mocks/pressReleaseListing.mock.json'
+import { formattedPressReleases } from '@/mocks/formattedPressReleases.mock'
+
+const meta: Meta<typeof PressReleaseListing> = {
+  title: 'Layouts/Press Release Listing',
+  component: PressReleaseListing,
+}
+export default meta
+
+type Story = StoryObj<typeof PressReleaseListing>
+
+export const NoPressReleases: Story = {
+  args: {
+    ...data,
+  },
+}
+
+export const List: Story = {
+  args: {
+    ...data,
+    releases: formattedPressReleases,
+  },
+}
