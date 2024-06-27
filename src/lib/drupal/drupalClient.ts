ff58f4d7b0df8c1c4f531d30a19708b53b49a43a
Jake Bapple <166155028+JakeBapple@users.noreply.github.com>
2024-06-27 09:31:48 -0500

    VANB-18071: Update response headers.
    Adding additional response headers.
    
    Co-authored-by: Tim Cosgrove <timcosgrove@users.noreply.github.com>

--- src/lib/drupal/drupalClient.ts
+++ src/lib/drupal/drupalClient.ts
@@ -23,6 +23,8 @@ export const drupalClient = new DrupalClient(baseUrl, {
   previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
   // Add request header to tell the CMS to return public-facing URLs for files.
   headers: {
-    'File-Public-Base-Url-Check': 'true',
+    "Content-Type": "application/vnd.api+json", 
+    Accept: "application/vnd.api+json",
+    "File-Public-Base-Url-Check": "true",
   },
 })
