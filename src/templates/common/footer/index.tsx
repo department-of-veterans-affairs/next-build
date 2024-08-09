68dfca92f9b71a2ac71228a7e70d5e15ad838d96
Therisa Tran <therisatran@gmail.com>
2024-08-09 15:51:07 -0400

    Removed wrapping section contentInfo and div announcement-root

--- src/templates/common/footer/index.tsx
+++ src/templates/common/footer/index.tsx
@@ -1,14 +1,11 @@
 export const Footer = () => {
   return (
-    <section role="contentinfo">
-      <div id="announcement-root"></div>
-      <footer className="footer">
-        <div
-          id="footerNav"
-          data-testid="footer"
-          data-minimal-footer="false"
-        ></div>
-      </footer>
-    </section>
+    <footer className="footer">
+      <div
+        id="footerNav"
+        data-testid="footer"
+        data-minimal-footer="false"
+      ></div>
+    </footer>
   )
 }
