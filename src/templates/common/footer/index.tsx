e048528ef57b37b39aaa207b1cabb16714c4f970
Therisa Tran <therisatran@gmail.com>
2024-08-12 13:09:32 -0400

    Installed yarn add react-html-comment to mimic the html commenting and added div to match with prod footer

--- src/templates/common/footer/index.tsx
+++ src/templates/common/footer/index.tsx
@@ -1,11 +1,21 @@
+import HTMLComment from 'react-html-comment'
+
 export const Footer = () => {
   return (
-    <footer className="footer">
-      <div
-        id="footerNav"
-        data-testid="footer"
-        data-minimal-footer="false"
-      ></div>
-    </footer>
+    <>
+      <footer className="footer">
+        <div
+          id="footerNav"
+          data-testid="footer"
+          data-minimal-footer="false"
+        ></div>
+      </footer>
+      <div id="logout-modal-root"></div>
+      <HTMLComment
+        text="
+      VA's mission: To fulfill President Lincoln's promise to care for those who have served in our nation's military and for their families, caregivers, and survivors.
+      "
+      />
+    </>
   )
 }
