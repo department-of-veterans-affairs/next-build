4a1b8bb440932a34095995a565fe21d9e127c37f
mreed101 <mreed@maphabit.com>
2024-08-25 22:28:14 -0400

    turn on console logs

--- src/lib/drupal/getGlobalElements.ts
+++ src/lib/drupal/getGlobalElements.ts
@@ -10,6 +10,8 @@ export async function getGlobalElements(
   // This query is cached so header and footer menu data is only directly requested once per build.
   const headerFooterData = await queries.getData('header-footer-data')
 
+  console.log('header footer data was fetched')
+
   // Banners can be fetched as long as we have context and a path (slug).
   if (!headerOnly && itemPath) {
     // Gather data for banners currently visible on this page.
@@ -17,11 +19,15 @@ export async function getGlobalElements(
       itemPath,
     })
 
+    console.log('return header footer, header only is false', itemPath)
+
     return {
       bannerData,
       headerFooterData,
     }
   }
+
+  console.log('return empty banner data, header is true')
 
   // Otherwise return header data without banners.
   return {
