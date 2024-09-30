72ed2f627e7fa2ab3c34a12a0e186c95980f219c
Tim Cosgrove <tcosgrove@metrostar.com>
2024-09-30 11:45:14 -0700

    More debugging.

--- packages/env-loader/src/env-file.ts
+++ packages/env-loader/src/env-file.ts
@@ -15,18 +15,15 @@ const loadEnvVarsFromPath = (path: string): EnvVars => {
 
   // eslint-disable-next-line no-console
   console.log(`Using environment variables from: ${path}`)
-
+  // eslint-disable-next-line no-console
+  console.log(envVars)
   return envVars
 }
 
 export const getEnvFileVars = (appEnv?: string): EnvVars => {
   //  Load env vars from `.env.${process.env.APP_ENV}` if it exists,
   //  otherwise load env vars from `.env.local` if it exists.
-  // eslint-disable-next-line no-console
-  console.log(`appEnv is ${appEnv}`)
   if (appEnv && fs.existsSync(`envs/.env.${appEnv}`)) {
-    // eslint-disable-next-line no-console
-    console.log(`we are trying to read envs/.env.${appEnv}`)
     return loadEnvVarsFromPath(`envs/.env.${appEnv}`)
   } else if (fs.existsSync('envs/.env.local')) {
     return loadEnvVarsFromPath(`envs/.env.local`)
