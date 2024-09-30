4adc884b7eefec32415299dfe56c07ee990cad9d
Tim Cosgrove <timcosgrove@users.noreply.github.com>
2024-09-30 11:06:35 -0700

    Tcosgrove debug prod env vars (#758)

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+ls -al envs
 APP_ENV=$APP_ENV yarn build:preview && yarn start
--- packages/env-loader/src/env-file.ts
+++ packages/env-loader/src/env-file.ts
@@ -22,7 +22,11 @@ const loadEnvVarsFromPath = (path: string): EnvVars => {
 export const getEnvFileVars = (appEnv?: string): EnvVars => {
   //  Load env vars from `.env.${process.env.APP_ENV}` if it exists,
   //  otherwise load env vars from `.env.local` if it exists.
+  // eslint-disable-next-line no-console
+  console.log(`appEnv is ${appEnv}`)
   if (appEnv && fs.existsSync(`envs/.env.${appEnv}`)) {
+    // eslint-disable-next-line no-console
+    console.log(`we are trying to read envs/.env.${appEnv}`)
     return loadEnvVarsFromPath(`envs/.env.${appEnv}`)
   } else if (fs.existsSync('envs/.env.local')) {
     return loadEnvVarsFromPath(`envs/.env.local`)
