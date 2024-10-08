6fe5522c744c71ecc764fe4df62decc2e623561a
Randi Mays <rmays@kindsys.us>
2024-10-08 15:47:44 -0500

    Adding secondary button group component for CTAs

--- src/templates/common/secondaryButtonGroup/index.tsx
+++ src/templates/common/secondaryButtonGroup/index.tsx
@@ -4,12 +4,11 @@ import { Button as FormattedButton } from '@/types/formatted/button'
 export const SecondaryButtonGroup = ({
   buttons
 }: FormattedButton[]): JSX.Element => {
-  console.log('buttons: ', buttons);
   if (buttons?.length > 1) {
     return (
-      <ul class="vads-u-margin-y--3 usa-unstyled-list">
-        {buttons?.map(button => (
-            <li class="vads-u-margin-bottom--2">
+      <ul className="vads-u-margin-y--3 usa-unstyled-list">
+        {buttons?.map((button, index) => (
+            <li key={index} className="vads-u-margin-bottom--2">
               <va-link-action
                 href={button.url}
                 text={button.label}
---
+++ src/templates/common/secondaryButtonGroup/secondaryButtonGroup.stories.ts
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { SecondaryButtonGroup } from './index'
+
+const meta: Meta<typeof SecondaryButtonGroup> = {
+  title: 'Components/Secondary Button Group',
+  component: SecondaryButtonGroup,
+}
+export default meta
+
+type Story = StoryObj<typeof SecondaryButtonGroup>
+
+export const Example: Story = {}
