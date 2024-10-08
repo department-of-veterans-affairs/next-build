b4974d26fa3210ee3c48dc892c26d45e4d2661c8
Randi Mays <rmays@kindsys.us>
2024-10-08 14:20:43 -0500

    Removed unused Link custom component; added secondaryButtonGroup for R&S pages

--- src/templates/layouts/checklist/index.tsx
+++ src/templates/layouts/checklist/index.tsx
@@ -1,9 +1,31 @@
 import { Checklist } from '@/types/formatted/checklist'
+import AlertSingle from '@/templates/components/alertSingle'
+import { SecondaryButtonGroup } from '@/templates/common/SecondaryButtonGroup'
 
-export function Checklist({ title }: Checklist) {
+export function Checklist({
+  alert,
+  buttons,
+  intro,
+  title
+}: Checklist) {
   return (
-    <div>
-      <p>{title}</p>
-    </div>
+    <main className="va-l-detail-page">
+      <div className="usa-grid usa-grid-full">
+        <div className="usa-width-three-fourths">
+          <div className="usa-content">
+            <div data-widget-type="i18-select"></div>
+            <div className="medium-screen:vads-u-border-bottom--2px vads-u-border-color--gray-light medium-screen:vads-u-margin-bottom--3">
+              <div className="va-hide-on-print-view" data-widget-type="resources-and-support-search" />
+            </div>
+            <article className="vads-u-padding-x--1 large-screen:vads-u-padding-x--0">
+              <h1>{title}</h1>
+              <div className="va-introtext" dangerouslySetInnerHTML={{ __html: intro }} />
+              {alert && <AlertSingle {...alert} />}
+              {buttons && <SecondaryButtonGroup buttons={buttons} />}
+            </article>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
