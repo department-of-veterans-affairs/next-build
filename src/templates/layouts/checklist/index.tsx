54606dd459629cb2ab68f64c69b158a8ba29d47e
Randi Mays <rmays@kindsys.us>
2024-10-09 11:47:20 -0500

    Add optional intro

--- src/templates/layouts/checklist/index.tsx
+++ src/templates/layouts/checklist/index.tsx
@@ -13,13 +13,13 @@ export function Checklist({
       <div className="usa-grid usa-grid-full">
         <div className="usa-width-three-fourths">
           <div className="usa-content">
-            <div data-widget-type="i18-select"></div>
+            <div data-widget-type="i18-select" />
             <div className="medium-screen:vads-u-border-bottom--2px vads-u-border-color--gray-light medium-screen:vads-u-margin-bottom--3">
               <div className="va-hide-on-print-view" data-widget-type="resources-and-support-search" />
             </div>
             <article className="vads-u-padding-x--1 large-screen:vads-u-padding-x--0">
               <h1>{title}</h1>
-              <div className="va-introtext" dangerouslySetInnerHTML={{ __html: intro }} />
+              {intro && <div className="va-introtext" dangerouslySetInnerHTML={{ __html: intro }} />}
               {alert && <AlertSingle {...alert} />}
               {buttons && <SecondaryButtonGroup buttons={buttons} />}
             </article>
