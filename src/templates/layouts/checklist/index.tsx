dac3b6caba61c73dc1f5d7c05ea650c8c10978ed
Randi Mays <rmays@kindsys.us>
2024-10-10 14:23:22 -0500

    Adding more content for checklist page

--- src/templates/layouts/checklist/index.tsx
+++ src/templates/layouts/checklist/index.tsx
@@ -1,5 +1,7 @@
+import { Fragment } from 'react';
 import { Checklist } from '@/types/formatted/checklist'
 import AlertSingle from '@/templates/components/alertSingle'
+import { AudienceTopics } from '@/templates/components/audienceTopics'
 import { SecondaryButtonGroup } from '@/templates/common/SecondaryButtonGroup'
 
 export function Checklist({
@@ -7,9 +9,11 @@ export function Checklist({
   buttons,
   checklist,
   intro,
+  repeatButtons,
+  tags,
   title
 }: Checklist) {
-  console.log('checklist: ', checklist[0]);
+  console.log('tags: ', tags);
   return (
     <main className="va-l-detail-page">
       <div className="usa-grid usa-grid-full">
@@ -24,14 +28,31 @@ export function Checklist({
               {intro && <div className="va-introtext" dangerouslySetInnerHTML={{ __html: intro }} />}
               {alert && <AlertSingle {...alert} />}
               {buttons && <SecondaryButtonGroup buttons={buttons} />}
-              {checklist?.length > 0 && (
-                checklist.map(list =>
-                  <>
-                    {list.header && <h2>{list.header}</h2>}
+              {checklist?.length && (
+                checklist.map((list, index) =>
+                  <Fragment key={index}>
+                    {list.header && <h2 className="vads-u-margin-bottom--4">{list.header}</h2>}
                     {list.intro && <p>{list.intro}</p>}
-                  </>
+                    {list.items && list.items?.length && (
+                      <ul className="usa-unstyled-list">
+                        {list.items.map((item, index) => (
+                          <li
+                            className={
+                              index < list.items.length - 1 ?
+                              `vads-u-margin-bottom--4` :
+                            ''}
+                            key={index}
+                          >
+                            <va-checkbox label={item} />
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </Fragment>
                 )
               )}
+              {repeatButtons && buttons && <SecondaryButtonGroup buttons={buttons} />}
+              {tags && <AudienceTopics tags={tags} />}
             </article>
           </div>
         </div>
