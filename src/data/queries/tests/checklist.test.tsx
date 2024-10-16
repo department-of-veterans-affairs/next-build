d5b89bb5c6d5a470aa0320ca143a7a8b64074a3f
Randi Mays <19175324+randimays@users.noreply.github.com>
2024-10-16 15:57:58 -0500

    Adding mock and tests for data query

--- src/data/queries/tests/checklist.test.tsx
+++ src/data/queries/tests/checklist.test.tsx
@@ -1,25 +1,42 @@
+/**
+ * @jest-environment node
+ */
+
 import { Checklist } from '@/types/drupal/node'
 import { queries } from '@/data/queries'
 import mockData from '@/mocks/checklist.mock.json'
+import { params } from '../checklist';
 
 const ChecklistMock: Checklist = mockData
 
+describe('DrupalJsonApiParams configuration', () => {
+  test('params function sets the correct include fields', () => {
+    const paramsInstance = params()
+    const queryString = decodeURIComponent(paramsInstance.getQueryString())
+
+    const expectedIncludes = [
+      'field_alert_single',
+      'field_buttons',
+      'field_checklist.field_checklist_sections',
+      'field_contact_information',
+      'field_related_benefit_hubs',
+      'field_related_information',
+      'field_tags'
+    ]
+
+    expectedIncludes.forEach((include) => {
+      expect(queryString).toMatch(new RegExp(`include=.*${include}`))
+    })
+  })
+})
+
 describe('Checklist formatData', () => {
-  let windowSpy
-
-  beforeEach(() => {
-    windowSpy = jest.spyOn(window, 'window', 'get')
-  })
-
-  afterEach(() => {
-    windowSpy.mockRestore()
-  })
-
   test('outputs formatted data', () => {
-    windowSpy.mockImplementation(() => undefined)
-
     expect(
-      queries.formatData('node--checklist', ChecklistMock)
+      queries.formatData(
+        'node--checklist',
+        ChecklistMock
+      )
     ).toMatchSnapshot()
   })
 })
