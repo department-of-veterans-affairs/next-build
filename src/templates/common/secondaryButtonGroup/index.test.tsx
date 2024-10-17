7cf319a057978a9741a8afbe203fd4e24ff44472
Randi Mays <19175324+randimays@users.noreply.github.com>
2024-10-17 12:21:01 -0500

    Update unit tests

--- src/templates/common/secondaryButtonGroup/index.test.tsx
+++ src/templates/common/secondaryButtonGroup/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { SecondaryButtonGroup } from './'
 
 describe('SecondaryButtonGroup Component', () => {
