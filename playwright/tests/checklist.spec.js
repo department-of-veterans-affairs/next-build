252457b420e288af176ddffe76bccaddefc9b589
Randi Mays <19175324+randimays@users.noreply.github.com>
2024-10-18 10:13:04 -0500

    Adding playwright test

--- playwright/tests/checklist.spec.js
+++ playwright/tests/checklist.spec.js
@@ -4,15 +4,15 @@ test.describe('Checklist', () => {
   test('Checklist page renders', async ({
     page,
   }) => {
-    await page.goto('/update-this-link')
-    await expect(page).toHaveURL('/update-this-link')
+    await page.goto('/resources/what-to-bring-to-create-your-online-sign-in-account')
+    await expect(page).toHaveURL('/resources/what-to-bring-to-create-your-online-sign-in-account')
   })
 
   test('Should render without a11y errors', async ({
     page,
     makeAxeBuilder,
   }) => {
-    await page.goto('/update-this-link')
+    await page.goto('/resources/what-to-bring-to-create-your-online-sign-in-account')
 
     const accessibilityScanResults = await makeAxeBuilder().analyze()
 
