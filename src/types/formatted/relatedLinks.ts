282d44a9b4c3a4e393a36976cc31e6bce77621b6
Randi Mays <19175324+randimays@users.noreply.github.com>
2024-10-25 16:29:02 -0500

    Refactor related information component to be shared

--- src/types/formatted/relatedLinks.ts
+++ src/types/formatted/relatedLinks.ts
@@ -1,7 +1,12 @@
 export type RelatedLink = {
   title: string
+<<<<<<< HEAD
   uri: string
   summary?: string
+=======
+  url: string
+  summary: string
+>>>>>>> d3dcb96c (Refactor related information component to be shared)
 }
 
 export type FormattedRelatedLinks = {
