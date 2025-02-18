57cf8478837a818b8cdc19b6a95ac50b6afaa21a
Brian Seek <brian.seek@civicactions.com>
2025-02-18 15:07:09 -0800

    updated GoogleMapsDirections test

--- src/templates/common/googleMapsDirections/index.test.tsx
+++ src/templates/common/googleMapsDirections/index.test.tsx
@@ -3,13 +3,14 @@ import { GoogleMapsDirections } from './'
 
 describe('GoogleMapsDirections Component', () => {
   test('renders anchor element with correct href attribute', () => {
-    const title = 'Target Location'
+    const location = 'Target Location'
     const address = '123 Main St, City, Country'
     const expectedUrl = `https://maps.google.com?saddr=Current+Location&daddr=${encodeURIComponent(
       address
     )}`
+    const expectedAriaLabel = `Get directions on Google Maps to ${location}`
 
-    render(<GoogleMapsDirections title={title} address={address} />)
+    render(<GoogleMapsDirections location={location} address={address} />)
 
     //leaving old elemet selector just in case we need to re-use it
     // const anchorElement = screen.getByRole('va-link', {
@@ -20,5 +21,6 @@ describe('GoogleMapsDirections Component', () => {
     expect(anchorElement).toBeInTheDocument()
 
     expect(anchorElement).toHaveAttribute('href', expectedUrl)
+    expect(anchorElement).toHaveAttribute('label', expectedAriaLabel)
   })
 })
