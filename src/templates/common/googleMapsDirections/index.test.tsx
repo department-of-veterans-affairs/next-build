8c691aa54d539ee42ca6e01792ab3a884472b000
Brian Seek <brian.seek@civicactions.com>
2025-02-19 12:02:11 -0800

    VACMS-19689 -9674 location updates (#891)

--- src/templates/common/googleMapsDirections/index.test.tsx
+++ src/templates/common/googleMapsDirections/index.test.tsx
@@ -2,14 +2,15 @@ import { render, screen } from '@testing-library/react'
 import { GoogleMapsDirections } from './'
 
 describe('GoogleMapsDirections Component', () => {
-  test('renders anchor element with correct href attribute', () => {
-    const title = 'Target Location'
+  test('renders anchor element with correct href attribute and label', () => {
+    const location = 'Target Location'
     const address = '123 Main St, City, Country'
     const expectedUrl = `https://maps.google.com?saddr=Current+Location&daddr=${encodeURIComponent(
       address
     )}`
+    const expectedAriaLabel = `Get directions on Google Maps to ${location}`
 
-    render(<GoogleMapsDirections title={title} address={address} />)
+    render(<GoogleMapsDirections location={location} address={address} />)
 
     //leaving old elemet selector just in case we need to re-use it
     // const anchorElement = screen.getByRole('va-link', {
@@ -20,5 +21,15 @@ describe('GoogleMapsDirections Component', () => {
     expect(anchorElement).toBeInTheDocument()
 
     expect(anchorElement).toHaveAttribute('href', expectedUrl)
+    expect(anchorElement).toHaveAttribute('label', expectedAriaLabel)
+  })
+  test('excludes label from render if none given', () => {
+    const address = '123 Main St, City, Country'
+
+    render(<GoogleMapsDirections address={address} />)
+
+    const anchorElement = screen.getByTestId('maps-directions')
+    expect(anchorElement).toBeInTheDocument()
+    expect(anchorElement).not.toHaveAttribute('label')
   })
 })
