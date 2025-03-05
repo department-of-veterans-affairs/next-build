5f92fd273dd046ef1023452aad988f0e936104fb
Chris Valarida <chris@themostudio.com>
2025-03-05 15:42:59 -0800

    Add some data and a lot of placeholders to the page
    We'll break these placeholders out into individual components later.

--- src/templates/layouts/healthCareLocalFacility/index.tsx
+++ src/templates/layouts/healthCareLocalFacility/index.tsx
@@ -2,10 +2,72 @@ import { HealthCareLocalFacility as FormattedHealthCareLocalFacility } from '@/t
 
 export function HealthCareLocalFacility({
   title,
-}: HealthCareLocalFacilityProps) {
+  introText,
+}: FormattedHealthCareLocalFacility) {
   return (
-    <div>
-      <p>{title}</p>
+    <div className="interior" id="content">
+      <main className="va-l-detail-page va-facility-page">
+        <div className="usa-grid usa-grid-full">
+          <div>TODO: Sidebar nav</div>
+          <div className="usa-width-three-fourths">
+            <article className="usa-content va-l-facility-detail">
+              <div>TODO: Lovell switch link</div>
+
+              {title && <h1>{title}</h1>}
+
+              {introText && (
+                <div className="va-introtext">
+                  <p>{introText}</p>
+                </div>
+              )}
+
+              <div className="usa-grid usa-grid-full vads-u-margin-y--1p5 vads-u-margin-bottom--6">
+                <div>TODO: facilities_health_services_buttons</div>
+              </div>
+
+              <va-on-this-page></va-on-this-page>
+
+              {/* Main content */}
+              <h2 className="vads-u-line-height--1 vads-u-margin-bottom--3">
+                Location and contact information
+              </h2>
+              <div className="region-list usa-grid usa-grid-full vads-u-display--flex vads-u-flex-direction--column small-screen:vads-u-flex-direction--row facility vads-u-margin-bottom--2p5 vads-u-margin-bottom--4">
+                <div className="usa-width-two-thirds vads-u-display--block vads-u-width--full">
+                  <div>
+                    <div className="vads-c-facility-detail">
+                      <div>TODO: Operating status flags</div>
+                      <section className="vads-facility-detail">
+                        <script type="application/ld+json">
+                          {/* TODO: Fill this in */}
+                        </script>
+
+                        <h3 className="vads-u-font-size--lg vads-u-margin-top--0 vads-u-line-height--1 vads-u-margin-bottom--1">
+                          Address
+                        </h3>
+                        <div>TODO: Address stuff</div>
+
+                        <h3 className="vads-u-font-size--lg vads-u-margin-top--0 vads-u-line-height--1 vads-u-margin-bottom--1">
+                          Phone numbers
+                        </h3>
+                        <div>TODO: Phone numbers</div>
+                        <div>TODO: Office hours</div>
+                      </section>
+                    </div>
+                  </div>
+                </div>
+                <div>TODO: Image and static map</div>
+              </div>
+              <div>TODO: Location services section</div>
+              <div>TODO: List of links section</div>
+              <div>TODO: Local health services section</div>
+              <div>TODO: Patient satisfaction scores section</div>
+              <div>TODO: Social links section</div>
+              <va-back-to-top></va-back-to-top>
+              <div>TODO: Last updated & feedback button</div>
+            </article>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
