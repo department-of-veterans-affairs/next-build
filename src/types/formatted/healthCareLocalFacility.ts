658227ae67ff01374f7b2638110090f2e3763fc9
Chris Valarida <chris@themostudio.com>
2025-03-05 15:53:00 -0800

    It's not actually optional, but nullable

--- src/types/formatted/healthCareLocalFacility.ts
+++ src/types/formatted/healthCareLocalFacility.ts
@@ -2,5 +2,5 @@ import { PublishedEntity } from './publishedEntity'
 
 export type HealthCareLocalFacility = PublishedEntity & {
   // Other attributes here
-  introText?: string
+  introText: string | null
 }
