7cfe9d4d331e6fbf2d384f7b7d707f7dca65427a
Chris Valarida <chris@themostudio.com>
2025-03-05 15:43:20 -0800

    Update test

--- src/templates/layouts/healthCareLocalFacility/index.test.tsx
+++ src/templates/layouts/healthCareLocalFacility/index.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react'
 import { HealthCareLocalFacility } from './index'
+import { HealthCareLocalFacility as FormattedHealthCareLocalFacility } from '@/types/formatted/healthCareLocalFacility'
+
+const mockData: FormattedHealthCareLocalFacility = {
+  id: '123',
+  title: 'Test facility',
+  type: 'node--health_care_local_facility',
+  published: true,
+  lastUpdated: '',
+}
 
 describe('HealthCareLocalFacility with valid data', () => {
   test('renders HealthCareLocalFacility component', () => {
-    render(<HealthCareLocalFacility title={'Hello world'} />)
+    render(<HealthCareLocalFacility {...mockData} />)
 
-    expect(screen.queryByText(/Hello world/)).toBeInTheDocument()
+    expect(screen.queryByText(/Test facility/)).toBeInTheDocument()
   })
 })
