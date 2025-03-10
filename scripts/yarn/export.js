2932e6f0232afe4c3a2d16f4ff6a78b4743c58d8
Chris Valarida <chris@themostudio.com>
2025-03-10 16:50:13 -0700

    Stinking es modules...
    It's a love-hate relationship

--- scripts/yarn/export.js
+++ scripts/yarn/export.js
@@ -1,6 +1,10 @@
 import { processEnv } from 'env-loader'
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const exportBuildPath = path.resolve(__dirname, '../../.next/export')
 if (fs.existsSync(exportBuildPath)) {
