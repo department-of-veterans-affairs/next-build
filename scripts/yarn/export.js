c5598b93e101310391f87bf99e64c564519e039b
Chris Valarida <chris@themostudio.com>
2025-03-10 16:47:19 -0700

    Yes, yes, yes, import path then!

--- scripts/yarn/export.js
+++ scripts/yarn/export.js
@@ -1,5 +1,6 @@
 import { processEnv } from 'env-loader'
 import fs from 'fs'
+import path from 'path'
 
 const exportBuildPath = path.resolve(__dirname, '../../.next/export')
 if (fs.existsSync(exportBuildPath)) {
