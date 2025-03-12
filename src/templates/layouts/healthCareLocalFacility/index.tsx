3181135ee667b21ab32c67ec3a0d5a7923d9f1bd
Chris Valarida <chris@themostudio.com>
2025-03-12 15:39:13 -0700

    Remove extra div and class

--- src/templates/layouts/healthCareLocalFacility/index.tsx
+++ src/templates/layouts/healthCareLocalFacility/index.tsx
@@ -34,25 +34,23 @@ export function HealthCareLocalFacility({
             <div className="region-list usa-grid usa-grid-full vads-u-display--flex vads-u-flex-direction--column small-screen:vads-u-flex-direction--row facility vads-u-margin-bottom--4">
               <div className="usa-width-two-thirds">
                 <div>
-                  <div className="vads-c-facility-detail">
-                    <div>TODO: Operating status flags</div>
-                    <section className="vads-facility-detail">
-                      <script type="application/ld+json">
-                        {/* TODO: Fill this in */}
-                      </script>
+                  <div>TODO: Operating status flags</div>
+                  <section>
+                    <script type="application/ld+json">
+                      {/* TODO: Fill this in */}
+                    </script>
 
-                      <h3 className="vads-u-margin-top--0 vads-u-margin-bottom--1">
-                        Address
-                      </h3>
-                      <div>TODO: Address stuff</div>
+                    <h3 className="vads-u-margin-top--0 vads-u-margin-bottom--1">
+                      Address
+                    </h3>
+                    <div>TODO: Address stuff</div>
 
-                      <h3 className="vads-u-margin-top--0 vads-u-margin-bottom--1">
-                        Phone numbers
-                      </h3>
-                      <div>TODO: Phone numbers</div>
-                      <div>TODO: Office hours</div>
-                    </section>
-                  </div>
+                    <h3 className="vads-u-margin-top--0 vads-u-margin-bottom--1">
+                      Phone numbers
+                    </h3>
+                    <div>TODO: Phone numbers</div>
+                    <div>TODO: Office hours</div>
+                  </section>
                 </div>
               </div>
               <div>TODO: Image and static map</div>
