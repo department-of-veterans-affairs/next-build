54636b255f42430ecf1988463343854690e914ae
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2025-03-24 14:01:50 -0700

    simplify - does self closing work in next-build

--- src/templates/layouts/vamcSystemVaPolice/index.tsx
+++ src/templates/layouts/vamcSystemVaPolice/index.tsx
@@ -20,15 +20,13 @@ export function VamcSystemVaPolice({
       path={entityPath || path?.alias || window.location.pathname}
     >
       <h1>{title}</h1>
-
       <WysiwygField
         className="va-introtext vads-u-font-size--lg vads-u-font-family--serif"
         html={field_cc_va_police_overview.html}
       />
-      <va-on-this-page></va-on-this-page>
+      <va-on-this-page />
       <SummaryBox {...field_cc_police_report} />
       <VaPoliceContactInfo />
-      {/* This work was already done, just the data processing that transformed the CC FAQ to regular hadn't been */}
       <CCQaSection {...field_cc_faq} />
     </VamcSystemFacilityPage>
   )
