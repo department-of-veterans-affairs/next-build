77a2a68119d64f4261c84160edbde08edfd26b79
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-03-25 12:42:32 -0500

    Add person profile teaser tests

--- src/templates/components/personProfileTeaser/index.test.tsx
+++ src/templates/components/personProfileTeaser/index.test.tsx
@@ -1,1 +1,84 @@
-// todo
+import { render } from '@testing-library/react'
+import PersonProfileTeaser from '.'
+
+describe('PersonProfileTeaser with valid data', () => {
+  test('correctly renders the component for someone with a profile page', () => {
+    const validData = {
+      completeBiographyCreate: true,
+      description: 'Chief Experience Officer',
+      emailAddress: 'brent-kratky@sample.com',
+      entityPath: '/houston-health-care/staff-profiles/brent-kratky',
+      firstName: 'Brent',
+      lastName: 'Kratky',
+      media: {
+        id: 'c9711539-f785-4b8a-b897-f1537c92dad2',
+        type: 'media--image',
+        links: {
+          '2_3_medium_thumbnail': {
+            href: 'https://dsva-vagov-staging-cms-files.s3.us-gov-west-1.amazonaws.com/styles/2_3_medium_thumbnail/public/2022-04/Brent%20Kratky%20VA%20Headshot%20Jan%202021.jpg',
+          },
+        },
+        alt: 'Brent Kratky',
+        width: 1048,
+        height: 1584,
+        title: ''
+      },
+      office: 'VA Houston health care',
+      phoneNumber: {
+        label: 'Phone',
+        extension: '12345',
+        number: '000-859-5743',
+        phoneType: 'tel',
+      }
+    }
+
+    const screen = render(<PersonProfileTeaser {...validData} />)
+
+    expect(screen.container.innerHTML).toContain('https%3A%2F%2Fdsva-vagov-staging-cms-files.s3.us-gov-west-1.amazonaws.com%2Fstyles%2F2_3_medium_thumbnail%2Fpublic%2F2022-04%2FBrent%2520Kratky%2520VA%2520Headshot%2520Jan%25202021.jpg')
+    expect(screen.container.innerHTML).toContain('text=\"Brent Kratky\"')
+    expect(screen.getByTestId('pp-teaser-desc')).toHaveTextContent('Chief Experience Officer')
+    expect(screen.getByTestId('pp-teaser-office')).toHaveTextContent('VA Houston health care')
+    expect(screen.getByTestId('pp-teaser-email')).toHaveTextContent('Email:')
+    expect(screen.container.innerHTML).toContain('text=\"brent-kratky@sample.com\"')
+  })
+
+  test('correctly renders the component for someone without a profile page', () => {
+    const validData = {
+      completeBiographyCreate: false,
+      description: 'Executive Director',
+      emailAddress: 'julianne-flynn@sample.com',
+      entityPath: '/south-texas-health-care/staff-profiles/julianne-flynn',
+      firstName: 'Julianne',
+      lastName: 'Flynn',
+      media: {
+        id: 'c9711539-f785-4b8a-b897-f1537c92dad2',
+        type: 'media--image',
+        links: {
+          '2_3_medium_thumbnail': {
+            href: 'https://dsva-vagov-staging-cms-files.s3.us-gov-west-1.amazonaws.com/styles/2_3_medium_thumbnail/public/2022-04/Julianne%20Flynn%20VA%20Headshot%20Jan%202021.jpg',
+          },
+        },
+        alt: 'Julianne Flynn',
+        width: 1048,
+        height: 1584,
+        title: ''
+      },
+      office: 'VA South Texas health care',
+      phoneNumber: {
+        label: 'Phone',
+        extension: '12345',
+        number: '000-859-5743',
+        phoneType: 'tel',
+      }
+    }
+
+    const screen = render(<PersonProfileTeaser {...validData} />)
+
+    expect(screen.container.innerHTML).toContain('https%3A%2F%2Fdsva-vagov-staging-cms-files.s3.us-gov-west-1.amazonaws.com%2Fstyles%2F2_3_medium_thumbnail%2Fpublic%2F2022-04%2FJulianne%2520Flynn%2520VA%2520Headshot%2520Jan%25202021.jpg')
+    expect(screen.getByTestId('pp-teaser-name')).toHaveTextContent('Julianne Flynn')
+    expect(screen.getByTestId('pp-teaser-desc')).toHaveTextContent('Executive Director')
+    expect(screen.getByTestId('pp-teaser-office')).toHaveTextContent('VA South Texas health care')
+    expect(screen.getByTestId('pp-teaser-email')).toHaveTextContent('Email:')
+    expect(screen.container.innerHTML).toContain('text=\"julianne-flynn@sample.com\"')
+  })
+})
--- src/templates/components/personProfileTeaser/index.tsx
+++ src/templates/components/personProfileTeaser/index.tsx
@@ -3,20 +3,6 @@ import { PersonProfileTeaser as FormattedPersonProfileTeaser } from '@/types/for
 import { MediaImage } from '@/templates/common/mediaImage'
 import { PhoneNumber } from '@/templates/common/phoneNumber'
 
-// //     emailAddress: entity.field_email_address,
-// //     phoneNumber: entity?.field_telephone?.field_phone_number || null,
-// //     description: entity.field_description,
-// //     introText: entity.field_intro_text,
-// //     body: entity.field_body?.processed || null,
-// //     media: queries.formatData('media--image', entity.field_media),
-// //     completeBiography: entity?.field_complete_biography?.uri || null,
-// //     completeBiographyCreate: entity.field_complete_biography_create,
-// //     photoAllowHiresDownload: entity.field_photo_allow_hires_download,
-// //     vamcOfficalName:
-// //       entity?.field_office?.field_vamc_system_official_name || null,
-// //     office: entity.field_office,
-// //     menu: formattedMenu,
-
 const PersonProfileTeaser = ({
   completeBiographyCreate,
   description,
@@ -49,17 +35,17 @@ const PersonProfileTeaser = ({
           text={staffName}
         />
       ) :
-        <p className="vads-u-margin--0 vads-u-font-family--serif vads-u-font-weight--bold vads-u-font-size--lg">
+        <p data-testid="pp-teaser-name" className="vads-u-margin--0 vads-u-font-family--serif vads-u-font-weight--bold vads-u-font-size--lg">
           {staffName}
         </p>
       }
       {description && (
-        <p className="vads-u-margin--0 vads-u-margin-top--1 vads-u-font-family--serif vads-u-font-size--base medium-screen:vads-u-font-size--lg">
+        <p data-testid="pp-teaser-desc" className="vads-u-margin--0 vads-u-margin-top--1 vads-u-font-family--serif vads-u-font-size--base medium-screen:vads-u-font-size--lg">
           {description}
         </p>
       )}
       {office && (
-        <p className="vads-u-margin--0 vads-u-font-family--serif vads-u-font-size--base medium-screen:vads-u-font-size--lg">
+        <p data-testid="pp-teaser-office" className="vads-u-margin--0 vads-u-font-family--serif vads-u-font-size--base medium-screen:vads-u-font-size--lg">
           {office}
         </p>
       )}
@@ -67,8 +53,8 @@ const PersonProfileTeaser = ({
         <PhoneNumber className="vads-u-margin-top--1" {...phoneNumber} />
       )}
       {emailAddress && (
-        <p className="vads-u-margin--0 medium-screen:vads-u-font-size--lg">
-          <b>Email:</b>
+        <p data-testid="pp-teaser-email" className="vads-u-margin--0 medium-screen:vads-u-font-size--lg">
+          <b>Email:</b>{' '}
           <va-link type="email" href={`mailto:${emailAddress}`} text={emailAddress}></va-link>
         </p>
       )}
