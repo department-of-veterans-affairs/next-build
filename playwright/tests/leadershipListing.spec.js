fa5a32a8d1d2c631ada5ecf6d4132295e3cd64d2
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-03-25 10:25:23 -0500

    Update Playwright tests

--- playwright/tests/leadershipListing.spec.js
+++ playwright/tests/leadershipListing.spec.js
@@ -4,15 +4,15 @@ test.describe('leadershipListing', () => {
   test('leadershipListing page renders', async ({
     page,
   }) => {
-    await page.goto('/update-this-link')
-    await expect(page).toHaveURL('/update-this-link')
+    await page.goto('/houston-health-care/about-us/leadership/')
+    await expect(page).toHaveURL('/houston-health-care/about-us/leadership/')
   })
 
   test('Should render without a11y errors', async ({
     page,
     makeAxeBuilder,
   }) => {
-    await page.goto('/update-this-link')
+    await page.goto('/houston-health-care/about-us/leadership/')
 
     const accessibilityScanResults = await makeAxeBuilder().analyze()
 
