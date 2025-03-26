e331f12db37daa6dfb2bd0d3ec149d40ca94df65
Bryan Thompson <18094023+SnowboardTechie@users.noreply.github.com>
2025-03-26 08:12:39 -0700

    slight tweak to padding/margin in spotlight

--- src/templates/common/featuredContent/index.tsx
+++ src/templates/common/featuredContent/index.tsx
@@ -7,7 +7,7 @@ export function FeaturedContent({
   link,
 }: ParagraphComponent<FormattedFeaturedContent>) {
   return (
-    <div className="vads-u-border--1px featured-content-list-item vads-u-flex--fill vads-u-padding-y--1p5 vads-u-padding-x--1p5 vads-u-margin-bottom--0 medium-screen:vads-u-margin-bottom--2">
+    <div className="vads-u-border--1px featured-content-list-item vads-u-flex--fill vads-u-padding-bottom--2 vads-u-padding-x--1p5 vads-u-margin-y--0 medium-screen:vads-u-margin-bottom--2">
       {title && (
         <>
           <h3 className="vads-u-margin-bottom--2">{title}</h3>
