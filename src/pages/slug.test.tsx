35daad349b772a2e28993792cb89be7c56302a3f
Brian Seek <brian.seek@civicactions.com>
2025-03-26 11:03:21 -0700

    added test file for slug and tested DoNotPublishError

---
+++ src/pages/slug.test.tsx
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment node
+ */
+
+import { getStaticProps } from './[[...slug]]'
+import { getStaticPropsResource } from '@/lib/drupal/staticProps'
+import { DoNotPublishError } from '@/lib/drupal/query'
+
+jest.mock('@/lib/drupal/staticProps')
+jest.mock('@/lib/drupal/getGlobalElements')
+
+describe('[[...slug]].tsx', () => {
+  describe('getStaticProps', () => {
+    const mockContext = { params: { slug: ['test-path'] }, preview: false }
+    const mockResource = { published: true }
+
+    beforeEach(() => {
+      ;(getStaticPropsResource as jest.Mock).mockResolvedValue(mockResource)
+    })
+
+    it('handles DoNotPublish error', async () => {
+      ;(getStaticPropsResource as jest.Mock).mockRejectedValue(
+        new DoNotPublishError('Do not publish error')
+      )
+      const result = await getStaticProps(mockContext)
+      expect(result).toEqual({ notFound: true })
+    })
+  })
+})
