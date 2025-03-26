c8d22ef2b725ad1079e232fe1903a0a7491d830b
Bryan Thompson <18094023+SnowboardTechie@users.noreply.github.com>
2025-03-26 12:56:37 -0700

    bring in va-card and directly copy production styles

--- src/templates/common/featuredContent/index.tsx
+++ src/templates/common/featuredContent/index.tsx
@@ -7,10 +7,10 @@ export function FeaturedContent({
   link,
 }: ParagraphComponent<FormattedFeaturedContent>) {
   return (
-    <div className="vads-u-border--1px featured-content-list-item vads-u-flex--fill vads-u-padding-bottom--2 vads-u-padding-x--1p5 vads-u-margin-y--0 medium-screen:vads-u-margin-bottom--2">
+    <va-card class="vads-u-flex--fill vads-u-margin-bottom--2 vads-u-margin-right--0 medium-screen:vads-u-margin-x--0p5 spotlight-card hydrated">
       {title && (
         <>
-          <h3 className="vads-u-margin-bottom--2">{title}</h3>
+          <h3 className="vads-u-margin-top--0">{title}</h3>
         </>
       )}
       {description && (
@@ -21,13 +21,13 @@ export function FeaturedContent({
       )}
       {link && link.url && (
         <va-link
-          className="vads-u-display--block vads-u-padding-top--1"
+          class="hydrated"
           href={link.url}
           text={link.label}
           active={true}
           data-testid="featured-content-link"
         />
       )}
-    </div>
+    </va-card>
   )
 }
