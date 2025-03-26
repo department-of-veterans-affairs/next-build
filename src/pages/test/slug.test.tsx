bf68b0015f82fbb5d7f55265512efecbe64ead3e
Brian Seek <brian.seek@civicactions.com>
2025-03-26 13:02:54 -0700

    move to test folder

--- src/pages/test/slug.test.tsx
+++ src/pages/test/slug.test.tsx
@@ -2,21 +2,15 @@
  * @jest-environment node
  */
 
-import { getStaticProps } from './[[...slug]]'
-import { getStaticPropsResource } from '../lib/drupal/staticProps'
-import { DoNotPublishError } from '../lib/drupal/query'
+import { getStaticProps } from '../[[...slug]]'
+import { getStaticPropsResource } from '@/lib/drupal/staticProps'
+import { DoNotPublishError } from '@/lib/drupal/query'
 
 jest.mock('@/lib/drupal/staticProps')
-jest.mock('@/lib/drupal/getGlobalElements')
 
 describe('[[...slug]].tsx', () => {
   describe('getStaticProps', () => {
     const mockContext = { params: { slug: ['test-path'] }, preview: false }
-    const mockResource = { published: true }
-
-    beforeEach(() => {
-      ;(getStaticPropsResource as jest.Mock).mockResolvedValue(mockResource)
-    })
 
     it('handles DoNotPublish error', async () => {
       ;(getStaticPropsResource as jest.Mock).mockRejectedValue(
