955e1901b26012b7dc34e6c9e75504fce9f3732d
Tim Cosgrove <tcosgrove@metrostar.com>
2025-04-02 16:51:47 -0700

    Minor tweaks to get Playwright tests running.

--- playwright/tests/pressRelease.spec.ts
+++ playwright/tests/pressRelease.spec.ts
@@ -2,15 +2,21 @@ import { test, expect } from '../utils/next-test'
 
 test.describe('pressRelease', () => {
   test('pressRelease page renders', async ({ page }) => {
-    await page.goto('/update-this-link')
-    await expect(page).toHaveURL('/update-this-link')
+    await page.goto(
+      '/southern-nevada-health-care/news-releases/vasnhs-helping-veterans-prepare-for-secure-sign-in-changes/'
+    )
+    await expect(page.locator('h1')).toHaveText(
+      'VASNHS Helping Veterans Prepare for Secure Sign-in Changes'
+    )
   })
 
   test('Should render without a11y errors', async ({
     page,
     makeAxeBuilder,
   }) => {
-    await page.goto('/update-this-link')
+    await page.goto(
+      '/southern-nevada-health-care/news-releases/vasnhs-helping-veterans-prepare-for-secure-sign-in-changes/'
+    )
 
     const accessibilityScanResults = await makeAxeBuilder().analyze()
 
