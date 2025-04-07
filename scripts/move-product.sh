e26da2ff0541763cf861246ec755cdc74cbb6e31
Chris Valarida <chris@themostudio.com>
2025-04-07 14:26:08 -0700

    Don't actually need to exclude the node_modules
    Since we're only searching in `src/` anyhow 🤷

--- scripts/move-product.sh
+++ scripts/move-product.sh
@@ -128,7 +128,7 @@ update_all_imports() {
     replace_escaped=$(printf '%s\n' "$replace" | sed 's/[\/&]/\\&/g')
 
     # Find and replace with word boundaries
-    grep -rl --exclude-dir=node_modules --include="*.ts*" --include="*.js*" "$search" ./src | while read -r file; do
+    grep -rl --include="*.ts*" --include="*.js*" "$search" ./src | while read -r file; do
       log "↪ Updating file: $(color "$file" cyan)"
       run_or_echo "sed -i '' -E \"s|$search_escaped([^a-zA-Z])|$replace_escaped\1|g\" \"$file\""
     done
