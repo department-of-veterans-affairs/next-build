5de5a700fdf6ff3ba7382a591762e65994420bbd
Brian Seek <brian.seek@civicactions.com>
2025-04-14 13:18:52 -0700

    phone number TS change.

--- src/data/queries/phoneNumber.ts
+++ src/data/queries/phoneNumber.ts
@@ -2,9 +2,10 @@ import { ParagraphPhoneNumber } from '@/types/drupal/paragraph'
 import { QueryFormatter } from 'next-drupal-query'
 import { PhoneNumber } from '@/types/formatted/phoneNumber'
 
-export const formatter: QueryFormatter<ParagraphPhoneNumber, PhoneNumber> = (
-  entity: ParagraphPhoneNumber
-) => {
+export const formatter: QueryFormatter<
+  ParagraphPhoneNumber,
+  PhoneNumber | null
+> = (entity: ParagraphPhoneNumber) => {
   if (!entity) {
     return null
   }
