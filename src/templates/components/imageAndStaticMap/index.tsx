5a45568e7635e9fdef40bc9a98b7e5c1601b49b6
Chris Valarida <chris@themostudio.com>
2025-04-23 14:27:58 -0700

    Change to 3_2_medium_thumbnail

--- src/templates/components/imageAndStaticMap/index.tsx
+++ src/templates/components/imageAndStaticMap/index.tsx
@@ -13,7 +13,11 @@ export const ImageAndStaticMap: React.FC<ImageAndStaticMapProps> = ({
 }) => {
   return (
     <div className="usa-width-one-third inline-table-helper vads-u-order--first mobile-lg:vads-u-order--initial vads-u-margin-bottom--2 vads-u-margin-left--auto facility">
-      <MediaImage {...image} imageStyle="2_1_large" className="facility-img" />
+      <MediaImage
+        {...image}
+        imageStyle="3_2_medium_thumbnail"
+        className="facility-img"
+      />
       <div data-widget-type="facility-map" data-facility={facilityId}>
         {/* TODO: Create Facility Map component for display here */}
       </div>
