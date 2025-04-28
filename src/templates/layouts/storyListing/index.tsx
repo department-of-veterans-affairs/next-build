18ab88f9f0cac9e010267971a055c40cf6ef96d2
“Anu <anu@themostudio.com>
2025-04-28 13:46:20 -0700

    revert original

--- src/templates/layouts/storyListing/index.tsx
+++ src/templates/layouts/storyListing/index.tsx
@@ -1,3 +1,14 @@
+/**
+ * ### Overview
+ * Story Listing represents the collection pages for News Stories in a facility.
+ *
+ * Story Listing expects data of type {@link FormattedStoryListing}.
+ *
+ * ### Examples
+ * @see https://va.gov/pittsburgh-health-care/stories/
+ *
+ */
+
 import { VaPagination } from '@department-of-veterans-affairs/component-library/dist/react-bindings'
 import { SideNavMenu } from '@/types/formatted/sideNav'
 import { StoryListing as FormattedStoryListing } from '@/types/formatted/storyListing'
@@ -9,6 +20,7 @@ import { LovellStaticPropsResource } from '@/lib/drupal/lovell/types'
 import { LovellSwitcher } from '@/templates/components/lovellSwitcher'
 import { DEFAULT_PAGE_LIST_LENGTH } from '../../../constants/pagination'
 
+// Allows additions to window object without overwriting global type
 interface customWindow extends Window {
   sideNav?: SideNavMenu
 }
@@ -25,13 +37,14 @@ export function StoryListing({
   lovellVariant,
   lovellSwitchPath,
 }: LovellStaticPropsResource<FormattedStoryListing>) {
+  // Add data to the window object for the sidebar widget
   useEffect(() => {
     window.sideNav = menu
   }, [menu])
 
   const storyTeasers =
     stories?.length > 0 ? (
-      stories.map((story: FormattedNewsStoryTeaser) => (
+      stories?.map((story: FormattedNewsStoryTeaser) => (
         <li key={story.id}>
           <NewsStoryTeaser {...story} />
         </li>
@@ -42,13 +55,11 @@ export function StoryListing({
 
   return (
     <div key={id} className="vads-grid-container">
-      {/* Widget coming from vets-website */}
       <nav
         data-template="navigation/facility_sidebar_nav"
         aria-label="secondary"
         data-widget-type="side-nav"
-      ></nav>
-
+      />
       <div className="vads-grid-row">
         <div className="vads-grid-col-9">
           <article className="usa-content">
@@ -64,7 +75,6 @@ export function StoryListing({
               <div className="vads-grid-container--full">
                 <ul className="usa-unstyled-list">{storyTeasers}</ul>
               </div>
-
               {totalPages > 1 && (
                 <VaPagination
                   page={currentPage}
@@ -74,7 +84,7 @@ export function StoryListing({
                     const newPage =
                       page.detail.page > 1 ? `page-${page.detail.page}` : ''
                     const newUrl = window.location.href.replace(
-                      /(?<=stories\/).*/, // everything after /stories/
+                      /(?<=stories\/).*/,
                       newPage
                     )
                     window.location.assign(newUrl)
