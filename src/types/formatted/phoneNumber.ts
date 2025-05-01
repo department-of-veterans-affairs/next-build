da6f2f856e81a32e74f3347bb6fd758fdd4a8658
Brian Seek <brian.seek@civicactions.com>
2025-05-01 11:37:31 -0700

    VAMS 20162 news release defects (#1003)

--- src/types/formatted/phoneNumber.ts
+++ src/types/formatted/phoneNumber.ts
@@ -2,7 +2,7 @@ import { PublishedParagraph } from '@/types/formatted/publishedEntity'
 
 export type PhoneNumber = PublishedParagraph & {
   extension: string
-  label: string
+  label?: string
   number: string
   phoneType: string
 }
