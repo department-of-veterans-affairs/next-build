37650a8e75089a61f2d0ca8540e374ecfb7d0fa0
Brian Seek <brian.seek@civicactions.com>
2025-05-01 16:49:28 -0700

    added early return for empty string

--- src/lib/utils/capitalizeWords.ts
+++ src/lib/utils/capitalizeWords.ts
@@ -4,8 +4,12 @@
  * @returns {string} The transformed string.
  */
 
-export const capitalizeWords = (str: string): string =>
-  str
+export const capitalizeWords = (str: string): string => {
+  if (!str) {
+    return ''
+  }
+  return str
     .split(' ')
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ')
+}
