dbe49fff9e75a37f3bdf3470fbeb52fb0673f01d
Chris Valarida <chris@themostudio.com>
2025-05-02 09:48:30 -0700

    getHtmlFromField

---
+++ src/lib/utils/getHtmlFromField.ts
@@ -0,0 +1,15 @@
+import { FieldFormattedText } from '@/types/drupal/field_type'
+import { drupalToVaPath, phoneLinks } from './helpers'
+
+/**
+ * Extract the processed HTML from a FieldFormattedText field and apply filters.
+ *
+ * Return the HTML string after processing.
+ */
+export const getHtmlFromField = (formattedTextField?: FieldFormattedText) => {
+  const data = [formattedTextField?.processed]
+  const filters = [phoneLinks, drupalToVaPath]
+  const filteredData = filters.reduce((d, f) => d.filter(f), data)
+
+  return filteredData[0]
+}
