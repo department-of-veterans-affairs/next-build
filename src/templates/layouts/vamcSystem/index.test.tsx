5192b3c87b2fffc1aab24b827b41447ed1bde6dc
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-05-06 14:16:01 -0700

    Some basic component unit tests for the features I have so far

--- src/templates/layouts/vamcSystem/index.test.tsx
+++ src/templates/layouts/vamcSystem/index.test.tsx
@@ -1,14 +1,99 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import drupalMockData from '@/mocks/vamcSystem.mock.json'
 import { VamcSystem } from './index'
+import { VamcSystem as FormattedVamcSystem } from '@/types/formatted/vamcSystem'
+import { formatter } from '@/data/queries/vamcSystem'
+import { DrupalMenuLinkContent } from 'next-drupal'
+
+const menuItem: DrupalMenuLinkContent = {
+  title: 'Foo',
+  type: 'meh',
+  url: '/nowhere/in-particular',
+  id: 'foo',
+  description: 'bar',
+  enabled: true,
+  expanded: true,
+  menu_name: 'baz',
+  meta: {},
+  options: {},
+  parent: null,
+  provider: null,
+  route: null,
+  weight: '0',
+}
+
+const mockMenu = {
+  items: [menuItem],
+  tree: [menuItem],
+}
+
+const mockData = formatter({
+  // @ts-expect-error drupalMockData technically has numbers instead of strings
+  // for some of the IDs, but this is a known problem. See
+  // https://github.com/chapter-three/next-drupal/issues/686#issuecomment-2083175598
+  entity: drupalMockData,
+  menu: mockMenu,
+  lovell: { isLovellVariantPage: false, variant: 'va' },
+})
 
 describe('VamcSystem with valid data', () => {
-  test('renders VamcSystem component', () => {
-    // I'll fill this in later. Doing a bogus operation for now:
-    expect(true).toBe(true)
+  test('renders VamcSystem component with basic data', () => {
+    render(<VamcSystem {...mockData} />)
 
-    // render(<VamcSystem title={'Hello world'} />)
+    const basicDataFields: Array<keyof FormattedVamcSystem> = [
+      'title',
+      'introText',
+    ]
+    basicDataFields.forEach((key) =>
+      expect(screen.getByText(mockData[key])).toBeInTheDocument()
+    )
+  })
 
-    // expect(screen.queryByText(/Hello world/)).toBeInTheDocument()
+  test('adds the sideNav to window.sideNav', () => {
+    render(<VamcSystem {...mockData} />)
+
+    // @ts-expect-error - window.sideNav is not a default window property, but
+    // we're adding it
+    expect(window.sideNav).toEqual(mockData.menu)
+  })
+
+  test('renders the administration information in a "manage your health online" section', () => {
+    render(<VamcSystem {...mockData} />)
+    expect(screen.getByText('Manage your health online')).toBeInTheDocument()
+    // TODO: Look for other info once we fill out this section. Note that just looking
+    // for the administration name in the document is not enough because it's the same
+    // as the page title.
+  })
+
+  test('does not render the "manage your health online" section if the administration is 1039', () => {
+    render(
+      <VamcSystem
+        {...mockData}
+        administration={{ ...mockData.administration, id: 1039 }}
+      />
+    )
+    expect(
+      screen.queryByText('Manage your health online')
+    ).not.toBeInTheDocument()
+  })
+
+  test('uses an alternate title for the administration section if the administration is 1040', () => {
+    render(
+      <VamcSystem
+        {...mockData}
+        administration={{ ...mockData.administration, id: 1040 }}
+      />
+    )
+    expect(screen.getByText('Manage your VA health online')).toBeInTheDocument()
+  })
+
+  test('renders the facility image with correct attributes', () => {
+    render(<VamcSystem {...mockData} />)
+
+    const image = screen.getByRole('img')
+    expect(image).toBeInTheDocument()
+    // Sanity check that we're passing the right data to the image component
+    expect(image).toHaveAttribute('alt', mockData.image.alt)
   })
 })
