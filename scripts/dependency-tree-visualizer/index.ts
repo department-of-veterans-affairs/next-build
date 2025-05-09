a81b6fc78c55af3a10629dbc3243b9e99ddbbcfe
Chris Valarida <chris@themostudio.com>
2025-05-09 15:19:44 -0700

    Make it a commander app
    Also find the content-build root and use that automagically

--- scripts/dependency-tree-visualizer/index.ts
+++ scripts/dependency-tree-visualizer/index.ts
@@ -5,29 +5,40 @@
  *   dot -Tsvg graph.dot -o graph.svg
  */
 import fs from 'fs'
+import path from 'path'
 import chalk from 'chalk'
+import { program } from 'commander'
 
 import { buildDependencyGraph } from './findDependencies'
 import { emitConsoleTree } from './emitConsoleTree'
 import { generateDotGraph } from './emitDotGraph'
 
 if (require.main === module) {
-  const [, , mode, entryFile, rootDir] = process.argv
-
-  if (!entryFile || !rootDir) {
-    console.log(
-      chalk.magenta(`Usage: ts-node index.ts <mode> <template> <rootDir>`)
+  program
+    .option('--dot', 'Emit a .dot file for Graphviz')
+    .argument(
+      '<path-to-template>',
+      'The path to the template to find the dependency tree of'
     )
-    process.exit(1)
-  }
+    .action((pathToTemplate, options) => {
+      const entryFile = path.resolve(pathToTemplate)
 
-  const graph = buildDependencyGraph(entryFile, rootDir)
+      // Find the root directory of content build
+      const parts = entryFile.split(path.sep)
+      const contentBuildPath = parts
+        .slice(0, parts.indexOf('content-build') + 1)
+        .join(path.sep)
 
-  if (mode === '--dot') {
-    const dot = generateDotGraph(graph)
-    fs.writeFileSync('graph.dot', dot)
-    console.log(chalk.green('DOT graph written to graph.dot'))
-  } else {
-    emitConsoleTree(graph)
-  }
+      const graph = buildDependencyGraph(entryFile, contentBuildPath)
+
+      if (options.dot) {
+        const dot = generateDotGraph(graph)
+        fs.writeFileSync('graph.dot', dot)
+        console.log(chalk.green('DOT graph written to graph.dot'))
+      } else {
+        emitConsoleTree(graph)
+      }
+    })
+
+  program.parse()
 }
