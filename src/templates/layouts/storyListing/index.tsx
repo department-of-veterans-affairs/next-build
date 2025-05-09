87ccb10f6bd16f2bae54b308607777c910c9e297
Anu Das <137448668+moanu404@users.noreply.github.com>
2025-05-09 09:28:53 -0700

    VACMS-20952: V3 Grid migration for story listings (#992)

--- src/templates/layouts/storyListing/index.tsx
+++ src/templates/layouts/storyListing/index.tsx
@@ -54,48 +54,49 @@ export function StoryListing({
     )
 
   return (
-    <div key={id} className="usa-grid usa-grid-full">
+    <div key={id} className="vads-grid-container">
       {/* Widget coming from vets-website */}
       <nav
         data-template="navigation/facility_sidebar_nav"
         aria-label="secondary"
         data-widget-type="side-nav"
       ></nav>
+      <div className="vads-grid-row">
+        <div className="vads-grid-col-12">
+          <article>
+            <LovellSwitcher
+              currentVariant={lovellVariant}
+              switchPath={lovellSwitchPath}
+            />
+            <h1>{title}</h1>
+            <div className="vads-grid-container--full">
+              <div className="va-introtext">
+                {introText && <p>{introText}</p>}
+              </div>
+              <div className="vads-grid-container--full">
+                <ul className="usa-unstyled-list">{storyTeasers}</ul>
+              </div>
 
-      <div className="usa-width-three-fourths">
-        <article className="usa-content">
-          <LovellSwitcher
-            currentVariant={lovellVariant}
-            switchPath={lovellSwitchPath}
-          />
-          <h1>{title}</h1>
-          <div className="vads-l-grid-container--full">
-            <div className="va-introtext">
-              {introText && <p>{introText}</p>}
+              {totalPages > 1 && (
+                <VaPagination
+                  page={currentPage}
+                  pages={totalPages}
+                  maxPageListLength={DEFAULT_PAGE_LIST_LENGTH}
+                  onPageSelect={(page) => {
+                    const newPage =
+                      page.detail.page > 1 ? `page-${page.detail.page}` : ''
+                    const newUrl = window.location.href.replace(
+                      /(?<=stories\/).*/, // everything after /stories/
+                      newPage
+                    )
+                    window.location.assign(newUrl)
+                  }}
+                />
+              )}
             </div>
-            <div className="vads-l-grid-container--full">
-              <ul className="usa-unstyled-list">{storyTeasers}</ul>
-            </div>
-
-            {totalPages > 1 && (
-              <VaPagination
-                page={currentPage}
-                pages={totalPages}
-                maxPageListLength={DEFAULT_PAGE_LIST_LENGTH}
-                onPageSelect={(page) => {
-                  const newPage =
-                    page.detail.page > 1 ? `page-${page.detail.page}` : ''
-                  const newUrl = window.location.href.replace(
-                    /(?<=stories\/).*/, // everything after /stories/
-                    newPage
-                  )
-                  window.location.assign(newUrl)
-                }}
-              />
-            )}
-          </div>
-        </article>
-        <ContentFooter />
+          </article>
+          <ContentFooter />
+        </div>
       </div>
     </div>
   )
