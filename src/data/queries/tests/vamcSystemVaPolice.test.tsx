85f8bc648130f80df0f6242841934a5d5698f948
Christian Burk <omahane@gmail.com>
2025-05-19 09:01:08 -0500

    VACMS-20871: Updates test

--- src/data/queries/tests/vamcSystemVaPolice.test.tsx
+++ src/data/queries/tests/vamcSystemVaPolice.test.tsx
@@ -1,28 +1,28 @@
-/**
- * @jest-environment node
- */
-
 import { NodeVamcSystemVaPolice } from '@/types/drupal/node'
 import { queries } from '@/data/queries'
-import mockData from '@/mocks/vamcSystemVaPolice.mock.json'
+import mockData from '@/mocks/healthCareLocalFacility.mock.json'
 
-const VamcSystemVaPoliceMock: NodeVamcSystemVaPolice = mockData
-
-// remove if this component does not have a data fetch
-describe('DrupalJsonApiParams configuration', () => {
-  test('params function sets the correct include fields', () => {
-    // TODO
-  })
-})
+const VamcSystemVaPoliceMock : NodeVamcSystemVaPolice = mockData
 
 describe('VamcSystemVaPolice formatData', () => {
-  test('outputs formatted data', () => {
-    expect(
-      queries.formatData('node--vamc_system_va_police', VamcSystemVaPoliceMock)
-    ).toMatchSnapshot()
+  let windowSpy
+
+  beforeEach(() => {
+    windowSpy = jest.spyOn(window, 'window', 'get')
   })
 
-  test('handles no answers correctly', () => {
-    // TODO
+  afterEach(() => {
+    windowSpy.mockRestore()
+  })
+
+  test('outputs formatted data', () => {
+    windowSpy.mockImplementation(() => undefined)
+
+    expect(
+      queries.formatData(
+        'node--vamc_system_va_police',
+        VamcSystemVaPoliceMock
+      )
+    ).toMatchSnapshot()
   })
 })
