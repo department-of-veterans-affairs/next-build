127600985ec565b47bf04fb08369690b620ef592
“Anu <anu@themostudio.com>
2025-05-22 14:47:49 -0700

    unexperimenting

--- src/test/slug.test.tsx
+++ src/test/slug.test.tsx
@@ -26,24 +26,24 @@ describe('[[...slug]].tsx', () => {
   describe('getStaticProps', () => {
     const mockContext = { params: { slug: ['test-path'] }, preview: false }
     it('returns notFound when DoNotPublishError is thrown', async () => {
-      // // Mock the expanded context
-      // const mockExpandedContext = { drupalPath: '/test-path', preview: false }
-      // const mockPathInfo = { jsonapi: { resourceName: 'node--event' } }
-      // // Mock the functions
-      // ;(getExpandedStaticPropsContext as jest.Mock).mockReturnValue(
-      //   mockExpandedContext
-      // )
-      // ;(drupalClient.translatePath as jest.Mock).mockResolvedValue(mockPathInfo)
-      // // Mock getStaticPropsResource to throw DoNotPublishError
-      // ;(getStaticPropsResource as jest.Mock).mockImplementation(() => {
-      //   throw new DoNotPublishError('Do not publish error')
-      // })
-      // // Call getStaticProps
-      // const result = await getStaticProps(mockContext)
-      // // Assert that notFound is returned
-      // expect(result).toEqual({ notFound: true })
-      // // Assert that getStaticPropsResource was called
-      // expect(getStaticPropsResource).toHaveBeenCalled()
+      // Mock the expanded context
+      const mockExpandedContext = { drupalPath: '/test-path', preview: false }
+      const mockPathInfo = { jsonapi: { resourceName: 'node--event' } }
+      // Mock the functions
+      ;(getExpandedStaticPropsContext as jest.Mock).mockReturnValue(
+        mockExpandedContext
+      )
+      ;(drupalClient.translatePath as jest.Mock).mockResolvedValue(mockPathInfo)
+      // Mock getStaticPropsResource to throw DoNotPublishError
+      ;(getStaticPropsResource as jest.Mock).mockImplementation(() => {
+        throw new DoNotPublishError('Do not publish error')
+      })
+      // Call getStaticProps
+      const result = await getStaticProps(mockContext)
+      // Assert that notFound is returned
+      expect(result).toEqual({ notFound: true })
+      // Assert that getStaticPropsResource was called
+      expect(getStaticPropsResource).toHaveBeenCalled()
     })
   })
 })
