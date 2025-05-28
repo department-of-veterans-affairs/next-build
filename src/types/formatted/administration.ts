a902ec4f55bde4bbda225be05ce955e3f1f9a7f4
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-05-28 10:34:50 -0700

    Add some doc comments to the type itself

--- src/types/formatted/administration.ts
+++ src/types/formatted/administration.ts
@@ -1,4 +1,11 @@
 export interface Administration {
+  /**
+   * This is the internal Drupal id of the administration term. See the administration
+   * formatter function comments for more details.
+   */
   entityId: number
+  /**
+   * The human-readable name of the administration term.
+   */
   name: string | null
 }
