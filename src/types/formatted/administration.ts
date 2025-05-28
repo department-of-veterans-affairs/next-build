48cd299e8b4fb75428cf8c32831958e64158ed35
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-05-28 10:52:36 -0700

    Unify `field_administration` handling across content types (#1048)

--- src/types/formatted/administration.ts
+++ src/types/formatted/administration.ts
@@ -1,4 +1,11 @@
-export type Administration = {
-  id: number
-  name: string
+export interface Administration {
+  /**
+   * This is the internal Drupal id of the administration term. See the administration
+   * formatter function comments for more details.
+   */
+  entityId: number | null
+  /**
+   * The human-readable name of the administration term.
+   */
+  name: string | null
 }
