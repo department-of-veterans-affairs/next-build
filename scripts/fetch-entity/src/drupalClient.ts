e968efce9b4f469757084a098ab8b77ce8b39baa
Chris Valarida <cvalarida@gmail.com>
2025-05-28 09:50:52 -0700

    Add fetch-entity CLI (#1044)

---
+++ scripts/fetch-entity/src/drupalClient.ts
@@ -0,0 +1,51 @@
+/**
+ * This is essentially a copy of what's in the drupalClient.ts module, but
+ * copying it over here for use in the script because importing it from
+ * next-build/src/lib/drupal/drupalClient.ts is a pain.
+ */
+
+import { DrupalClient } from 'next-drupal'
+
+/* eslint-disable import/no-extraneous-dependencies */
+import { SocksProxyAgent } from 'socks-proxy-agent'
+import fetch from 'node-fetch'
+
+const syswideCas = await import('syswide-cas')
+syswideCas.addCAs('certs/VA-Internal-S2-RCA-combined.pem')
+syswideCas.addCAs('certs/rootCA.pem')
+
+const baseUrl =
+  process.env.NEXT_PUBLIC_DRUPAL_BASE_URL || 'https://va-gov-cms.ddev.site'
+
+const proxyFetch = (input: RequestInfo, init: RequestInit = {}) => {
+  const agent = new SocksProxyAgent('socks5h://127.0.0.1:2001')
+  const initParams = { ...init }
+  const url = input instanceof Request ? input.url : input
+  if (url.match('va-gov-cms.ddev.site')) {
+    syswideCas.addCAs('certs/rootCA.pem')
+  } else {
+    // @ts-expect-error We don't have any TS types for node-fetch, so it doesn't
+    // know that this is a thing we can add.
+    initParams.agent = agent
+  }
+  return fetch(input, initParams)
+}
+
+export const drupalClient = new DrupalClient(baseUrl, {
+  fetcher: proxyFetch,
+  useDefaultResourceTypeEntry: true,
+  throwJsonApiErrors: false,
+  auth: {
+    // @ts-expect-error It's possible for the env vars to not be set properly
+    clientId: process.env.DRUPAL_CLIENT_ID,
+    // @ts-expect-error It's possible for the env vars to not be set properly
+    clientSecret: process.env.DRUPAL_CLIENT_SECRET,
+  },
+  previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
+  // Add request header to tell the CMS to return public-facing URLs for files.
+  headers: {
+    'Content-Type': 'application/vnd.api+json',
+    Accept: 'application/vnd.api+json',
+    'File-Public-Base-Url-Check': 'true',
+  },
+})
