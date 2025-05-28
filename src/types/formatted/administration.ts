2cd3118c54c8b2e3049725bcda94d2144b18af62
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-05-28 10:35:40 -0700

    This value can be null according to the logic, though it shouldn't be in practice

--- src/types/formatted/administration.ts
+++ src/types/formatted/administration.ts
@@ -3,7 +3,7 @@ export interface Administration {
    * This is the internal Drupal id of the administration term. See the administration
    * formatter function comments for more details.
    */
-  entityId: number
+  entityId: number | null
   /**
    * The human-readable name of the administration term.
    */
