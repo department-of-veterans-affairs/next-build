23e45ca92bb83bdc308193c07bfdaf793cd6fd42
Brian Seek <brian.seek@civicactions.com>
2025-05-30 14:12:17 -0700

    updated playwrite test

--- playwright/tests/leadershipListing.spec.js
+++ playwright/tests/leadershipListing.spec.js
@@ -2,17 +2,17 @@ import { test, expect } from '../utils/next-test'
 
 // This test should be modified to test page output and function.
 // test.describe('Leadership_listing', () => {
-test.skip('Leadership_listing', () => {
+test('Leadership_listing', () => {
   test('Leadership_listing page renders', async ({ page }) => {
-    await page.goto('/update-this-link')
-    await expect(page).toHaveURL('/update-this-link')
+    await page.goto('/boston-health-care/about-us/leadership')
+    await expect(page).toHaveURL('/boston-health-care/about-us/leadership')
   })
 
   test('Should render without a11y errors', async ({
     page,
     makeAxeBuilder,
   }) => {
-    await page.goto('/update-this-link')
+    await page.goto('/boston-health-care/about-us/leadership')
 
     const accessibilityScanResults = await makeAxeBuilder().analyze()
 
