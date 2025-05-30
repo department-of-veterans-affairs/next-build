798198c96e7c892cb308a3cbc715a856c8165b34
Brian Seek <brian.seek@civicactions.com>
2025-05-30 12:02:10 -0700

    added query test

--- src/data/queries/tests/leadershipListing.test.tsx
+++ src/data/queries/tests/leadershipListing.test.tsx
@@ -4,24 +4,26 @@
 import { NodeLeadershipListing } from '@/types/drupal/node'
 import { queries } from '@/data/queries'
 import mockData from '@/mocks/leadershipListing.mock.json'
+import { params } from '../leadershipListing'
 
-// const LeadershipListingMock: NodeLeadershipListing = mockData
+//eslint-disable-next-line
+const leadershipListingMock: NodeLeadershipListing | any = mockData
 
-// remove if this component does not have a data fetch
-describe.skip('DrupalJsonApiParams configuration', () => {
-  test('params function sets the correct include fields', () => {
-    // TODO
+describe('DrupalJsonApiParams configuration', () => {
+  test('params function includes the correct fields', () => {
+    const paramsInstance = params()
+    const queryString = decodeURIComponent(paramsInstance.getQueryString())
+    expect(queryString).toMatch(/include=field_leadership,field_office/)
   })
 })
 
-describe.skip('LeadershipListing formatData', () => {
+describe('LeadershipListing formatData', () => {
   test('outputs formatted data', () => {
-    // expect(
-    //   queries.formatData('node--leadership_listing', LeadershipListingMock)
-    // ).toMatchSnapshot()
-  })
-
-  test('handles no answers correctly', () => {
-    // TODO
+    expect(
+      queries.formatData('node--leadership_listing', {
+        entity: leadershipListingMock,
+        menu: null,
+      })
+    ).toMatchSnapshot()
   })
 })
