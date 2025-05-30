06eb619f2a0676ce1305a2c9d4fb6fce021c88ba
Brian Seek <brian.seek@civicactions.com>
2025-05-30 15:32:49 -0700

    fix playwrite test

--- playwright/tests/leadershipListing.spec.js
+++ playwright/tests/leadershipListing.spec.js
@@ -1,10 +1,10 @@
 import { test, expect } from '../utils/next-test'
 
 // This test should be modified to test page output and function.
-test('Leadership_listing', () => {
+test.describe('Leadership_listing', () => {
   test('Leadership_listing page renders', async ({ page }) => {
     await page.goto('/boston-health-care/about-us/leadership')
-    await expect(page).toHaveURL('/boston-health-care/about-us/leadership')
+    await expect(page).toHaveURL('/boston-health-care/about-us/leadership/')
   })
 
   test('Should render without a11y errors', async ({
