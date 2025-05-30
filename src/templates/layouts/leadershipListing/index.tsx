485018ad6f523d8c1a3226270f97fc963c54f8b9
Brian Seek <brian.seek@civicactions.com>
2025-05-30 14:42:02 -0700

    added lovell todo

--- src/templates/layouts/leadershipListing/index.tsx
+++ src/templates/layouts/leadershipListing/index.tsx
@@ -20,6 +20,7 @@ export function LeadershipListing({
         <nav aria-label="secondary" data-widget-type="side-nav" />
         <div className="usa-width-three-fourths">
           <article>
+            <div>*TODO Lovell Switch*</div>
             <h1 className="vads-u-margin-bottom--3">{title}</h1>
             <div className="va-introtext">
               <p>{introText}</p>
