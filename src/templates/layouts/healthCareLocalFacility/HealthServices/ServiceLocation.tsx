45c2ba5f3fb918f6801f0c965cb3ca4f236770e5
Chris Valarida <chris@themostudio.com>
2025-06-02 16:35:43 -0700

    Fill in ServiceLocation
    And hope for the best! 🤞

--- src/templates/layouts/healthCareLocalFacility/HealthServices/ServiceLocation.tsx
+++ src/templates/layouts/healthCareLocalFacility/HealthServices/ServiceLocation.tsx
@@ -1,5 +1,281 @@
 import { ServiceLocationTemplateData } from './types'
+import { ServiceAddress } from './ServiceAddress'
+import { PhoneNumber } from '@/templates/common/phoneNumber'
+
+// TODO: Replace these with the web components
+const VaIcon = ({ icon, size }: { icon: string; size: string }) => (
+  <span>{icon}</span>
+)
+const VaLinkAction = ({ href, text }: { href: string; text: string }) => (
+  <a href={href}>{text}</a>
+)
 
 export const ServiceLocation = (props: ServiceLocationTemplateData) => {
-  return <div>Stub ServiceLocation</div>
+  const {
+    typeOfLocation,
+    serviceLocationSubHeaderLevel,
+    serviceHeader,
+    serviceDescription,
+    entityId,
+    fieldReferralRequired,
+    fieldTelephone,
+    fieldPhoneNumber,
+    isMentalHealthService,
+    single,
+  } = props
+
+  const serviceLocationH = Number(serviceLocationSubHeaderLevel) || 3
+  const HeadingTag = `h${serviceLocationH}` as keyof JSX.IntrinsicElements
+  const nextHeadingLevel = serviceLocationH + 1
+
+  // Determine container
+  const Container = typeOfLocation === 'nonclinical' ? 'div' : 'va-card'
+
+  // Determine service main phone
+  // Determine main phone number
+  const mainPhone =
+    isMentalHealthService && fieldTelephone
+      ? fieldTelephone
+      : fieldPhoneNumber
+        ? { number: fieldPhoneNumber, extension: '', phoneType: 'tel' }
+        : null
+
+  const showMainNumberForAppointments =
+    mainPhone && single.fieldUseFacilityPhoneNumber
+  const hasAppointmentPhoneNumbers =
+    (single.fieldOtherPhoneNumbers?.length || 0) > 0
+  const hasOtherContactPhoneNumbers = (single.fieldPhone?.length || 0) > 0
+
+  const hasOfficeVisits =
+    single.fieldOfficeVisits &&
+    single.fieldOfficeVisits !== 'no' &&
+    single.fieldOfficeVisits !== 'null'
+  const hasVirtualSupport =
+    single.fieldVirtualSupport &&
+    single.fieldVirtualSupport !== 'no' &&
+    single.fieldVirtualSupport !== 'null'
+  const showOnlineScheduleLink = single.fieldOnlineSchedulingAvail === 'yes'
+
+  return (
+    <Container className="break-word-wrap">
+      {serviceHeader && (
+        <>
+          <HeadingTag className="vads-u-margin-y--0">
+            {serviceHeader}
+          </HeadingTag>
+        </>
+      )}
+
+      {serviceDescription && (
+        <p
+          id={`vba-service-body-${entityId}`}
+          className="vads-u-margin-top--2 vads-u-margin-bottom--0"
+        >
+          {serviceDescription}{' '}
+          {/* Add drupalToVaPath/phoneLinks transforms if needed */}
+        </p>
+      )}
+
+      {/* Office visits and virtual support */}
+      {(hasOfficeVisits ||
+        hasVirtualSupport ||
+        (fieldReferralRequired &&
+          !['not_applicable', 'unknown', '2'].includes(
+            fieldReferralRequired
+          ))) && (
+        <div className="vads-u-padding-top--1">
+          {hasOfficeVisits && (
+            <p className="vads-u-margin-top--0 vads-u-margin-bottom--0">
+              <VaIcon icon="location_city" size="3" />{' '}
+              {{
+                yes_appointment_only: 'Visit our office, by appointment only',
+                yes_walk_in_visits_only:
+                  'Visit our office, walk-in visits only',
+                yes_with_or_without_appointment:
+                  'Visit our office, with or without an appointment',
+              }[single.fieldOfficeVisits as string] || ''}
+            </p>
+          )}
+          {hasVirtualSupport && (
+            <p className="vads-u-margin-top--1 vads-u-margin-bottom--0">
+              <VaIcon
+                icon={
+                  single.fieldVirtualSupport === 'yes_veterans_can_call'
+                    ? 'phone'
+                    : 'calendar_today'
+                }
+                size="3"
+              />{' '}
+              {{
+                yes_appointment_only: 'Virtual visits by appointment only',
+                yes_veterans_can_call: 'Call at your convenience',
+                virtual_visits_may_be_available:
+                  'Virtual visits may be available',
+              }[single.fieldVirtualSupport as string] || ''}
+            </p>
+          )}
+          {fieldReferralRequired &&
+            !['not_applicable', 'unknown', '2'].includes(
+              fieldReferralRequired
+            ) && (
+              <p className="vads-u-margin-top--1 vads-u-margin-bottom--0">
+                <VaIcon
+                  icon={
+                    fieldReferralRequired === '1' ? 'check_circle' : 'cancel'
+                  }
+                  size="3"
+                />{' '}
+                {fieldReferralRequired === '1'
+                  ? 'A referral is required'
+                  : 'A referral is not required'}
+              </p>
+            )}
+        </div>
+      )}
+
+      {/* Appointments header */}
+      {(single.fieldApptIntroTextType !== 'remove_text' ||
+        hasAppointmentPhoneNumbers ||
+        showMainNumberForAppointments ||
+        showOnlineScheduleLink) && (
+        <HeadingTag
+          className="vads-u-margin-top--2 vads-u-line-height--1"
+          data-testid="service-location-appoinments-header"
+        >
+          Appointments
+        </HeadingTag>
+      )}
+
+      {/* Appointment intro text */}
+      {single.fieldApptIntroTextType &&
+        single.fieldApptIntroTextType !== 'remove_text' && (
+          <div>
+            {single.fieldApptIntroTextType === 'customize_text' ? (
+              <p
+                data-testid="service-location-custom-text"
+                className="vads-u-margin-bottom--0"
+              >
+                {single.fieldApptIntroTextCustom}
+              </p>
+            ) : (
+              <p
+                data-testid="service-location-default-text"
+                className="vads-u-margin-bottom--0"
+              >
+                {typeOfLocation === 'vba'
+                  ? 'Contact us to schedule, reschedule, or cancel your appointment.'
+                  : 'Contact us to schedule, reschedule, or cancel your appointment. If a referral is required, you’ll need to contact your primary care provider first.'}
+              </p>
+            )}
+          </div>
+        )}
+
+      {/* Main phone number */}
+      {showMainNumberForAppointments && mainPhone && (
+        <div data-testid="service-location-main-facility-phone">
+          <PhoneNumber
+            number={mainPhone.number}
+            extension={mainPhone.extension}
+            phoneType={mainPhone.phoneType}
+            label="Main Phone"
+            testId="main-phone-appointments"
+          />
+        </div>
+      )}
+
+      {/* Other appointment phone numbers */}
+      {hasAppointmentPhoneNumbers && (
+        <div data-testid="service-location-show-other-phone-numbers">
+          {single.fieldOtherPhoneNumbers?.map((num, idx) => (
+            <PhoneNumber key={idx} {...num} />
+          ))}
+        </div>
+      )}
+
+      {/* Online scheduling link */}
+      {showOnlineScheduleLink && (
+        <div
+          data-testid="service-location-action-link-online"
+          className="vads-u-margin-top--2 vads-u-margin-bottom--1"
+        >
+          <VaLinkAction
+            href={
+              typeOfLocation === 'vba'
+                ? 'https://va.my.site.com/VAVERA/s/'
+                : '/health-care/schedule-view-va-appointments'
+            }
+            text="Schedule an appointment online"
+          />
+        </div>
+      )}
+
+      {/* Service location address */}
+      {single.fieldServiceLocationAddress?.entity && (
+        <ServiceAddress
+          serviceLocationAddress={single.fieldServiceLocationAddress.entity}
+          headerLevel={nextHeadingLevel}
+        />
+      )}
+
+      {/* Main facility phone for contact */}
+
+      {single.fieldUseMainFacilityPhone && mainPhone && (
+        <div data-testid="service-location-main-facility-phone-for-contact">
+          <PhoneNumber
+            number={mainPhone.number}
+            extension={mainPhone.extension}
+            phoneType={mainPhone.phoneType}
+            label="Main Phone"
+            testId="main-phone-contact"
+          />
+        </div>
+      )}
+
+      {/* Additional contact phone numbers */}
+      {hasOtherContactPhoneNumbers && (
+        <div data-testid="service-location-show-contact-phone-numbers">
+          {single.fieldPhone?.map((num, idx) => (
+            <PhoneNumber key={idx} {...num} />
+          ))}
+        </div>
+      )}
+
+      {/* Email contacts */}
+      {single.fieldEmailContacts?.map((email, i) => (
+        <p key={i} data-testid="service-location-email-contact">
+          {email.fieldEmailLabel && (
+            <HeadingTag>{email.fieldEmailLabel}</HeadingTag>
+          )}
+          <a href={`mailto:${email.fieldEmailAddress}`}>
+            {email.fieldEmailAddress}
+          </a>
+        </p>
+      ))}
+
+      {/* Service hours */}
+      {single.fieldHours !== '1' && (
+        <HeadingTag data-testid="service-location-field-hours">
+          Service Hours
+        </HeadingTag>
+      )}
+      {single.fieldHours && (
+        <div>
+          {single.fieldHours === '2' && single.fieldOfficeHours && (
+            <p>(Service-specific hours rendering here)</p>
+          )}
+          {single.fieldHours === '0' && (
+            <p data-testid="service-location-field-hours-same-as-facility">
+              The service hours are the same as our facility hours.
+            </p>
+          )}
+        </div>
+      )}
+
+      {single.fieldAdditionalHoursInfo && (
+        <p className="vads-u-margin-bottom--0">
+          {single.fieldAdditionalHoursInfo}
+        </p>
+      )}
+    </Container>
+  )
 }
