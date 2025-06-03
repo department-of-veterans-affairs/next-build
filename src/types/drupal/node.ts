0b25d85639e59e52d547d0386244de4afe12c6fc
Christian Burk <766573+omahane@users.noreply.github.com>
2025-06-03 13:13:37 -0500

    VACMS-20854: Police contact (#1064)

--- src/types/drupal/node.ts
+++ src/types/drupal/node.ts
@@ -432,6 +432,7 @@ export interface NodeSupportService extends DrupalNode {
 export interface NodeVamcSystemVaPolice extends DrupalNode {
   field_administration: FieldAdministration
   field_cc_va_police_overview: FieldCCText
+  field_phone_numbers_paragraph: ParagraphPhoneNumber[]
 }
 
 export interface NodeLeadershipListing extends DrupalNode {
