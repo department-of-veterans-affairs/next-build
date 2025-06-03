415d2893c6d0aba1e58170d487b02f58cc225c8c
Christian Burk <766573+omahane@users.noreply.github.com>
2025-06-03 13:13:37 -0500

    VACMS-20854: Police contact (#1064)

--- src/types/formatted/vamcSystemVaPolice.ts
+++ src/types/formatted/vamcSystemVaPolice.ts
@@ -2,10 +2,14 @@ import { PublishedEntity } from './publishedEntity'
 import { Administration } from '@/types/formatted/administration'
 import { SideNavMenu } from '@/types/formatted/sideNav'
 import { Wysiwyg } from '@/types/formatted/wysiwyg'
+import { Phone } from './contactInfo'
+import { PhoneNumber } from '@/types/formatted/phoneNumber'
 
 export type VamcSystemVaPolice = PublishedEntity & {
   title: string
   menu: SideNavMenu
   path: string
   policeOverview: Wysiwyg
+  system: string
+  phoneNumber: PhoneNumber
 }
