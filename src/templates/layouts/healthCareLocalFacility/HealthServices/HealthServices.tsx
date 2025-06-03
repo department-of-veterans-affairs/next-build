af73d830a46806ffe3bf4c56df35042e5308a04a
Chris Valarida <chris@themostudio.com>
2025-06-03 11:43:51 -0700

    Fill in HealthServices

--- src/templates/layouts/healthCareLocalFacility/HealthServices/HealthServices.tsx
+++ src/templates/layouts/healthCareLocalFacility/HealthServices/HealthServices.tsx
@@ -1,9 +1,10 @@
-import { NodeHealthCareLocalHealthService } from '@/types/drupal/node'
+import { FormattedHealthServices } from '@/types/formatted/healthCareLocalFacility'
+import { ServiceLocation } from './ServiceLocation'
 
 export const HealthServices = ({
   healthServices,
 }: {
-  healthServices: NodeHealthCareLocalHealthService[]
+  healthServices: FormattedHealthServices[]
 }) => {
   return (
     <>
@@ -14,6 +15,68 @@ export const HealthServices = ({
         Health services offered here
       </h2>
       <p>Select a topic to learn more.</p>
+
+      {healthServices.map((service) => {
+        const id = service.name.toLowerCase().replace(/\s+/g, '-').slice(0, 60)
+        const isVha = service.fieldFacilityLocatorApiId?.startsWith('vha_')
+        const hasLocationData =
+          service.locations?.[0]?.single?.fieldServiceLocationAddress
+
+        return (
+          <va-accordion-item
+            key={id}
+            data-label={service.name}
+            data-childlabel={service.fieldAlsoKnownAs}
+            data-template="facilities/facilities_health_service"
+            id={id}
+            class="facilities_health_service va-accordion-item"
+            subheader={service.fieldAlsoKnownAs}
+          >
+            <h3 slot="headline">{service.name}</h3>
+            <div id={`${service.entityBundle}-${service.entityId}`}>
+              {service.fieldCommonlyTreatedCondition && (
+                <p className="vads-u-margin-bottom--2">
+                  Common conditions: {service.fieldCommonlyTreatedCondition}
+                </p>
+              )}
+
+              {service.fieldTricareDescription ? (
+                <div>{service.fieldTricareDescription}</div>
+              ) : service.description?.processed ? (
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: service.description.processed,
+                  }}
+                />
+              ) : null}
+
+              {hasLocationData ? (
+                service.locations.map((locationData, i) => (
+                  <ServiceLocation key={i} {...locationData} />
+                ))
+              ) : (
+                <div>{service.localServiceDescription}</div>
+              )}
+
+              {isVha && (
+                <div
+                  data-widget-type="facility-appointment-wait-times-widget"
+                  data-facility={service.fieldFacilityLocatorApiId}
+                  data-service={service.name}
+                />
+              )}
+
+              {service.fieldBody?.processed && (
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: service.fieldBody.processed.replace(/<h3/g, '<h4'),
+                  }}
+                />
+              )}
+            </div>
+          </va-accordion-item>
+        )
+      })}
     </>
   )
 }
