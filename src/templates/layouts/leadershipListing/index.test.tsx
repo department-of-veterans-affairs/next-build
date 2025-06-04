11ff1a9e51e7c9e899f84b4517237b5c7f946234
Brian Seek <brian.seek@civicactions.com>
2025-06-04 08:16:44 -0700

    VACMS-21440: profile teaser (#1071)

--- src/templates/layouts/leadershipListing/index.test.tsx
+++ src/templates/layouts/leadershipListing/index.test.tsx
@@ -13,6 +13,7 @@ describe('LeadershipListing with valid data', () => {
         title={'Hello world'}
         introText={'This is an intro text.'}
         menu={null}
+        profiles={[]}
       />
     )
 
@@ -40,11 +41,67 @@ describe('LeadershipListing with valid data', () => {
         title={'Hello world'}
         menu={mockMenu}
         introText={'This is an intro text.'}
+        profiles={[]}
       />
     )
 
     // @ts-expect-error window.sideNav is not a default window property, but
     // we're adding it
     expect(window.sideNav).toEqual(mockMenu)
+  })
+  test('renders LeadershipListing component with profiles', () => {
+    const mockProfiles = [
+      {
+        id: 'profile-1',
+        firstName: 'John',
+        lastName: 'Doe',
+        vamcTitle: 'Chief of Staff',
+        description: 'Test description',
+        suffix: '',
+        phoneNumber: {
+          id: 'phone-1',
+          number: '123-456-7890',
+          phoneType: 'Office',
+          extension: '',
+          type: 'phone_number',
+        },
+        media: null,
+        link: '/profiles/john-doe',
+      },
+      {
+        id: 'profile-2',
+        firstName: 'Jane',
+        lastName: 'Doe',
+        vamcTitle: 'Also Chief of Staff',
+        description: 'Test description',
+        suffix: '',
+        phoneNumber: {
+          id: 'phone-1',
+          number: '123-456-7891',
+          phoneType: 'Office',
+          extension: '',
+          type: 'phone_number',
+        },
+        media: null,
+        link: '/profiles/jane-doe',
+      },
+    ]
+
+    render(
+      <LeadershipListing
+        id="test-id"
+        type="test-type"
+        published={true}
+        lastUpdated={new Date().toISOString()}
+        title={'Hello world'}
+        menu={null}
+        introText={'This is an intro text.'}
+        profiles={mockProfiles}
+      />
+    )
+
+    mockProfiles.forEach((profile) => {
+      expect(screen.getByText(profile.vamcTitle)).toBeInTheDocument()
+    })
   })
 })
