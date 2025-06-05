11bb453237bc8b3fa264455f6f02c05743fbedc8
Chris Valarida <chris@themostudio.com>
2025-06-05 15:05:34 -0700

    Update comment

--- src/types/drupal/field_type.d.ts
+++ src/types/drupal/field_type.d.ts
@@ -34,7 +34,7 @@ export interface FieldLink {
   options: unknown[]
 }
 export interface FieldOfficeHours {
-  /** Day of the week (e.g., "Monday", "Tuesday", or numeric). */
+  /** Numeric representation of the day of the week (0 is Sunday...I think...) */
   day: number
   /** Start time in military format (0–2359) or undefined; special values (0=midnight, 1200=noon). */
   starthours: number
