fb87325424fb4226e7ddaf6a5e637fc6227e4ae5
Brian Seek <brian.seek@civicactions.com>
2025-06-06 08:08:06 -0700

    VACMS-21441: leadership Lovell (#1072)

--- src/types/formatted/leadershipListing.ts
+++ src/types/formatted/leadershipListing.ts
@@ -1,10 +1,13 @@
 import { PublishedEntity } from './publishedEntity'
 import { SideNavMenu } from './sideNav'
 import { StaffProfileTeaser } from './staffProfile'
+import { LovellChildVariant } from '@/lib/drupal/lovell/types'
 
 export type LeadershipListing = PublishedEntity & {
   title: string
   introText: string
   menu: SideNavMenu | null
   profiles: StaffProfileTeaser[]
+  lovellVariant?: LovellChildVariant
+  lovellSwitchPath?: string
 }
