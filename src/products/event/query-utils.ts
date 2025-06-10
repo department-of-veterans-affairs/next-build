cf8a80e6ec3b62ad949d4c56a10b2622249b8aba
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-06-10 14:06:39 -0700

    Add more of an explanation about why we're getting and sorting next occurences

--- src/products/event/query-utils.ts
+++ src/products/event/query-utils.ts
@@ -7,6 +7,15 @@ import { NodeEvent } from '@/types/drupal/node'
  * filter out any events before the timestamp. Remaining events will then be sorted from
  * earliest to latest.
  *
+ * This function was written to be able to find the next event after a given timestamp.
+ * For recurring events, each event will have multiple entries in the
+ * field_datetime_range_timezone array—some in the past and some in the future. In order
+ * to sort them, we need to be able to compare the "next occurence" of one event with the
+ * "next occurence" of another event. We can't build a query for the Drupal JSON API that
+ * will filter each event's field_datetime_range_timezone before sorting. Therefore we
+ * have to fetch all of the events, figure out the "next occurence" of each event, and
+ * then use that information to sort the events and take the first one.
+ *
  * This is based on [this function from vets-website that drives the event listing page](https://github.com/department-of-veterans-affairs/vets-website/blob/main/src/applications/static-pages/events/helpers/index.js#L78).
  * Originally I recreated a version of that function here, which expanded all recurring
  * events, but I found that really I only needed the next occurence of each event for my
