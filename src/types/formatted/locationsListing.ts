904825193bb3b8f93f63aaeacab9e05f67d3608d
“Anu <anu@themostudio.com>
2025-06-10 19:21:56 -0700

    sidenav data

--- src/types/formatted/locationsListing.ts
+++ src/types/formatted/locationsListing.ts
@@ -1,5 +1,8 @@
 import { PublishedEntity } from './publishedEntity'
+import { SideNavMenu } from '@/types/formatted/sideNav'
 
 export type LocationsListing = PublishedEntity & {
   title: string
+  menu: SideNavMenu
+  path: string
 }
