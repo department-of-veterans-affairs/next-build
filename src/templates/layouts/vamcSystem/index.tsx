c80fe9396e9c12fe46c08b31e4f1fe63b54a45dc
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-06-10 18:43:28 -0700

    Migrate VAMC system page layout to v3 grid

--- src/templates/layouts/vamcSystem/index.tsx
+++ src/templates/layouts/vamcSystem/index.tsx
@@ -52,119 +52,123 @@ export function VamcSystem({
 
   return (
     <div className="va-l-detail-page va-facility-page">
-      <div className="usa-grid usa-grid-full">
-        {/* Nav data fille in by a separate script from `window.sideNav` */}
+      <div className="vads-grid-container">
+        {/* Nav data filled in by a separate script from `window.sideNav` */}
         <nav aria-label="secondary" data-widget-type="side-nav" />
-        {/* Main page content */}
-        <div className="usa-width-three-fourths">
-          <article className="usa-content va-l-facility-detail vads-u-padding-bottom--0">
-            <LovellSwitcher
-              currentVariant={lovellVariant}
-              switchPath={lovellSwitchPath}
-            />
-            {title && <h1>{title}</h1>}
-            {hasValidImage && (
-              <div className="duotone darken lighten medium-screen:vads-u-margin-bottom--0p5">
-                <MediaImage {...image} imageStyle={imageStyle} alt="" />
-              </div>
-            )}
-            <RegionalTopTasks
-              path={path}
-              administration={administration}
-              vamcEhrSystem={vamcEhrSystem}
-            />
-            {introText && (
-              <div className="va-introtext">
-                <p className="vads-u-margin-bottom--0">{introText}</p>
-              </div>
-            )}
-            {/* Locations Section */}
-            <section>
-              <h2 className="vads-u-font-size--xl vads-u-margin-top--3 medium-screen:vads-u-margin-top--5 medium-screen:vads-u-margin-bottom--2p5">
-                Locations
-              </h2>
-              {mainFacilities.map((facility) => (
-                <FacilityListing
-                  key={facility.title}
-                  facility={facility}
-                  basePath={path}
-                />
-              ))}
-              <va-link
-                active
-                className="vads-u-font-size--md vads-u-display--block vads-u-width--full"
-                href={`${path}/locations`}
-                text="See all locations"
-              ></va-link>
-            </section>
-
-            {/* Manage your health online section */}
-            {administration?.entityId !==
-              LOVELL.tricare.administration.entityId && (
+        <div className="vads-grid-row">
+          <div className="vads-grid-col-12">
+            <article className="usa-content va-l-facility-detail vads-u-padding-bottom--0">
+              <LovellSwitcher
+                currentVariant={lovellVariant}
+                switchPath={lovellSwitchPath}
+              />
+              {title && <h1>{title}</h1>}
+              {hasValidImage && (
+                <div className="duotone darken lighten medium-screen:vads-u-margin-bottom--0p5">
+                  <MediaImage {...image} imageStyle={imageStyle} alt="" />
+                </div>
+              )}
+              <RegionalTopTasks
+                path={path}
+                administration={administration}
+                vamcEhrSystem={vamcEhrSystem}
+              />
+              {introText && (
+                <div className="va-introtext">
+                  <p className="vads-u-margin-bottom--0">{introText}</p>
+                </div>
+              )}
+              {/* Locations Section */}
               <section>
-                <h2>
-                  {administration?.entityId ===
-                  LOVELL.va.administration.entityId
-                    ? 'Manage your VA health online'
-                    : 'Manage your health online'}
+                <h2 className="vads-u-font-size--xl vads-u-margin-top--3 medium-screen:vads-u-margin-top--5 medium-screen:vads-u-margin-bottom--2p5">
+                  Locations
                 </h2>
-                <ManageYourHealthLinks vamcEhrSystem={vamcEhrSystem} />
-              </section>
-            )}
-
-            {/* Related links section */}
-            <div className="vads-u-margin-top--5">
-              <RelatedLinks {...relatedLinks} />
-            </div>
-
-            {/* Stories Section */}
-            {featuredStories.length > 0 && (
-              <section>
-                <h2 className="vads-u-margin-bottom--3 medium-screen:vads-u-margin-top--5">
-                  Stories
-                </h2>
-                {featuredStories.slice(0, MAX_FEATURED_STORIES).map((story) => (
-                  <StoryTeaser key={story.id} {...story} />
+                {mainFacilities.map((facility) => (
+                  <FacilityListing
+                    key={facility.title}
+                    facility={facility}
+                    basePath={path}
+                  />
                 ))}
                 <p className="vads-u-margin-y--0">
                   <va-link
                     active
-                    href={`${path}/stories`}
-                    text="See all stories"
+                    href={`${path}/locations`}
+                    text="See all locations"
                   ></va-link>
                 </p>
               </section>
-            )}
-            {/* Events Section */}
-            {(featuredEvents.length > 0 || fallbackEvent) && (
-              <section>
-                <h2 className="vads-u-margin-top--4 vads-u-margin-bottom--2 medium-screen:vads-u-margin-bottom--2p5">
-                  Events
-                </h2>
-                {featuredEvents.length
-                  ? featuredEvents.map((event) => (
-                      <EventTeaser key={event.entityId} {...event} />
-                    ))
-                  : fallbackEvent && (
-                      <EventTeaser
-                        key={fallbackEvent.entityId}
-                        {...fallbackEvent}
-                      />
-                    )}
-                <p className="vads-u-margin-y--0">
-                  <va-link
-                    active
-                    href={`${path}/events`}
-                    text="See all events"
-                  ></va-link>
-                </p>
-              </section>
-            )}
-            {/* Social Links */}
-            <FacilitySocialLinks {...socialLinks} />
-            <va-back-to-top></va-back-to-top>
-            <ContentFooter />
-          </article>
+
+              {/* Manage your health online section */}
+              {administration?.entityId !==
+                LOVELL.tricare.administration.entityId && (
+                <section>
+                  <h2>
+                    {administration?.entityId ===
+                    LOVELL.va.administration.entityId
+                      ? 'Manage your VA health online'
+                      : 'Manage your health online'}
+                  </h2>
+                  <ManageYourHealthLinks vamcEhrSystem={vamcEhrSystem} />
+                </section>
+              )}
+
+              {/* Related links section */}
+              <div className="vads-u-margin-top--5">
+                <RelatedLinks {...relatedLinks} />
+              </div>
+
+              {/* Stories Section */}
+              {featuredStories.length > 0 && (
+                <section>
+                  <h2 className="vads-u-margin-bottom--3 medium-screen:vads-u-margin-top--5">
+                    Stories
+                  </h2>
+                  {featuredStories
+                    .slice(0, MAX_FEATURED_STORIES)
+                    .map((story) => (
+                      <StoryTeaser key={story.id} {...story} />
+                    ))}
+                  <p className="vads-u-margin-y--0">
+                    <va-link
+                      active
+                      href={`${path}/stories`}
+                      text="See all stories"
+                    ></va-link>
+                  </p>
+                </section>
+              )}
+              {/* Events Section */}
+              {(featuredEvents.length > 0 || fallbackEvent) && (
+                <section>
+                  <h2 className="vads-u-margin-top--4 vads-u-margin-bottom--2 medium-screen:vads-u-margin-bottom--2p5">
+                    Events
+                  </h2>
+                  {featuredEvents.length
+                    ? featuredEvents.map((event) => (
+                        <EventTeaser key={event.entityId} {...event} />
+                      ))
+                    : fallbackEvent && (
+                        <EventTeaser
+                          key={fallbackEvent.entityId}
+                          {...fallbackEvent}
+                        />
+                      )}
+                  <p className="vads-u-margin-y--0">
+                    <va-link
+                      active
+                      href={`${path}/events`}
+                      text="See all events"
+                    ></va-link>
+                  </p>
+                </section>
+              )}
+              {/* Social Links */}
+              <FacilitySocialLinks {...socialLinks} />
+              <va-back-to-top></va-back-to-top>
+              <ContentFooter />
+            </article>
+          </div>
         </div>
       </div>
     </div>
