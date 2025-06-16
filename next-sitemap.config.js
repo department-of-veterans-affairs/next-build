baac2f5d1cb6146aae5a5c76b8f617524ad1d2a6
Brian Seek <brian.seek@civicactions.com>
2025-06-16 08:34:44 -0700

    parse files in build output to fetch lastmod from meta DC.Date

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -12,9 +12,20 @@ const sitemapConfig = {
   sitemapBaseFileName: 'sitemap-nb',
   changefreq: false,
   priority: false,
-
   generateRobotsTxt: false, // (optional)
-
+  transform: async (config, path) => {
+    const { JSDOM } = await import('jsdom')
+    const pathToFile = `./out${path}/index.html`
+    const date = JSDOM.fromFile(pathToFile).then((dom) => {
+      return dom.window.document
+        .querySelector("meta[name='DC.Date']")
+        .getAttribute('content')
+    })
+    return {
+      loc: path,
+      lastmod: await date, // static date for now, can be updated later
+    }
+  },
   // todo: migrate to server side sitemap to include last edited date from content for lastmod
   // will need to update static-path-resources to optionally include that field when requested
   // https://www.npmjs.com/package/next-sitemap#server-side-sitemap-getserversidesitemap
