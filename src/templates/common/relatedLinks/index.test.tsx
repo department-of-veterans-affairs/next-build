d8f126e2cf0a1e3ed593751fea2bea2acf0d75c7
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-06-26 14:15:40 -0700

    Send an event via `recordEvents` like the original (#1117)

--- src/templates/common/relatedLinks/index.test.tsx
+++ src/templates/common/relatedLinks/index.test.tsx
@@ -1,6 +1,13 @@
 import { render } from '@testing-library/react'
+import { fireEvent } from '@testing-library/dom'
 import { RelatedLinks } from '.'
 import { FormattedRelatedLinks } from '@/types/formatted/relatedLinks'
+import { recordEvent } from '@/lib/analytics/recordEvent'
+
+// Mock recordEvent
+jest.mock('@/lib/analytics/recordEvent', () => ({
+  recordEvent: jest.fn(),
+}))
 
 describe('RelatedLinks Component', () => {
   test('renders the correct number of links when there are multiple', () => {
@@ -60,5 +67,30 @@ describe('RelatedLinks Component', () => {
     expect(container.innerHTML).toContain(
       'href="https://va.gov/burials-memorials/schedule-a-burial"'
     )
+  })
+
+  test('clicking a link sends correct params to recordEvent', () => {
+    const sectionTitle = 'Related information'
+    const title = 'Eligibility for burial in a VA national cemetery'
+    const relatedLinks: FormattedRelatedLinks = {
+      links: [
+        {
+          uri: 'https://va.gov/burials-memorials/eligibility',
+          title,
+          summary:
+            'Here is a summary for this URL so you can see how it displays underneath.',
+        },
+      ],
+      sectionTitle,
+    }
+    const { container } = render(<RelatedLinks {...relatedLinks} />)
+    const linkEl = container.querySelector('va-link')
+
+    fireEvent.click(linkEl)
+    expect(recordEvent).toHaveBeenCalledWith({
+      event: 'nav-featured-content-link-click',
+      'featured-content-header': sectionTitle,
+      'featured-content-click-label': title,
+    })
   })
 })
