e08ad2580a25a0010ef62de5b3b264d570af44c3
Chris Valarida <chris@themostudio.com>
2025-06-27 12:40:01 -0700

    refactor: flatten health service locations by removing "single" key

--- src/types/formatted/healthCareLocalFacility.ts
+++ src/types/formatted/healthCareLocalFacility.ts
@@ -114,6 +114,8 @@ export interface FormattedVAMCFacilityHealthService {
   /** Flag indicating if this is a mental health service */
   isMentalHealthService?: boolean
   /** Locations associated with this service */
+  // Let's flatten this object to remove the "single" key. Remember to change
+  // the usage in the components and tests as well. AI!
   locations: {
     single: VamcFacilityServiceLocation
   }[]
