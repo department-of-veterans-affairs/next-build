b81eeeba11c24863d7717e39d783c82ccc445ca0
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-07-15 15:41:43 -0700

    Ignore null (archived) services (fixes `/kalispell-vet-center`)

--- src/data/queries/vetCenterHealthServices.ts
+++ src/data/queries/vetCenterHealthServices.ts
@@ -16,26 +16,30 @@ export const formatter: QueryFormatter<
   VetCenterFieldHealthServicesArray,
   FormattedHealthServices
 > = (entities: VetCenterFieldHealthServicesArray): FormattedHealthServices => {
-  const formattedServices = entities.map((item): FormattedHealthService => {
-    const serviceEntity = item.field_service_name_and_descripti
-    if (!serviceEntity) return null
+  const formattedServices = entities
+    .map((item): FormattedHealthService => {
+      // I think this does not fetch when the service is unpublished
+      const serviceEntity = item.field_service_name_and_descripti
+      if (!serviceEntity) return null
 
-    return {
-      name: serviceEntity.name || null,
-      vetCenterTypeOfCare: serviceEntity.field_vet_center_type_of_care || null,
-      vetCenterFriendlyName:
-        serviceEntity.field_vet_center_friendly_name || null,
-      alsoKnownAs: serviceEntity.field_also_known_as || null,
-      vetCenterComConditions:
-        serviceEntity.field_vet_center_com_conditions || null,
-      commonlyTreatedCondition:
-        serviceEntity.field_commonly_treated_condition || null,
-      vetCenterServiceDescription:
-        serviceEntity.field_vet_center_service_descrip || null,
-      description: serviceEntity.description?.processed || null,
-      body: item.field_body?.processed || null,
-    }
-  })
+      return {
+        name: serviceEntity.name || null,
+        vetCenterTypeOfCare:
+          serviceEntity.field_vet_center_type_of_care || null,
+        vetCenterFriendlyName:
+          serviceEntity.field_vet_center_friendly_name || null,
+        alsoKnownAs: serviceEntity.field_also_known_as || null,
+        vetCenterComConditions:
+          serviceEntity.field_vet_center_com_conditions || null,
+        commonlyTreatedCondition:
+          serviceEntity.field_commonly_treated_condition || null,
+        vetCenterServiceDescription:
+          serviceEntity.field_vet_center_service_descrip || null,
+        description: serviceEntity.description?.processed || null,
+        body: item.field_body?.processed || null,
+      }
+    })
+    .filter(Boolean)
 
   // Sort services alphabetically by name
   formattedServices.sort((a, b) => {
