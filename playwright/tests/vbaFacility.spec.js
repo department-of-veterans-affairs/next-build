e6706403647d127c244028fbd1a8a3da29d48d4b
Brian Seek <brian.seek@civicactions.com>
2025-07-18 16:56:51 -0700

    update playwrite test/.

--- playwright/tests/vbaFacility.spec.js
+++ playwright/tests/vbaFacility.spec.js
@@ -1,17 +1,23 @@
 import { test, expect } from '../utils/next-test'
 
 // This test should be modified to test page output and function.
-test.skip('Vba_facility', () => {
+test('Vba_facility', () => {
   test('Vba_facility page renders', async ({ page }) => {
-    await page.goto('/update-this-link')
-    await expect(page).toHaveURL('/update-this-link')
+    await page.goto(
+      '/togus-va-regional-benefit-office-at-togus-va-medical-center'
+    )
+    await expect(page).toHaveURL(
+      '/togus-va-regional-benefit-office-at-togus-va-medical-center'
+    )
   })
 
   test('Should render without a11y errors', async ({
     page,
     makeAxeBuilder,
   }) => {
-    await page.goto('/update-this-link')
+    await page.goto(
+      '/togus-va-regional-benefit-office-at-togus-va-medical-center'
+    )
 
     const accessibilityScanResults = await makeAxeBuilder().analyze()
 
