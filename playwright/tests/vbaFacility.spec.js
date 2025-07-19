763b34fd4e69b79e36c24aca2843adc59db51436
Brian Seek <brian.seek@civicactions.com>
2025-07-18 17:24:28 -0700

    describe

--- playwright/tests/vbaFacility.spec.js
+++ playwright/tests/vbaFacility.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '../utils/next-test'
 
 // This test should be modified to test page output and function.
-test('Vba_facility', () => {
+test.describe('Vba_facility', () => {
   test('Vba_facility page renders', async ({ page }) => {
     await page.goto(
       '/togus-va-regional-benefit-office-at-togus-va-medical-center'
