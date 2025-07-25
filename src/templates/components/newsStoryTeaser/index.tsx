fc300199af40da1af09da99ec6c3bb85092ab82d
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-07-24 17:37:43 -0700

    Apply this layout component to the news story teaser too

--- src/templates/components/newsStoryTeaser/index.tsx
+++ src/templates/components/newsStoryTeaser/index.tsx
@@ -1,6 +1,7 @@
 import { MediaImage } from '@/templates/common/mediaImage'
 import { truncateWordsOrChar } from '@/lib/utils/helpers'
 import { NewsStoryTeaser as FormattedNewsStoryTeaser } from '@/products/newsStory/formatted-type'
+import { TextWithImage } from '@/templates/components/textWithImage'
 
 /** Teaser news story. */
 export const NewsStoryTeaser = ({
@@ -16,22 +17,17 @@ export const NewsStoryTeaser = ({
   }
 
   return (
-    <>
-      <div className="vads-grid-container vads-u-padding-x--0 vads-u-margin-bottom--3 tablet:vads-u-margin-bottom--4">
-        <div className="vads-grid-row">
-          <div className="tablet:vads-grid-col-8 tablet:vads-u-padding-right--3">
-            <TitleTag className="vads-u-margin-top--0 vads-u-font-size--md tablet:vads-u-font-size--lg tablet:vads-u-margin-bottom--0p5">
-              <va-link href={link} text={title} />
-            </TitleTag>
-            <p className="vads-u-margin-y--0">
-              {truncateWordsOrChar(introText, 60, true)}
-            </p>
-          </div>
-          <div className="tablet:vads-grid-col-4 stories-list vads-u-order--first tablet:vads-u-order--initial vads-u-margin-bottom--2 tablet:vads-u-margin-bottom--0">
-            <MediaImage {...image} imageStyle="2_1_large" />
-          </div>
-        </div>
-      </div>
-    </>
+    <TextWithImage
+      className="vads-u-margin-bottom--3 tablet:vads-u-margin-bottom--4"
+      image={<MediaImage {...image} imageStyle="2_1_large" />}
+      imageClassName="stories-list"
+    >
+      <TitleTag className="vads-u-margin-top--0 vads-u-font-size--md tablet:vads-u-font-size--lg tablet:vads-u-margin-bottom--0p5">
+        <va-link href={link} text={title} />
+      </TitleTag>
+      <p className="vads-u-margin-y--0">
+        {truncateWordsOrChar(introText, 60, true)}
+      </p>
+    </TextWithImage>
   )
 }
