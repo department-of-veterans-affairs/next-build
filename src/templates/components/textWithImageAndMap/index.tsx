5367b0a3f4f9d6ac790467143bf5ed057024a168
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-08-04 15:22:31 -0700

    Absorb the `ImageAndStaticMap` component into the `TextWithImageAndMap` component

--- src/templates/components/textWithImageAndMap/index.tsx
+++ src/templates/components/textWithImageAndMap/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import clsx from 'clsx'
 import { TextWithImage, TextWithImageProps } from '../textWithImage'
-import { ImageAndStaticMap } from '../imageAndStaticMap'
 import { MediaImage as FormattedMediaImage } from '@/types/formatted/media'
+import { MediaImage } from '@/templates/common/mediaImage'
 
 type TextWithImageAndMapProps = Omit<TextWithImageProps, 'image'> & {
   imageProps: FormattedMediaImage
@@ -10,13 +10,19 @@ type TextWithImageAndMapProps = Omit<TextWithImageProps, 'image'> & {
 }
 
 /**
- * The old CSS for the `ImageAndStaticMap` component used in production is pretty hacky.
- * Because the `ImageAndStaticMap` elements have a "facility-img" class that applies a
- * minimum width, our column needs to be able to grow to contain it. This was being
- * achieved in a hacky way by setting the image column's display property to
- * `inline-table`, whereas a more flexbox-friendly way would be to set the `flex-grow`
- * to 1 and the `flex-basis` to `fit-content`. We don't have a VADS utility class for
- * `flex-basis`. An even better way would be to not have a minimum width on the image.
+ * This wrapper encapsulates special layout CSS necessary for the image and static map
+ * widget to not break the flex layout. Both the CSS being applied in the widget and the
+ * parent layout CSS have to work together to not break things, so we're putting that
+ * tight coupling into a single component.
+ *
+ * More details about the CSS:
+ * The old CSS for the image and static map widget used in production is pretty hacky.
+ * The image and map elements in this widget have a "facility-img" class that applies a
+ * minimum width, so our column needs to be able to grow to contain it. Previously, this
+ * was achieved in a hacky way by setting the image column's display property to
+ * `inline-table`. A more flexbox-friendly approach would be to set `flex-grow` to 1 and
+ * `flex-basis` to `fit-content`, but we don't have a VADS utility class for
+ * `flex-basis`. Ideally, there would be no minimum width on the image at all.
  */
 export const TextWithImageAndMap = ({
   children,
@@ -26,7 +32,18 @@ export const TextWithImageAndMap = ({
   ...otherProps
 }: TextWithImageAndMapProps) => (
   <TextWithImage
-    image={<ImageAndStaticMap image={imageProps} facilityId={facilityId} />}
+    image={
+      <>
+        <MediaImage
+          {...imageProps}
+          imageStyle="3_2_medium_thumbnail"
+          className="facility-img"
+        />
+        <div data-widget-type="facility-map" data-facility={facilityId}>
+          {/* TODO: Create Facility Map component for display here */}
+        </div>
+      </>
+    }
     imageClassName={clsx(imageClassName, 'inline-table-helper')}
     {...otherProps}
   >
