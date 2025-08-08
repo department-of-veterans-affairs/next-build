1518fc0017b4092c184e8f7862abda39599fdb6f
Christian Burk <omahane@gmail.com>
2025-08-08 06:54:54 -0500

    VACMS-21459: Reduces grep complexity

--- scripts/check-broken-links.js
+++ scripts/check-broken-links.js
@@ -53,8 +53,8 @@ const LINKS_TO_SKIP = [
   'www.choicehotels.com',
   'www.googletagmanager.com',
   /visn\d+.*?\.va\.gov/,
-  /^.*fb\.(com|me|watch).*/,
-  /^.*www.facebook.com.*/,
+  /fb\.(com|me|watch)/,
+  /www\.facebook\.com/,
   // process.env.SKIP_IMAGES ? '' : null
 ]
 
