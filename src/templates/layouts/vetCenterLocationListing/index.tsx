26fa9dcf0aa715ad0597ba512f653b47cc4a58af
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-08-14 12:14:42 -0700

    Migrate all pre-v3 classes
    Also simplified the wrapper structure like we did for the main vets page

--- src/templates/layouts/vetCenterLocationListing/index.tsx
+++ src/templates/layouts/vetCenterLocationListing/index.tsx
@@ -50,83 +50,79 @@ export function VetCenterLocationListing({
     mobileVetCenters.length > 0
 
   return (
-    <div className="va-l-detail-page va-facility-page">
-      <div className="usa-grid usa-grid-full">
-        <div className="usa-width-three-fourths">
-          <article className="usa-content va-l-facility-detail vads-u-padding-bottom--0">
-            {title && <h1>{title}</h1>}
+    <div className="vads-grid-container">
+      <article className="usa-content vads-u-padding-bottom--0">
+        {title && <h1>{title}</h1>}
 
+        <h2
+          className="vads-u-margin-top--0 vads-u-margin-bottom--2p5 tablet:vads-u-margin-bottom--3"
+          id="main-location"
+        >
+          Main location
+        </h2>
+        <VetCenterLocationInfo vetCenter={mainOffice} isMainOffice={true} />
+
+        {showSatelliteLocations && (
+          <>
             <h2
-              className="vads-u-margin-top--0 vads-u-margin-bottom--2p5 medium-screen:vads-u-margin-bottom--3"
-              id="main-location"
+              className="vads-u-margin-top--3 tablet:vads-u-margin-top--5 vads-u-margin-bottom--2p5 tablet:vads-u-margin-bottom--4"
+              id="satellite-locations"
             >
-              Main location
+              Satellite locations
             </h2>
-            <VetCenterLocationInfo vetCenter={mainOffice} isMainOffice={true} />
-
-            {showSatelliteLocations && (
-              <>
-                <h2
-                  className="vads-u-margin-top--3 medium-screen:vads-u-margin-top--5 vads-u-margin-bottom--2p5 medium-screen:vads-u-margin-bottom--4"
-                  id="satellite-locations"
-                >
-                  Satellite locations
-                </h2>
-                <p className="vads-u-margin-bottom--2p5 medium-screen:vads-u-margin-bottom--4">
-                  If you can’t make it to our {mainOffice.title} we offer
-                  satellite locations that may be closer to you. These satellite
-                  facilities provide select services with the same community,
-                  care, and confidentiality in a non-medical setting. Call us
-                  for more information about these locations.
-                </p>
-                {satelliteLocations.map((location) => (
-                  <VetCenterLocationInfo
-                    key={location.id}
-                    vetCenter={location}
-                    mainOffice={mainOffice}
-                  />
-                ))}
-
-                {mobileVetCenters.map((location) => (
-                  <VetCenterLocationInfo
-                    key={location.id}
-                    vetCenter={location}
-                    mainOffice={mainOffice}
-                  />
-                ))}
-
-                {nearbyMobileVetCenters.map((location) => (
-                  <VetCenterLocationInfo
-                    key={location.id}
-                    vetCenter={location}
-                    mainOffice={mainOffice}
-                  />
-                ))}
-              </>
-            )}
-
-            <div data-widget-type="vet-center-nearby" />
-
-            <h2
-              className="vads-u-margin-top--3 medium-screen:vads-u-margin-top--5 vads-u-margin-bottom--2p5 medium-screen:vads-u-margin-bottom--3"
-              id="vet-centers-other-areas"
-            >
-              Vet Centers in other areas
-            </h2>
-            <p className="vads-u-font-size--base">
-              Looking for a Vet Center in another area?{' '}
-              <va-link
-                href="/find-locations"
-                text="Find a Vet Center location"
-              ></va-link>
+            <p className="vads-u-margin-bottom--2p5 tablet:vads-u-margin-bottom--4">
+              If you can’t make it to our {mainOffice.title} we offer satellite
+              locations that may be closer to you. These satellite facilities
+              provide select services with the same community, care, and
+              confidentiality in a non-medical setting. Call us for more
+              information about these locations.
             </p>
+            {satelliteLocations.map((location) => (
+              <VetCenterLocationInfo
+                key={location.id}
+                vetCenter={location}
+                mainOffice={mainOffice}
+              />
+            ))}
 
-            <va-back-to-top></va-back-to-top>
+            {mobileVetCenters.map((location) => (
+              <VetCenterLocationInfo
+                key={location.id}
+                vetCenter={location}
+                mainOffice={mainOffice}
+              />
+            ))}
 
-            <ContentFooter />
-          </article>
-        </div>
-      </div>
+            {nearbyMobileVetCenters.map((location) => (
+              <VetCenterLocationInfo
+                key={location.id}
+                vetCenter={location}
+                mainOffice={mainOffice}
+              />
+            ))}
+          </>
+        )}
+
+        <div data-widget-type="vet-center-nearby" />
+
+        <h2
+          className="vads-u-margin-top--3 tablet:vads-u-margin-top--5 vads-u-margin-bottom--2p5 tablet:vads-u-margin-bottom--3"
+          id="vet-centers-other-areas"
+        >
+          Vet Centers in other areas
+        </h2>
+        <p className="vads-u-font-size--base">
+          Looking for a Vet Center in another area?{' '}
+          <va-link
+            href="/find-locations"
+            text="Find a Vet Center location"
+          ></va-link>
+        </p>
+
+        <va-back-to-top></va-back-to-top>
+
+        <ContentFooter />
+      </article>
     </div>
   )
 }
