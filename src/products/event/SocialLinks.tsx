0a78081a847c7b06faff70105cffe1e9ba8d039f
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-08-18 14:31:13 -0700

    Replace the shared SocialLinks component with some other shared functions
    It turns out that the social links for events and news stories (the only two places that used the `SocialLinks` component) were rendered differently depending on whether it was an event or news story. All the VBA logic wasn't being used. Therefore it made more sense to have different components for events and news stories and just share some logic between them for building links.
    
    See https://github.com/department-of-veterans-affairs/va.gov-cms/issues/21955 for some additional context
    
    I also included a function borrowed from the VBA formatter in this social utils module that I'm planning to use in a later commit

---
+++ src/products/event/SocialLinks.tsx
@@ -0,0 +1,62 @@
+import { getFacebookLink, getXLink } from '@/lib/utils/social'
+
+interface SocialLinksProps {
+  path: string
+  title: string
+  description?: string
+  address?: string
+  dateObject?: {
+    endValue: number
+    value: number
+  }
+}
+
+export const SocialLinks = ({
+  path,
+  title,
+  description,
+  address,
+  dateObject,
+}: SocialLinksProps) => {
+  const facebookLink = getFacebookLink(path)
+  const xLink = getXLink(path, title)
+
+  return (
+    <div id="va-c-social-share">
+      <p className="vads-u-margin-y--2p5">
+        <va-icon
+          class="va-c-social-icon vads-u-margin-right--0p5"
+          icon="calendar_today"
+          size="3"
+        />
+        <va-link
+          data-description={description}
+          data-end={dateObject?.endValue}
+          data-location={address}
+          data-start={dateObject?.value}
+          data-subject={title}
+          href={path}
+          id="add-to-calendar-link"
+          text="Add to Calendar"
+        />
+      </p>
+
+      <p className="vads-u-margin-y--2p5">
+        <va-icon
+          class="va-c-social-icon vads-u-margin-right--0p5"
+          icon={facebookLink.icon}
+          size="3"
+        />
+        <va-link href={facebookLink.href} text={facebookLink.text} />
+      </p>
+      <p>
+        <va-icon
+          class="va-c-social-icon vads-u-margin-right--0p5"
+          icon={xLink.icon}
+          size="3"
+        />
+        <va-link href={xLink.href} text={xLink.text} />
+      </p>
+    </div>
+  )
+}
