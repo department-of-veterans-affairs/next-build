e729bccf61091a3c62b72e1aa4c81f8c80caa728
Chris Valarida <chris@themostudio.com>
2025-08-25 12:26:12 -0700

    Continue to try fixing the TS errors...
    I dunno, man...stuff still got weird? We're close here, though. Still a
    couple type errors nested deep in the component library react bindings,
    though.

--- additional.d.ts
+++ additional.d.ts
@@ -20,22 +20,4 @@ declare module 'mq-polyfill'
 declare module 'nock'
 declare module 'test-utils'
 
-declare namespace React.JSX {
-  interface IntrinsicElements {
-    'va-accordion': any
-    'va-accordion-item': any
-    'va-additional-info': any
-    'va-alert': any
-    'va-alert-expandable': any
-    'va-back-to-top': any
-    'va-breadcrumbs': any
-    'va-button': any
-    'va-card': any
-    'va-icon': any
-    'va-link': any
-    'va-link-action': any
-    'va-on-this-page': any
-    'va-summary-box': any
-    'va-telephone': any
-  }
-}
+
---
+++ src/types/jsx.d.ts
@@ -0,0 +1,23 @@
+import 'react'
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'va-accordion': any
+      'va-accordion-item': any
+      'va-additional-info': any
+      'va-alert': any
+      'va-alert-expandable': any
+      'va-back-to-top': any
+      'va-breadcrumbs': any
+      'va-button': any
+      'va-card': any
+      'va-icon': any
+      'va-link': any
+      'va-link-action': any
+      'va-on-this-page': any
+      'va-summary-box': any
+      'va-telephone': any
+    }
+  }
+}
