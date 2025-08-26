0a8ee9c64937ea57cb776c5b8069db7603bdfbf0
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-08-25 17:28:48 -0700

    Move the tests and mocks for "paragraph--rich_text_char_limit_1000", which is implemented with the WYSIWYG component

--- src/components/wysiwyg/query.test.ts
+++ src/components/wysiwyg/query.test.ts
@@ -4,15 +4,28 @@
 
 import { queries } from '@/data/queries'
 import { ParagraphWysiwyg } from '@/types/drupal/paragraph'
-import mockData from '@/components/wysiwyg/mock.json'
+import { ParagraphRichTextCharLimit1000 } from '@/types/drupal/paragraph'
+import wysiwygMockData from './mock.json'
+import richTextMockData from './mock.richTextCharLimit1000.json'
 
-const wysiwygMock: ParagraphWysiwyg[] = mockData
+const wysiwygMock: ParagraphWysiwyg[] = wysiwygMockData
+const richTextMock: ParagraphRichTextCharLimit1000[] = richTextMockData
 
 describe('paragraph--wysiwyg formatData', () => {
   test('outputs formatted data', () => {
     expect(
       wysiwygMock.map((mock) => {
         return queries.formatData('paragraph--wysiwyg', mock)
+      })
+    ).toMatchSnapshot()
+  })
+})
+
+describe('paragraph--rich_text_char_limit_1000 formatData', () => {
+  test('outputs formatted data', () => {
+    expect(
+      richTextMock.map((mock) => {
+        return queries.formatData('paragraph--rich_text_char_limit_1000', mock)
       })
     ).toMatchSnapshot()
   })
