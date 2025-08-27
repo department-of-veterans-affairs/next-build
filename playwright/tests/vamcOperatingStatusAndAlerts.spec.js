f87affe9a4f72500dce3069c05ca1da9d10cf007
Brian Seek <brian.seek@civicactions.com>
2025-08-27 16:41:30 -0700

    update playwrite

--- playwright/tests/vamcOperatingStatusAndAlerts.spec.js
+++ playwright/tests/vamcOperatingStatusAndAlerts.spec.js
@@ -1,18 +1,17 @@
 import { test, expect } from '../utils/next-test'
 
 // This test should be modified to test page output and function.
-// test.describe('Vamc_operating_status_and_alerts', () => {
-test.skip('Vamc_operating_status_and_alerts', () => {
+test.describe('Vamc_operating_status_and_alerts', () => {
   test('Vamc_operating_status_and_alerts page renders', async ({ page }) => {
-    await page.goto('/update-this-link')
-    await expect(page).toHaveURL('/update-this-link')
+    await page.goto('/tomah-health-care/operating-status/')
+    await expect(page).toHaveURL('/tomah-health-care/operating-status/')
   })
 
   test('Should render without a11y errors', async ({
     page,
     makeAxeBuilder,
   }) => {
-    await page.goto('/update-this-link')
+    await page.goto('/tomah-health-care/operating-status/')
 
     const accessibilityScanResults = await makeAxeBuilder().analyze()
 
