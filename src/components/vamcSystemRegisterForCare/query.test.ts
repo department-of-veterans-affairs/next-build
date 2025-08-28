4fe02886fc34ff0c0bf4a9e0ea96835d41bb03b5
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-08-28 11:28:37 -0700

    Add the side nav

--- src/components/vamcSystemRegisterForCare/query.test.ts
+++ src/components/vamcSystemRegisterForCare/query.test.ts
@@ -1,14 +1,23 @@
 import { formatter } from './query'
 import mockData from './mock.json'
+import mockMenu from './mock.menu.json'
+import { Menu } from '@/types/drupal/menu'
 
 describe('VamcSystemRegisterForCare formatter', () => {
   it('formats basic fields correctly', () => {
-    const result = formatter({ entity: mockData })
+    const result = formatter({
+      entity: mockData,
+      menu: mockMenu as unknown as Menu,
+    })
 
     expect(result.title).toBe('Register for care')
     expect(result.entityId).toBe(44606)
     expect(result.entityPath).toBe('/richmond-health-care/register-for-care')
     expect(result.vamcSystem.title).toBe('VA Richmond health care')
+    expect(result.menu).toBeDefined()
+    expect(result.menu.rootPath).toBe(
+      '/richmond-health-care/register-for-care/'
+    )
   })
 
   // it('formats centralized content fields', () => {
