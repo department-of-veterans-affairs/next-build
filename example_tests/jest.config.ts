e1667f8f330393f7e27b676d355dd89b1c8da53e
Brian Seek <brian.seek@civicactions.com>
2025-09-08 12:16:06 -0700

    update example jest config.

--- example_tests/jest.config.ts
+++ example_tests/jest.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const parentDir = path.dirname(__dirname)
 
