85dd2a9e64388c971a24a17fa79edb13987b18ea
Jeff Keene <jkeene@metrostar.com>
2025-09-15 11:45:10 -0400

    removed can init apm check

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,4 @@
 import { canInitDatadogApm } from './datadogConnector/utils/canInitDatadogApm'
 export async function register() {
-  if (canInitDatadogApm()) {
-    await import('./datadogConnector/DatadogApmConnector')
-  }
+  await import('./datadogConnector/DatadogApmConnector')
 }
