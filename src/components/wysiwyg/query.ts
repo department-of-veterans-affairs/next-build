3df886d20cfd98d596790b0f7005be5ead3a8d03
Chris Valarida <chris@themostudio.com>
2025-09-22 10:46:52 -0700

    Add optional `options` to WYSIWYG formatter

--- src/components/wysiwyg/query.ts
+++ src/components/wysiwyg/query.ts
@@ -5,13 +5,17 @@ import {
 import { QueryFormatter } from 'next-drupal-query'
 import { Wysiwyg } from '@/components/wysiwyg/formatted-type'
 import { getHtmlFromField } from '@/lib/utils/getHtmlFromField'
+import { GetHtmlFromDrupalContentOptions } from '@/lib/utils/getHtmlFromDrupalContent'
 
 // Define the formatter for returning paragraph--wysiwyg & paragraph--rich_text_char_limit_1000 data.
 export const formatter: QueryFormatter<
   ParagraphWysiwyg | ParagraphRichTextCharLimit1000,
   Wysiwyg
-> = (entity: ParagraphWysiwyg | ParagraphRichTextCharLimit1000) => {
-  const html = getHtmlFromField(entity.field_wysiwyg)
+> = (
+  entity: ParagraphWysiwyg | ParagraphRichTextCharLimit1000,
+  options?: GetHtmlFromDrupalContentOptions
+) => {
+  const html = getHtmlFromField(entity.field_wysiwyg, options)
 
   return {
     type: entity.type as
