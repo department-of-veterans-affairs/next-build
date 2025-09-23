4a092e72c43f3f1c25981926f3d4f42a2ab93da3
Chris Valarida <chris@themostudio.com>
2025-09-23 12:33:18 -0700

    Add comment explaining why we expect a TS error

--- src/components/vamcSystemPoliciesPage/mock.ts
+++ src/components/vamcSystemPoliciesPage/mock.ts
@@ -1,4 +1,6 @@
 import { NodeVamcSystemPoliciesPage } from '@/types/drupal/node'
 import mockData from './mock.json'
 
-export default mockData as unknown as NodeVamcSystemPoliciesPage
+// @ts-expect-error field_office isn't hydrated in the mock data. We only use it
+// for fetching the menu, but we're mocking that call in the test.
+export default mockData as NodeVamcSystemPoliciesPage
