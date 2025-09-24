6c078703583cb6cd674ca641710bb6bb34c81162
Chris Valarida <chris@themostudio.com>
2025-09-24 14:41:44 -0700

    Add lovell properties to the formatter

--- src/components/vamcSystemPoliciesPage/query.ts
+++ src/components/vamcSystemPoliciesPage/query.ts
@@ -18,6 +18,10 @@ import { FieldCCText } from '@/types/drupal/field_type'
 import { Wysiwyg } from '../wysiwyg/formatted-type'
 import { Menu } from '@/types/drupal/menu'
 import { buildSideNavDataFromMenu } from '@/lib/drupal/facilitySideNav'
+import {
+  getLovellVariantOfUrl,
+  getOppositeChildVariant,
+} from '@/lib/drupal/lovell/utils'
 
 // Define the query params for fetching node--vamc_system_policies_page.
 export const params: QueryParams<null> = () => {
@@ -70,7 +74,7 @@ export const data: QueryData<
 export const formatter: QueryFormatter<
   VamcSystemPoliciesPageData,
   VamcSystemPoliciesPage
-> = ({ entity, menu }) => {
+> = ({ entity, menu, lovell }) => {
   const formatCcWysiwyg = (field: FieldCCText) =>
     formatParagraph(normalizeEntityFetchedParagraphs(field)) as Wysiwyg
 
@@ -89,6 +93,13 @@ export const formatter: QueryFormatter<
     bottomOfPageContent: formatCcWysiwyg(
       entity.field_cc_bottom_of_page_content
     ),
+    lovellVariant: lovell?.variant,
+    lovellSwitchPath: lovell?.variant
+      ? getLovellVariantOfUrl(
+          entity.path.alias,
+          getOppositeChildVariant(lovell?.variant)
+        )
+      : null,
   }
 
   return formattedEntity
