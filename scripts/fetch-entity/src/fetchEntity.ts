55dc48383be15486fe29f0393abb65e1bfe5b657
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-09-24 17:18:11 -0700

    Make it so we can just call the `entityFetch` function and get back json

--- scripts/fetch-entity/src/fetchEntity.ts
+++ scripts/fetch-entity/src/fetchEntity.ts
@@ -1,5 +1,4 @@
 import { DrupalJsonApiParams } from 'drupal-jsonapi-params'
-import util from 'util'
 import { deflateObjectGraph } from '../../../src/lib/utils/object-graph'
 
 import { getEnvFileVars } from './getEnvVars'
@@ -28,7 +27,6 @@ export async function fetchEntity(
   uuid?: string,
   options: {
     include?: string[]
-    json?: boolean
     deflate?: boolean
     collection?: boolean
     limit?: string
@@ -73,19 +71,5 @@ export async function fetchEntity(
     data = deflateObjectGraph(data)
   }
 
-  // NOTE: There may be a thing we can do to just _check_ for circular
-  // references. console.log(util.format('%j', data)) will output just
-  // "[Circular]" if there are circular references.
-
-  if (options.json) {
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify(data, null, 2))
-  } else {
-    // If we're printing to the terminal, pretty print with colors.
-    // NOTE: This is not proper JSON. (No quotes around property names.)
-    // eslint-disable-next-line no-console
-    console.log(
-      util.inspect(data, { depth: null, colors: process.stdout.isTTY })
-    )
-  }
+  return data
 }
