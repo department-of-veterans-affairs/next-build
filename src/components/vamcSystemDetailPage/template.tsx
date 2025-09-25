0e198bc1c9a003a3c7e49bcd5eec1c650f8d187a
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-09-25 15:54:46 -0700

    Okay, this looks like a page

--- src/components/vamcSystemDetailPage/template.tsx
+++ src/components/vamcSystemDetailPage/template.tsx
@@ -1,69 +1,91 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { VamcSystemDetailPage as FormattedVamcSystemDetailPage } from './formatted-type'
 import { LovellStaticPropsResource } from '@/lib/drupal/lovell/types'
 import { LovellSwitcher } from '../lovellSwitcher/template'
 import { ListOfLinkTeasers } from '../listOfLinkTeasers/template'
 import { ContentFooter } from '../contentFooter/template'
 
-export function VamcSystemDetailPage({ entityPath, title, introText, showTableOfContents, menu, lovellVariant, lovellSwitchPath, relatedLinks, lastUpdated }: LovellStaticPropsResource<FormattedVamcSystemDetailPage>) {
+export function VamcSystemDetailPage({
+  entityPath,
+  title,
+  introText,
+  showTableOfContents,
+  menu,
+  lovellVariant,
+  lovellSwitchPath,
+  relatedLinks,
+  lastUpdated,
+}: LovellStaticPropsResource<FormattedVamcSystemDetailPage>) {
   const isContactPage = entityPath?.includes('contact-us') || false
 
+  // Populate the side nav data for the side nav widget to fill in
+  // Note: The side nav widget is in a separate app in the static-pages bundle
+  useEffect(() => {
+    window.sideNav = menu
+  }, [menu])
+
   return (
-    <article 
-      aria-labelledby="article-heading" 
-      role="region" 
-      className="usa-content" 
-      data-template="vamc-system-detail-page"
+    <div
+      className="vads-grid-container"
+      data-template="vamc_system_detail_page"
     >
-      <LovellSwitcher
-        currentVariant={lovellVariant}
-        switchPath={lovellSwitchPath}
-      />
+      {/* Nav data filled in by a separate script from `window.sideNav` */}
+      <nav aria-label="secondary" data-widget-type="side-nav" />
+      <div className="vads-grid-row">
+        <div className="vads-grid-col-12"></div>
+        <article
+          aria-labelledby="article-heading"
+          role="region"
+          className="usa-content"
+          data-template="vamc-system-detail-page"
+        >
+          <LovellSwitcher
+            currentVariant={lovellVariant}
+            switchPath={lovellSwitchPath}
+          />
 
-      <h1 id="article-heading">{title}</h1>
-      
-      {introText && (
-        <div className="va-introtext">
-          <p>{introText}</p>
-        </div>
-      )}
+          <h1 id="article-heading">{title}</h1>
 
-      {/* Main action buttons for contact pages */}
-      {isContactPage && (
-        <div className="usa-grid usa-grid-full vads-u-margin-y--1p5">
-          <div>TODO: Main action buttons</div>
-        </div>
-      )}
+          {introText && (
+            <div className="va-introtext">
+              <p>{introText}</p>
+            </div>
+          )}
 
-      {/* Alerts */}
-      <div>TODO: Alerts component</div>
+          {/* Main action buttons for contact pages */}
+          {isContactPage && (
+            <div className="usa-grid usa-grid-full vads-u-margin-y--1p5">
+              <div>TODO: Main action buttons</div>
+            </div>
+          )}
 
-      {/* Table of contents */}
-      {showTableOfContents && (
-        <va-on-this-page></va-on-this-page>
-      )}
+          {/* Alerts */}
+          <div>TODO: Alerts component</div>
 
-      {/* Featured content */}
-      <div>TODO: Featured content component</div>
+          {/* Table of contents */}
+          {showTableOfContents && <va-on-this-page></va-on-this-page>}
 
-      {/* Main content blocks */}
-      <div>TODO: Main content blocks component</div>
+          {/* Featured content */}
+          <div>TODO: Featured content component</div>
 
-      {/* Related links */}
-      {relatedLinks && (
-        <div className="va-nav-linkslist va-nav-linkslist--related">
-          <ListOfLinkTeasers {...relatedLinks} />
-        </div>
-      )}
+          {/* Main content blocks */}
+          <div>TODO: Main content blocks component</div>
 
-      {/* Social links for contact pages */}
-      {isContactPage && (
-        <div>TODO: Social links component</div>
-      )}
+          {/* Related links */}
+          {relatedLinks && (
+            <div className="va-nav-linkslist va-nav-linkslist--related">
+              <ListOfLinkTeasers {...relatedLinks} />
+            </div>
+          )}
 
-      <va-back-to-top></va-back-to-top>
-      <ContentFooter lastUpdated={lastUpdated} />
-    </article>
+          {/* Social links for contact pages */}
+          {isContactPage && <div>TODO: Social links component</div>}
+
+          <va-back-to-top></va-back-to-top>
+          <ContentFooter lastUpdated={lastUpdated} />
+        </article>
+      </div>
+    </div>
   )
 }
 
