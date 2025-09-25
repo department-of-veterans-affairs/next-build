49ce2cef5ffec5e01cb557081935bc3c8dec2bff
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-09-25 16:26:28 -0700

    Add some more unit tests for the template

--- src/components/vamcSystemDetailPage/template.test.tsx
+++ src/components/vamcSystemDetailPage/template.test.tsx
@@ -6,6 +6,7 @@ import mockMenu from './mock.menu.json'
 import { Menu } from '@/types/drupal/menu'
 import { NodeVamcSystemDetailPage } from '@/types/drupal/node'
 import { formatter } from './query'
+import { LOVELL } from '@/lib/drupal/lovell/constants'
 
 describe('VamcSystemDetailPage', () => {
   const formattedMockData = formatter({
@@ -26,5 +27,70 @@ describe('VamcSystemDetailPage', () => {
         "Explore VA Bronx's research initiatives with specialty programs in [List research here] . You can also volunteer to participate in a research study."
       )
     ).toBeInTheDocument()
+  })
+
+  test('adds the sideNav to window.sideNav', () => {
+    render(<VamcSystemDetailPage {...formattedMockData} />)
+    expect(window.sideNav).toEqual(formattedMockData.menu)
+  })
+
+  describe('Related links', () => {
+    it('renders list of link teasers when related links are provided', () => {
+      render(<VamcSystemDetailPage {...formattedMockData} />)
+
+      // Check that the related links section is rendered
+      expect(screen.getByText('More information')).toBeInTheDocument()
+      expect(
+        screen.getByText(/You should keep copies of your medical/)
+      ).toBeInTheDocument()
+    })
+
+    it('does not render list of link teasers when related links are null', () => {
+      render(
+        <VamcSystemDetailPage {...formattedMockData} relatedLinks={null} />
+      )
+
+      // Check that the related links section is not rendered
+      expect(screen.queryByText('More information')).not.toBeInTheDocument()
+      expect(
+        screen.queryByText(/You should keep copies of your medical/)
+      ).not.toBeInTheDocument()
+    })
+  })
+
+  describe('LovellSwitcher integration', () => {
+    test('renders LovellSwitcher for TRICARE variant', () => {
+      const dataWithLovell = {
+        ...formattedMockData,
+        lovellVariant: LOVELL.tricare.variant,
+        lovellSwitchPath: '/lovell-federal-health-care-va',
+      }
+
+      const { container } = render(<VamcSystemDetailPage {...dataWithLovell} />)
+
+      // Look for the LovellSwitcher content
+      expect(
+        screen.getByText('You are viewing this page as a TRICARE beneficiary.')
+      ).toBeInTheDocument()
+
+      // Check for the va-link component with the correct text attribute
+      const switcherLink = container.querySelector(
+        'va-alert va-link[text="View this page as a VA beneficiary"]'
+      )
+      expect(switcherLink).toBeInTheDocument()
+      expect(switcherLink).toHaveAttribute(
+        'href',
+        dataWithLovell.lovellSwitchPath
+      )
+    })
+
+    test('does not render LovellSwitcher when lovellVariant is missing', () => {
+      render(<VamcSystemDetailPage {...formattedMockData} />)
+
+      // Should not find LovellSwitcher content
+      expect(
+        screen.queryByText(/You are viewing this page as a .* beneficiary/)
+      ).not.toBeInTheDocument()
+    })
   })
 })
