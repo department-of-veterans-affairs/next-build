17f1e46df8b0614952c60177f273149c38e66019
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-09-26 13:38:46 -0700

    Introduce a new SideNavLayout component and use it everywhere it's appropriate

--- src/components/vamcSystemBillingAndInsurance/template.tsx
+++ src/components/vamcSystemBillingAndInsurance/template.tsx
@@ -1,7 +1,6 @@
-import { Fragment, useEffect } from 'react'
+import { Fragment } from 'react'
 import { VamcSystemBillingAndInsurance as FormattedVamcSystemBillingAndInsurance } from './formatted-type'
 import { ContentFooter } from '../contentFooter/template'
-import { SideNavMenu } from '@/types/formatted/sideNav'
 import { Wysiwyg } from '../wysiwyg/template'
 import { ListOfLinkTeasers } from '../listOfLinkTeasers/template'
 import {
@@ -12,12 +11,7 @@ import { Address } from '@/components/address/template'
 import { LovellSwitcher } from '@/components/lovellSwitcher/template'
 import { PhoneNumber } from '@/components/phoneNumber/template'
 import { Hours } from '../hours/template'
-
-// Allows additions to window object without overwriting global type
-interface customWindow extends Window {
-  sideNav?: SideNavMenu
-}
-declare const window: customWindow
+import { SideNavLayout } from '@/components/sideNavLayout/template'
 
 export const VamcSystemBillingAndInsurance = ({
   title,
@@ -34,102 +28,87 @@ export const VamcSystemBillingAndInsurance = ({
   lovellVariant,
   lovellSwitchPath,
 }: FormattedVamcSystemBillingAndInsurance) => {
-  // Populate the side nav data for the side nav widget to fill in
-  // Note: The side nav widget is in a separate app in the static-pages bundle
-  useEffect(() => {
-    window.sideNav = menu
-  }, [menu])
-
   return (
-    <div
-      className="vads-grid-container"
-      data-template="vamc_system_billing_insurance"
-    >
-      {/* Nav data filled in by a separate script from `window.sideNav` */}
-      <nav aria-label="secondary" data-widget-type="side-nav" />
-      <div className="vads-grid-row">
-        <div className="vads-grid-col-12">
-          <article
-            aria-labelledby="article-heading"
-            role="region"
-            className="usa-content"
-          >
-            <LovellSwitcher
-              currentVariant={lovellVariant}
-              switchPath={lovellSwitchPath}
-            />
+    <SideNavLayout menu={menu} data-template="vamc_system_billing_insurance">
+      <article
+        aria-labelledby="article-heading"
+        role="region"
+        className="usa-content"
+      >
+        <LovellSwitcher
+          currentVariant={lovellVariant}
+          switchPath={lovellSwitchPath}
+        />
 
-            <h1 id="article-heading">{title}</h1>
-            <div className="va-introtext">
-              <p>
-                You can pay your {vamcSystem.title} bill online, by phone, mail,
-                or in person.
-              </p>
-            </div>
-
-            <va-on-this-page></va-on-this-page>
-
-            <div className="usa-content" data-testid="copay-section">
-              {aboveTopOfPageContent ? (
-                <Wysiwyg {...aboveTopOfPageContent} />
-              ) : (
-                <>
-                  <h2>Questions about copay balance</h2>
-                  <p>
-                    For questions about the copay balance of your{' '}
-                    {vamcSystem?.title} bill, call us toll free at the number
-                    below. You won&apos;t need to pay any copays for X-rays, lab
-                    tests, preventative tests, and services like health
-                    screenings or immunizations.
-                  </p>
-                </>
-              )}
-              <PhoneNumber
-                {...phoneNumber}
-                treatment="h3"
-                className="vads-u-margin-bottom--2"
-              />
-              <Hours allHours={officeHours} headerType="standard" />
-            </div>
-
-            <div className="usa-content">
-              <Wysiwyg {...topOfPageContent} />
-            </div>
-
-            {services.map((service) => (
-              <Fragment key={service.id}>
-                <h3>
-                  <va-link href={service.path} text={service.title}></va-link>
-                </h3>
-                {service.address && (
-                  <Address address={service.address} showDirections={false} />
-                )}
-                {service.serviceLocations.map((serviceLocation) => (
-                  <ServiceLocation
-                    key={serviceLocation.id}
-                    location={serviceLocation}
-                    locationType={ServiceLocationType.NON_CLINICAL}
-                    mainPhoneString={service.phoneNumber}
-                    headingLevel={4}
-                  />
-                ))}
-              </Fragment>
-            ))}
-
-            <Wysiwyg {...bottomOfPageContent} />
-
-            {/* TODO: Related links */}
-            <div className="va-nav-linkslist va-nav-linkslist--related">
-              <ListOfLinkTeasers {...relatedLinks} />
-            </div>
-
-            <va-back-to-top></va-back-to-top>
-
-            <ContentFooter lastUpdated={lastUpdated} />
-          </article>
+        <h1 id="article-heading">{title}</h1>
+        <div className="va-introtext">
+          <p>
+            You can pay your {vamcSystem.title} bill online, by phone, mail, or
+            in person.
+          </p>
         </div>
-      </div>
-    </div>
+
+        <va-on-this-page></va-on-this-page>
+
+        <div className="usa-content" data-testid="copay-section">
+          {aboveTopOfPageContent ? (
+            <Wysiwyg {...aboveTopOfPageContent} />
+          ) : (
+            <>
+              <h2>Questions about copay balance</h2>
+              <p>
+                For questions about the copay balance of your{' '}
+                {vamcSystem?.title} bill, call us toll free at the number below.
+                You won&apos;t need to pay any copays for X-rays, lab tests,
+                preventative tests, and services like health screenings or
+                immunizations.
+              </p>
+            </>
+          )}
+          <PhoneNumber
+            {...phoneNumber}
+            treatment="h3"
+            className="vads-u-margin-bottom--2"
+          />
+          <Hours allHours={officeHours} headerType="standard" />
+        </div>
+
+        <div className="usa-content">
+          <Wysiwyg {...topOfPageContent} />
+        </div>
+
+        {services.map((service) => (
+          <Fragment key={service.id}>
+            <h3>
+              <va-link href={service.path} text={service.title}></va-link>
+            </h3>
+            {service.address && (
+              <Address address={service.address} showDirections={false} />
+            )}
+            {service.serviceLocations.map((serviceLocation) => (
+              <ServiceLocation
+                key={serviceLocation.id}
+                location={serviceLocation}
+                locationType={ServiceLocationType.NON_CLINICAL}
+                mainPhoneString={service.phoneNumber}
+                headingLevel={4}
+              />
+            ))}
+          </Fragment>
+        ))}
+
+        <Wysiwyg {...bottomOfPageContent} />
+
+        {/* TODO: Related links */}
+        <div className="va-nav-linkslist va-nav-linkslist--related">
+          <ListOfLinkTeasers {...relatedLinks} />
+        </div>
+
+        <va-back-to-top></va-back-to-top>
+
+        <ContentFooter lastUpdated={lastUpdated} />
+      </article>
+    </SideNavLayout>
   )
 }
 
