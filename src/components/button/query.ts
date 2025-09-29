2940366f273d21b0b948758882611580047ee338
Chris Valarida <chris@themostudio.com>
2025-09-29 14:05:51 -0700

    Add comment

--- src/components/button/query.ts
+++ src/components/button/query.ts
@@ -10,6 +10,8 @@ export const formatter: QueryFormatter<ParagraphButton, Button> = (
     type: entity.type as Button['type'],
     id: entity.id || null,
     entityId: entity.drupal_internal__id || null,
+    // Paragraph entities pulled in via entity_field_fetch only have a uri and
+    // no url field, so the url field is unreliable at the moment
     url: entity.field_button_link?.url || entity.field_button_link?.uri || null,
     label: entity.field_button_label || null,
   }
