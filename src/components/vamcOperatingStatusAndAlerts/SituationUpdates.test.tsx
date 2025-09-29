d9a593c1b4079e2c41ba90c81e07630715014d4d
Brian Seek <brian.seek@civicactions.com>
2025-09-29 11:32:30 -0700

    simplified mock data.

--- src/components/vamcOperatingStatusAndAlerts/SituationUpdates.test.tsx
+++ src/components/vamcOperatingStatusAndAlerts/SituationUpdates.test.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SituationUpdates } from './SituationUpdates'
-import mockData from './mock.json'
-import { formatter } from './query'
 
 describe('SituationUpdates', () => {
-  const vamcOperatingStatusAndAlertsData = formatter({
-    entity: mockData,
-    menu: null,
-  })
+  const situationUpdateData = [
+    {
+      updates: [
+        {
+          dateTime: '2025-09-19T21:41:00+00:00',
+          timezone: 'America/Los_Angeles',
+          updateText: '<p>A new update</p>',
+        },
+      ],
+      info: '<p>this is info</p>',
+    },
+  ]
+
   test('renders situation updates', () => {
-    render(
-      <SituationUpdates
-        situationUpdates={vamcOperatingStatusAndAlertsData.situationUpdates}
-      />
-    )
+    render(<SituationUpdates situationUpdates={situationUpdateData} />)
     expect(screen.getByTestId('situation-updates-wrapper')).toBeInTheDocument()
     expect(screen.getByTestId('situation-1-update-1-date')).toHaveTextContent(
       'Friday, Sep 19, 2:41 p.m. PT'
