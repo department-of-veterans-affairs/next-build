fd17c855376b6ecfb9fb37a43a23977a8528299f
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-10-08 14:58:22 -0700

    Fix the number callout

--- src/components/numberCallout/template.tsx
+++ src/components/numberCallout/template.tsx
@@ -11,8 +11,15 @@ export function NumberCallout({
       data-entity-id={entityId}
       className="card information"
     >
-      <span className="number">{numberPhrase}</span>
-      <span className="description">{description}</span>
+      <va-card background class="vads-u-margin-bottom--3">
+        <span className="number vads-u-font-family--serif 40px vads-u-font-size--h2 vads-u-font-weight--bold vads-u-line-height--1 vads-u-padding-bottom--2 vads-u-border-bottom--1px vads-u-border-color--gray-light vads-u-display--inline-block">
+          {numberPhrase}
+        </span>
+        <span
+          className="description vads-u-font-size--base vads-u-font-weight--normal vads-u-padding-top--0p5 vads-u-display--block"
+          dangerouslySetInnerHTML={{ __html: description }}
+        ></span>
+      </va-card>
     </div>
   )
 }
