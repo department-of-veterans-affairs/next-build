91716ffa8ce6d3297ac7d4944162dbe0d49a7f31
Chris Kim <chriskim@Chriss-MBP-2.attlocal.net>
2025-10-09 10:11:17 -0700

    updates to benefits hub template

--- src/components/benefitsHub/template.tsx
+++ src/components/benefitsHub/template.tsx
@@ -4,29 +4,25 @@ import { BenefitsHub as FormattedBenefitsHub } from './formatted-type'
 export function BenefitsHub({ title, titleIcon }: FormattedBenefitsHub) {
   const iconConfig = getHubIcon(titleIcon)
   return (
-    <div id="content" className="interior" data-template="layouts/landing_page">
-      <main>
-        <div className="usa-grid usa-grid-full">
-          <article className="usa-width-two-thirds">
-            {iconConfig ? (
-              <div className="tablet:vads-u-display--flex vads-u-margin-y--1 vads-u-align-items--flex-start">
-                <span className="vads-u-margin-top--1">
-                  <va-icon
-                    icon={iconConfig.icon}
-                    size="3"
-                    class={iconConfig.className}
-                  />
-                </span>
-                <h1 className="vads-u-margin-top--1 tablet:vads-u-margin-left--1 tablet:vads-u-margin-y--0">
-                  {title}
-                </h1>
-              </div>
-            ) : (
-              <h1>{title}</h1>
-            )}
-          </article>
-        </div>
-      </main>
+    <div className="usa-grid usa-grid-full">
+      <article className="usa-width-two-thirds">
+        {iconConfig ? (
+          <div className="tablet:vads-u-display--flex vads-u-margin-y--1 vads-u-align-items--flex-start">
+            <span className="vads-u-margin-top--1">
+              <va-icon
+                icon={iconConfig.icon}
+                size="3"
+                class={iconConfig.className}
+              />
+            </span>
+            <h1 className="vads-u-margin-top--1 tablet:vads-u-margin-left--1 tablet:vads-u-margin-y--0">
+              {title}
+            </h1>
+          </div>
+        ) : (
+          <h1>{title}</h1>
+        )}
+      </article>
     </div>
   )
 }
