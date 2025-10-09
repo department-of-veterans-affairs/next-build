9b38963b1c2c16c80896b3dba391caceff49669e
Jeff Keene <jkeene@metrostar.com>
2025-10-09 12:51:19 -0400

    update to app tracing to add instrumentation

---
+++ src/instrumentation.node.js
@@ -0,0 +1,19 @@
+// instrumentation.node.js
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces',
+  }),
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-fs': {
+        enabled: false, // Too noisy for local dev
+      },
+    }),
+  ],
+})
+
+sdk.start()
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,4 +1,7 @@
 import { canInitDatadogApm } from './datadogConnector/utils/canInitDatadogApm'
 export async function register() {
   await import('./datadogConnector/DatadogApmConnector')
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./instrumentation.node')
+  }
 }
