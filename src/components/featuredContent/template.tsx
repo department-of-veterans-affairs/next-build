f554ea7db83c55d343288742d3486d1ae5bb9085
Brian Seek <brian.seek@civicactions.com>
2025-10-10 09:22:03 -0700

    check for HTML on description and add <p>

--- src/components/featuredContent/template.tsx
+++ src/components/featuredContent/template.tsx
@@ -1,11 +1,22 @@
 import { FeaturedContent as FormattedFeaturedContent } from '@/components/featuredContent/formatted-type'
 import { ParagraphComponent } from '@/components/paragraph/formatted-type'
+import { slugifyString } from '@/lib/utils/slug'
 
 export function FeaturedContent({
   title,
   description,
   link,
 }: ParagraphComponent<FormattedFeaturedContent>) {
+  // Sometimes this component is being used with a non-raw html string, in those cases it should be wrapped in a <p>
+  const setPTag = () => {
+    // Checks of empty or only whitespace characters
+    if (!description || /^\s*$/.test(description)) return ''
+    // Checks for any HTML tag
+    if (/^\s*<[^>]+>/.test(description.trim())) {
+      return description
+    }
+    return `<p>${description}</p>`
+  }
   return (
     <va-card class="vads-u-flex--fill vads-u-margin-bottom--2 vads-u-margin-right--0 tablet:vads-u-margin-x--0p5 spotlight-card">
       {title && (
@@ -15,8 +26,8 @@ export function FeaturedContent({
       )}
       {description && (
         <div
-          id={`featured-content-description${title}`}
-          dangerouslySetInnerHTML={{ __html: description }}
+          id={`featured-content-description${slugifyString(title)}`}
+          dangerouslySetInnerHTML={{ __html: setPTag() }}
         />
       )}
       {link && link.url && (
