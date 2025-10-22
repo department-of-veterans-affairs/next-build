55bc31c476b356e9d7da1fb7bad62c269fd9094e
Chris Valarida <chris@themostudio.com>
2025-10-22 15:28:03 -0700

    Update the comment
    Hopefully it's a little clearer.

--- src/components/linkTeaser/query.ts
+++ src/components/linkTeaser/query.ts
@@ -11,8 +11,9 @@ export const formatter: QueryFormatter<
     type: entity.type as LinkTeaser['type'],
     id: entity.id,
     entityId: entity.drupal_internal__id ?? null,
-    // URI is at (least sometimes) 'entity:node/1234', and not the actual URL we
-    // want to use for links.
+    // URL is preferred, but not always present. Something about the
+    // entity_fetched module... The URI is at (least sometimes)
+    // 'entity:node/1234', and not the actual URL we want to use for links.
     uri: entity.field_link?.url || entity.field_link?.uri,
     title: entity.field_link?.title,
     options: entity.field_link?.options,
