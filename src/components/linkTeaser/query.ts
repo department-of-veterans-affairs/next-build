78456119eb9bc081b5b821f42b2b518b2a4a0845
Chris Valarida <chris@themostudio.com>
2025-10-22 14:59:27 -0700

    Link teaser formatter to use the correct property
    I dislike still calling it `uri`, but...I'm really trying to keep my
    changes to a minimum here.

--- src/components/linkTeaser/query.ts
+++ src/components/linkTeaser/query.ts
@@ -11,7 +11,9 @@ export const formatter: QueryFormatter<
     type: entity.type as LinkTeaser['type'],
     id: entity.id,
     entityId: entity.drupal_internal__id ?? null,
-    uri: entity.field_link?.uri,
+    // URI is at (least sometimes) 'entity:node/1234', and not the actual URL we
+    // want to use for links.
+    uri: entity.field_link?.url || entity.field_link?.uri,
     title: entity.field_link?.title,
     options: entity.field_link?.options,
     summary: entity.field_link_summary,
