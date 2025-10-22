802b255b471a5a5e65514d420c5d09447dfe2ac9
Chris Kim <42885441+chriskim2311@users.noreply.github.com>
2025-10-22 16:42:35 -0700

    Update layout and icon display for benefits hub

--- src/components/benefitsHub/template.tsx
+++ src/components/benefitsHub/template.tsx
@@ -9,32 +9,28 @@ export function BenefitsHub({
   const iconConfig = getHubIcon(titleIcon)
 
   return (
-    <div id="content" className="interior" data-template="layouts/landing_page">
-      <main>
-        <div className="usa-grid usa-grid-full">
-          <article className="usa-width-two-thirds">
-            {iconConfig ? (
-              <div className="medium-screen:vads-u-display--flex vads-u-margin-y--1 vads-u-align-items--flex-start">
-                <span className="vads-u-margin-top--1">
-                  <va-icon
-                    icon={iconConfig.icon}
-                    size="3"
-                    class={iconConfig.className}
-                  />
-                </span>
-                <h1 className="vads-u-margin-top--1 medium-screen:vads-u-margin-left--1 medium-screen:vads-u-margin-y--0">
-                  {title}
-                </h1>
-              </div>
-            ) : (
-              <h1>{title}</h1>
-            )}
-            <div className="va-introtext">
-              <p>{fieldIntroText}</p>
-            </div>
-          </article>
+    <div className="usa-grid usa-grid-full">
+      <article className="usa-width-two-thirds">
+        {iconConfig ? (
+          <div className="tablet:vads-u-display--flex vads-u-margin-y--1 vads-u-align-items--flex-start">
+            <span className="vads-u-margin-top--1">
+              <va-icon
+                icon={iconConfig.icon}
+                size="3"
+                class={iconConfig.className}
+              />
+            </span>
+            <h1 className="vads-u-margin-top--1 tablet:vads-u-margin-left--1 tablet:vads-u-margin-y--0">
+              {title}
+            </h1>
+          </div>
+        ) : (
+          <h1>{title}</h1>
+        )}
+        <div className="va-introtext">
+          <p>{fieldIntroText}</p>
         </div>
-      </main>
+      </article>
     </div>
   )
 }
