8da711214983e3e0a7856b7e4e11d055680730a1
Zach Button <zachrey.button@gmail.com>
2025-10-23 13:56:25 -0700

    Campaign Landing Page - Hero Banner (#1427)

--- src/components/campaignLandingPage/formatted-type.ts
+++ src/components/campaignLandingPage/formatted-type.ts
@@ -1,5 +1,20 @@
 import { PublishedEntity } from '@/types/formatted/publishedEntity'
+import { DrupalFile } from 'next-drupal'
+import { MediaImage } from '@/components/mediaDocument/formatted-type'
+
+type Link = {
+  href: string
+  label: string
+}
 
 export interface CampaignLandingPage extends PublishedEntity {
   title: string
+  hero: {
+    cta: {
+      primary?: Link | null
+      secondary?: Link | null
+    }
+    blurb: string
+    image: MediaImage
+  }
 }
