534f6a5409855c72475de17ff81c235816432a1d
Chris Kim <chriskim@Chriss-MacBook-Pro-2.local>
2025-10-27 15:40:32 -0700

    remove divs

--- src/components/benefitsHub/template.tsx
+++ src/components/benefitsHub/template.tsx
@@ -12,63 +12,59 @@ export function BenefitsHub({
   const iconConfig = getHubIcon(titleIcon)
 
   return (
-    <div id="content" className="interior" data-template="layouts/landing_page">
-      <main>
-        <div className="usa-grid usa-grid-full">
-          <article className="usa-width-two-thirds">
-            {iconConfig ? (
-              <div className="tablet:vads-u-display--flex vads-u-margin-y--1 vads-u-align-items--flex-start">
-                <span className="vads-u-margin-top--1">
-                  <va-icon
-                    icon={iconConfig.icon}
-                    size="3"
-                    class={iconConfig.className}
-                  />
-                </span>
-                <h1 className="vads-u-margin-top--1 tablet:vads-u-margin-left--1 tablet:vads-u-margin-y--0">
-                  {title}
-                </h1>
-              </div>
-            ) : (
-              <h1>{title}</h1>
-            )}
-            <div className="va-introtext">
-              <p>{intro}</p>
-            </div>
-            {spokes?.map((spokeSection) => (
-              <div key={spokeSection.id}>
-                <section className="usa-grid">
-                  <div className="va-h-ruled--stars"></div>
-                </section>
-                <ListOfLinkTeasers {...spokeSection} />
-              </div>
-            ))}
-
-            {fieldLinks && fieldLinks.length > 0 && (
-              <va-accordion-item
-                class="va-accordion-item"
-                level="2"
-                open="true"
-                header="Not a Veteran?"
-                id="get-information-for"
-                bordered
-                uswds
-              >
-                <section>
-                  <h3 className="vads-u-font-size--h4">Get information for:</h3>
-                  <ul className="va-nav-linkslist-list links">
-                    {fieldLinks.map((link, index) => (
-                      <li key={index}>
-                        <va-link href={link.url.path} text={link.title} />
-                      </li>
-                    ))}
-                  </ul>
-                </section>
-              </va-accordion-item>
-            )}
-          </article>
+    <div className="usa-grid usa-grid-full">
+      <article className="usa-width-two-thirds">
+        {iconConfig ? (
+          <div className="tablet:vads-u-display--flex vads-u-margin-y--1 vads-u-align-items--flex-start">
+            <span className="vads-u-margin-top--1">
+              <va-icon
+                icon={iconConfig.icon}
+                size="3"
+                class={iconConfig.className}
+              />
+            </span>
+            <h1 className="vads-u-margin-top--1 tablet:vads-u-margin-left--1 tablet:vads-u-margin-y--0">
+              {title}
+            </h1>
+          </div>
+        ) : (
+          <h1>{title}</h1>
+        )}
+        <div className="va-introtext">
+          <p>{intro}</p>
         </div>
-      </main>
+        {spokes?.map((spokeSection) => (
+          <div key={spokeSection.id}>
+            <section className="usa-grid">
+              <div className="va-h-ruled--stars"></div>
+            </section>
+            <ListOfLinkTeasers {...spokeSection} />
+          </div>
+        ))}
+
+        {fieldLinks && fieldLinks.length > 0 && (
+          <va-accordion-item
+            class="va-accordion-item"
+            level="2"
+            open="true"
+            header="Not a Veteran?"
+            id="get-information-for"
+            bordered
+            uswds
+          >
+            <section>
+              <h3 className="vads-u-font-size--h4">Get information for:</h3>
+              <ul className="va-nav-linkslist-list links">
+                {fieldLinks.map((link, index) => (
+                  <li key={index}>
+                    <va-link href={link.url.path} text={link.title} />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          </va-accordion-item>
+        )}
+      </article>
     </div>
   )
 }
