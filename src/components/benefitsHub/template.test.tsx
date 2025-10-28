26b4409a7c40659fe1db4cc740379429363432e9
Skye Anico <229183366+sky3an1co@users.noreply.github.com>
2025-10-28 12:32:29 -0400

    [VACMS-21946] Add ContentFooter component to Benefits Hub (#1444)

--- src/components/benefitsHub/template.test.tsx
+++ src/components/benefitsHub/template.test.tsx
@@ -1,44 +1,34 @@
 import React from 'react'
+import { BenefitsHub as FormattedBenefitsHub } from './formatted-type'
 import { render, screen } from '@testing-library/react'
 import { axe } from '@/test-utils'
 import { BenefitsHub } from './template'
 
+const mockBenefitsData: FormattedBenefitsHub = {
+  id: '1',
+  type: '',
+  published: true,
+  lastUpdated: '2021-10-31T17:26:37+00:00',
+  title: 'Test Health Benefits Hub',
+  titleIcon: null,
+  intro: 'This is a test intro for the Benefits Hub component.',
+}
+
 describe('BenefitsHub with valid data', () => {
   test('renders BenefitsHub component', async () => {
-    const { container } = render(
-      <BenefitsHub
-        id="1"
-        type=""
-        published={true}
-        lastUpdated="2024-01-01"
-        title={'Hello world'}
-        titleIcon={null}
-        intro={null}
-      />
-    )
+    const { container } = render(<BenefitsHub {...mockBenefitsData} />)
 
-    expect(screen.queryByText(/Hello world/)).toBeInTheDocument()
+    expect(screen.queryByText(/Test Health Benefits Hub/)).toBeInTheDocument()
 
     const axeResults = await axe(container)
     expect(axeResults).toHaveNoViolations()
   })
 
   test('renders BenefitsHub component with intro text', () => {
-    render(
-      <BenefitsHub
-        id="2"
-        type=""
-        published={true}
-        lastUpdated="2024-01-01"
-        title={'Health Care'}
-        titleIcon={null}
-        intro={'This is an intro text for health care benefits.'}
-      />
-    )
+    render(<BenefitsHub {...mockBenefitsData} />)
 
-    expect(screen.queryByText(/Health Care/)).toBeInTheDocument()
     expect(
-      screen.queryByText(/This is an intro text for health care benefits./)
+      screen.queryByText(/This is a test intro for the Benefits Hub component./)
     ).toBeInTheDocument()
   })
 
@@ -67,5 +57,16 @@ describe('BenefitsHub with valid data', () => {
     expect(vaIcon).toHaveAttribute('size', '3')
     expect(vaIcon).toHaveClass('hub-icon')
     expect(vaIcon).toHaveClass('vads-u-background-color--hub-disability')
+  })
+
+  test('renders ContentFooter with lastUpdated date', () => {
+    render(<BenefitsHub {...mockBenefitsData} />)
+    expect(screen.getByTestId('content-footer')).toBeInTheDocument()
+    expect(screen.getByText('Last updated:')).toBeInTheDocument()
+    expect(screen.getByText('October 31, 2021')).toBeInTheDocument()
+    expect(screen.getByText('October 31, 2021')).toHaveAttribute(
+      'dateTime',
+      '2021-10-31'
+    )
   })
 })
