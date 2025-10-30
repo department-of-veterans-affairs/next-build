2d1d63dd08f46f777b99353a20341a3e61c526e8
sky3an1co <229183366+sky3an1co@users.noreply.github.com>
2025-10-30 18:35:49 -0400

    created ConnectWithUsPanel subcomponent

--- src/components/benefitsHub/template.tsx
+++ src/components/benefitsHub/template.tsx
@@ -2,6 +2,7 @@ import { getHubIcon } from '@/lib/utils/benefitsHub'
 import { BenefitsHub as FormattedBenefitsHub } from './formatted-type'
 import { ListOfLinkTeasers } from '@/components/listOfLinkTeasers/template'
 import { ContentFooter } from '@/components/contentFooter/template'
+import { ConnectWithUsPanel } from './ConnectWithUsPanel'
 
 export function BenefitsHub({
   title,
@@ -12,8 +13,6 @@ export function BenefitsHub({
   connectWithUs,
 }: FormattedBenefitsHub) {
   const iconConfig = getHubIcon(titleIcon)
-  const socialLinksObject =
-    connectWithUs.field_social_media_links.platform_values || null
 
   return (
     <div className="usa-grid usa-grid-full">
@@ -52,101 +51,7 @@ export function BenefitsHub({
       </article>
       <div className="usa-width-one-third vads-u-padding-right--2 vads-u-padding-bottom--2 mobile:vads-u-padding-left--2">
         <va-accordion>
-          <va-accordion-item
-            open
-            bordered
-            uswds
-            header="Connect with us"
-            level="2"
-          >
-            <h2 className="vads-u-font-size--h4">Get updates</h2>
-            {connectWithUs.field_email_updates_link.url &&
-              connectWithUs.field_email_updates_link.title && (
-                <div>
-                  <va-icon
-                    icon="mail"
-                    size="3"
-                    className="vads-u-color--link-default vads-u-padding-right--1"
-                  ></va-icon>
-                  <va-link
-                    href={connectWithUs.field_email_updates_link.url}
-                    text={connectWithUs.field_email_updates_link.title}
-                  />
-                </div>
-              )}
-            <h2 className="vads-u-font-size--h4">Follow us</h2>
-            <ul className="va-nav-linkslist-list social">
-              {socialLinksObject.twitter?.value && (
-                <li>
-                  <va-icon
-                    icon="x"
-                    size="3"
-                    className="vads-u-color--link-default vads-u-padding-right--1"
-                  ></va-icon>
-                  <va-link
-                    href={`https://twitter.com/${socialLinksObject.twitter.value}`}
-                    text={'Veterans Affairs X (formerly Twitter)'}
-                  />
-                </li>
-              )}
-
-              {socialLinksObject.facebook?.value && (
-                <li>
-                  <va-icon
-                    icon="facebook"
-                    size="3"
-                    className="vads-u-color--link-default vads-u-padding-right--1"
-                  ></va-icon>
-                  <va-link
-                    href={`https://facebook.com/${socialLinksObject.facebook.value}`}
-                    text={'Veterans Affairs Facebook'}
-                  />
-                </li>
-              )}
-
-              {socialLinksObject.youtube?.value && (
-                <li>
-                  <va-icon
-                    icon="youtube"
-                    size="3"
-                    className="vads-u-color--link-default vads-u-padding-right--1"
-                  ></va-icon>
-                  <va-link
-                    href={`https://youtube.com/${socialLinksObject.youtube.value}`}
-                    text={'Veterans Affairs YouTube'}
-                  />
-                </li>
-              )}
-
-              {socialLinksObject.linkedin?.value && (
-                <li>
-                  <va-icon
-                    icon="linkedin"
-                    size="3"
-                    className="vads-u-color--link-default vads-u-padding-right--1"
-                  ></va-icon>
-                  <va-link
-                    href={`https://linkedin.com/${socialLinksObject.linkedin.value}`}
-                    text={'Veterans Affairs LinkedIn'}
-                  />
-                </li>
-              )}
-
-              {socialLinksObject.instagram?.value && (
-                <li>
-                  <va-icon
-                    icon="instagram"
-                    size="3"
-                    className="vads-u-color--link-default vads-u-padding-right--1"
-                  ></va-icon>
-                  <va-link
-                    href={`https://instagram.com/${socialLinksObject.instagram.value}`}
-                    text={'Veterans Affairs Instagram'}
-                  />
-                </li>
-              )}
-            </ul>
-          </va-accordion-item>
+          <ConnectWithUsPanel connectWithUs={connectWithUs} />
         </va-accordion>
       </div>
     </div>
