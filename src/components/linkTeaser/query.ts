5014bbdf7212dce496b6ea8cf2330db1118ccd7e
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-10-31 14:38:06 -0700

    Get changes from the integration branch that were clobbered during rebase

--- src/components/linkTeaser/query.ts
+++ src/components/linkTeaser/query.ts
@@ -12,9 +12,14 @@ export const formatter: QueryFormatter<
     type: entity.type as LinkTeaser['type'],
     id: entity.id,
     entityId: entity.drupal_internal__id ?? null,
+    // URL is preferred, but not always present. Something about the
+    // entity_fetched module... The URI is at (least sometimes)
+    // 'entity:node/1234', and not the actual URL we want to use for links.
     uri: entity.field_link?.url || entity.field_link?.uri,
     title: entity.field_link?.title,
     options: entity.field_link?.options,
-    summary: entity.field_link_summary,
+    summary: entity.field_link_summary
+      ? getHtmlFromDrupalContent(entity.field_link_summary)
+      : null,
   }
 }
