9c11ae0a7a9e3525ecf74516f20511517bc6c409
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-10-31 12:18:15 -0700

    Don't need the sidenav itself to be a client side only component

--- src/components/sideNav/CustomSideNav.tsx
+++ src/components/sideNav/CustomSideNav.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { useEffect, useState } from 'react'
 import { usePathname } from 'next/navigation'
 import { SideNavMenu, SideNavItem } from '@/types/formatted/sideNav'
