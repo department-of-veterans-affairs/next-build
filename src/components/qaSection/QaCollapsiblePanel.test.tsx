ea8c9ec7257f44ed3536a1f7b6df3bf534c10d72
Tim Cosgrove <timcosgrove@users.noreply.github.com>
2025-11-05 16:08:47 -0800

    Revert "Fix QA accordion header ids (#1457)"
    This reverts commit 1990a55eb2b7f2acc266f78042986bd148d87bc4.

--- src/components/qaSection/QaCollapsiblePanel.test.tsx
+++ src/components/qaSection/QaCollapsiblePanel.test.tsx
@@ -19,23 +19,6 @@ const questionsData = [
     ],
     type: 'paragraph--q_a',
   },
-  {
-    id: '2222-2222-2222',
-    question: 'Question 2',
-    answers: [
-      {
-        id: '3',
-        html: '<p>test string 3</p>',
-        type: 'paragraph--wysiwyg',
-      },
-      {
-        id: '4',
-        html: '<p>test string 4</p>',
-        type: 'paragraph--wysiwyg',
-      },
-    ],
-    type: 'paragraph--q_a',
-  },
 ]
 
 describe('QaCollapsiblePanel with valid data', () => {
@@ -51,15 +34,5 @@ describe('QaCollapsiblePanel with valid data', () => {
 
     const axeResults = await axe(container)
     expect(axeResults).toHaveNoViolations()
-  })
-
-  test('slugifies the question header', () => {
-    const { container } = render(
-      <QaCollapsiblePanel questions={questionsData} />
-    )
-
-    const questionHeaders = container.querySelectorAll('va-accordion-item')
-    expect(questionHeaders[0]).toHaveAttribute('id', 'question-1')
-    expect(questionHeaders[1]).toHaveAttribute('id', 'question-2')
   })
 })
