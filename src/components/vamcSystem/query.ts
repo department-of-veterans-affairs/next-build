68a483c183a9c356d238906964c4d97199484366
Tim Cosgrove <timcosgrove@users.noreply.github.com>
2025-11-05 17:57:38 -0800

    Revert "Revert "Optimize location listing and vamc system queries of vamc facilities (#1425)""
    This reverts commit 28c4ae8b04fd59105aff016bd0048d5f503b9cb8.

--- src/components/vamcSystem/query.ts
+++ src/components/vamcSystem/query.ts
@@ -87,11 +87,11 @@ export const data: QueryData<VamcSystemDataOpts, VamcSystemData> = async (
   const { data: mainFacilities } =
     await fetchAndConcatAllResourceCollectionPages<NodeHealthCareLocalFacility>(
       RESOURCE_TYPES.VAMC_FACILITY,
-      queries
-        .getParams(RESOURCE_TYPES.VAMC_FACILITY)
+      new DrupalJsonApiParams()
+        .addInclude(['field_telephone', 'field_media.image'])
         .addFilter('field_region_page.id', entity.id)
         .addFilter('field_main_location', '1'),
-      PAGE_SIZES[RESOURCE_TYPES.VAMC_FACILITY]
+      PAGE_SIZES.MAX
     )
 
   // Fetch featured stories for this VAMC System
