department-of-veterans-affairs/next-build

commit 18e1f3e7d6c994098eac3b0bf019eb363dbe773e

Refactor and simplify handling of related links in VAMC System and Facility (#1456)

src/components/vamcFacility/__snapshots__/query.test.ts.snap
@@ -828,41 +828,102 @@ exports[`VamcFacility query should handle the Lovell variant page menu 1`] = `
     "path": "/lovell-federal-health-care-tricare/locations/causeway-va-clinic",
     "published": true,
     "relatedLinks": {
-      "links": [
+      "entityId": 22134,
+      "id": "c5836275-d0fe-46b1-8c42-027967bb5157",
+      "linkTeasers": [
         {
+          "entityId": 22133,
+          "id": "155593bc-cfb5-431d-a81c-2d3f31fb46f9",
+          "options": [],
+          "summary": null,
           "title": "Get help from a patient advocate",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/health-services/patient-advocates",
         },
         {
+          "entityId": 66785,
+          "id": "5d72c143-3d85-41f8-9505-9b38d1f5b12e",
+          "options": [],
+          "summary": null,
           "title": "Find a phone number",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/contact-us",
         },
         {
+          "entityId": 106575,
+          "id": "41703f88-f709-4dd8-957e-3e0996a99ab5",
+          "options": [],
+          "summary": "Interactive facility maps",
           "title": "Interactive facility maps",
+          "type": "paragraph--link_teaser",
           "uri": "https://www.va.gov/boston-health-care/programs/interactive-facility-maps",
         },
         {
+          "entityId": 66786,
+          "id": "0698f49f-7c08-4994-9984-fd3d740e3170",
+          "options": [],
+          "summary": null,
           "title": "Check your billing, insurance and payment options",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/billing-and-insurance",
         },
         {
+          "entityId": 66784,
+          "id": "5c072309-72d3-46ea-bcce-0d91ca60555e",
+          "options": [],
+          "summary": null,
           "title": "Access your health records",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/medical-records-office",
         },
         {
+          "entityId": 66789,
+          "id": "e9108e23-a405-4d1c-b480-af808897b53f",
+          "options": [],
+          "summary": null,
           "title": "Compare our performance with non-VA facilities",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/about-us/performance",
         },
         {
+          "entityId": 110591,
+          "id": "e36f1be9-f67f-43bc-93e5-264cb73adc03",
+          "options": [],
+          "summary": null,
           "title": "Learn about virtual care and service options",
+          "type": "paragraph--link_teaser",
           "uri": "https://www.va.gov/boston-health-care/programs/connected-care/",
         },
         {
+          "entityId": 66787,
+          "id": "92d18a35-2d3b-456e-9e2c-f0066a245f40",
+          "options": [],
+          "summary": null,
           "title": "Volunteer or donate",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/work-with-us/volunteer-or-donate",
         },
+        {
+          "entityId": 66788,
+          "id": "62134c8e-3349-4b2b-850a-555f39ab2ca0",
+          "options": [],
+          "summary": null,
+          "title": "Learn more about VA New England",
+          "type": "paragraph--link_teaser",
+          "uri": "https://www.newengland.va.gov/",
+        },
+        {
+          "entityId": 103024,
+          "id": "e38abc4d-f4da-4b0f-bef4-763514a6461e",
+          "options": [],
+          "summary": null,
+          "title": "Learn more about our COVID-19 vaccination and testing program",
+          "type": "paragraph--link_teaser",
+          "uri": "https://www.va.gov/boston-health-care/programs/covid-19-vaccines/",
+        },
       ],
-      "sectionTitle": "Other services at VA Boston health care",
+      "title": "In the spotlight at VA Boston health care",
+      "type": "paragraph--list_of_link_teasers",
     },
     "socialLinks": {
       "links": [
@@ -1742,41 +1803,102 @@ exports[`VamcFacility query should output formatted data 1`] = `
     "path": "/boston-health-care/locations/framingham-va-clinic",
     "published": true,
     "relatedLinks": {
-      "links": [
+      "entityId": 22134,
+      "id": "c5836275-d0fe-46b1-8c42-027967bb5157",
+      "linkTeasers": [
         {
+          "entityId": 22133,
+          "id": "155593bc-cfb5-431d-a81c-2d3f31fb46f9",
+          "options": [],
+          "summary": null,
           "title": "Get help from a patient advocate",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/health-services/patient-advocates",
         },
         {
+          "entityId": 66785,
+          "id": "5d72c143-3d85-41f8-9505-9b38d1f5b12e",
+          "options": [],
+          "summary": null,
           "title": "Find a phone number",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/contact-us",
         },
         {
+          "entityId": 106575,
+          "id": "41703f88-f709-4dd8-957e-3e0996a99ab5",
+          "options": [],
+          "summary": "Interactive facility maps",
           "title": "Interactive facility maps",
+          "type": "paragraph--link_teaser",
           "uri": "https://www.va.gov/boston-health-care/programs/interactive-facility-maps",
         },
         {
+          "entityId": 66786,
+          "id": "0698f49f-7c08-4994-9984-fd3d740e3170",
+          "options": [],
+          "summary": null,
           "title": "Check your billing, insurance and payment options",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/billing-and-insurance",
         },
         {
+          "entityId": 66784,
+          "id": "5c072309-72d3-46ea-bcce-0d91ca60555e",
+          "options": [],
+          "summary": null,
           "title": "Access your health records",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/medical-records-office",
         },
         {
+          "entityId": 66789,
+          "id": "e9108e23-a405-4d1c-b480-af808897b53f",
+          "options": [],
+          "summary": null,
           "title": "Compare our performance with non-VA facilities",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/about-us/performance",
         },
         {
+          "entityId": 110591,
+          "id": "e36f1be9-f67f-43bc-93e5-264cb73adc03",
+          "options": [],
+          "summary": null,
           "title": "Learn about virtual care and service options",
+          "type": "paragraph--link_teaser",
           "uri": "https://www.va.gov/boston-health-care/programs/connected-care/",
         },
         {
+          "entityId": 66787,
+          "id": "92d18a35-2d3b-456e-9e2c-f0066a245f40",
+          "options": [],
+          "summary": null,
           "title": "Volunteer or donate",
+          "type": "paragraph--link_teaser",
           "uri": "/boston-health-care/work-with-us/volunteer-or-donate",
         },
+        {
+          "entityId": 66788,
+          "id": "62134c8e-3349-4b2b-850a-555f39ab2ca0",
+          "options": [],
+          "summary": null,
+          "title": "Learn more about VA New England",
+          "type": "paragraph--link_teaser",
+          "uri": "https://www.newengland.va.gov/",
+        },
+        {
+          "entityId": 103024,
+          "id": "e38abc4d-f4da-4b0f-bef4-763514a6461e",
+          "options": [],
+          "summary": null,
+          "title": "Learn more about our COVID-19 vaccination and testing program",
+          "type": "paragraph--link_teaser",
+          "uri": "https://www.va.gov/boston-health-care/programs/covid-19-vaccines/",
+        },
       ],
-      "sectionTitle": "Other services at VA Boston health care",
+      "title": "In the spotlight at VA Boston health care",
+      "type": "paragraph--list_of_link_teasers",
     },
     "socialLinks": {
       "links": [

src/components/vamcFacility/formatted-type.ts
@@ -11,10 +11,10 @@ import {
 import { FacilityOperatingStatusFlags } from '@/types/drupal/node'
 import { VamcEhr } from '@/types/drupal/vamcEhr'
 import { PublishedEntity } from '@/types/formatted/publishedEntity'
-import { FormattedRelatedLinks } from '@/components/relatedLinks/formatted-type'
 import { SideNavMenu } from '@/types/formatted/sideNav'
 import { VamcSystemSocialLinks } from '../vamcSystem/formatted-type'
 import { ServiceLocation } from '@/components/serviceLocation/formatted-type'
+import { ListOfLinkTeasers } from '../listOfLinkTeasers/formatted-type'
 
 export type VamcFacility = PublishedEntity & {
   introText: string | null
@@ -39,7 +39,7 @@ export type VamcFacility = PublishedEntity & {
    */
   mentalHealthPhoneNumber: PhoneNumber | null
   vaHealthConnectPhoneNumber: string | null
-  relatedLinks: FormattedRelatedLinks
+  relatedLinks: ListOfLinkTeasers
   locationServices: Array<{
     title: string
     /**

src/components/vamcFacility/query.ts
@@ -19,11 +19,11 @@ import {
 } from '@/lib/drupal/lovell/utils'
 import { formatter as formatImage } from '@/components/mediaImage/query'
 import { formatter as formatPhone } from '@/components/phoneNumber/query'
-import { ParagraphLinkTeaser } from '@/types/drupal/paragraph'
 import { getHtmlFromField } from '@/lib/utils/getHtmlFromField'
 import { formatter as formatAdministration } from '@/components/administration/query'
 import { getVamcSystemSocialLinks } from '../vamcSystem/query'
 import { formatter as formatServiceLocation } from '@/components/serviceLocation/query'
+import { formatter as formatListOfLinkTeasers } from '@/components/listOfLinkTeasers/query'
 
 const isPublished = (entity: { status: boolean }) => entity.status === true
 
@@ -132,21 +132,9 @@ export const formatter: QueryFormatter<VamcFacilityData, VamcFacility> = ({
     image: formatImage(entity.field_media),
     facilityLocatorApiId: entity.field_facility_locator_api_id,
     geoLocation: entity.field_geolocation,
-    relatedLinks: {
-      sectionTitle: entity.field_region_page.title
-        ? `Other services at ${entity.field_region_page.title}`
-        : (entity.field_region_page.field_related_links?.field_title ?? ''),
-      links:
-        entity.field_region_page.field_related_links?.field_va_paragraphs
-          .slice(0, 8)
-          // Adding the type annotation because TS doesn't apparently pick up on
-          // this since we've done an Omit<> on the parent type.
-          .map((linkTeaser: ParagraphLinkTeaser) => ({
-            title: linkTeaser.field_link.title,
-            uri: linkTeaser.field_link.url,
-            // summary: ''
-          })) ?? null,
-    },
+    relatedLinks: formatListOfLinkTeasers(
+      entity.field_region_page.field_related_links
+    ),
     locationServices: entity.field_location_services.map((service) => ({
       title: service.field_title,
       wysiwigContents: getHtmlFromField(service.field_wysiwyg),

src/components/vamcFacility/template.tsx
@@ -165,10 +165,7 @@ export function VamcFacility({
           </div>
         </TextWithImage>
         <LocationServices items={locationServices} />
-        <RelatedLinks
-          sectionTitle={relatedLinks.sectionTitle}
-          links={relatedLinks.links}
-        />
+        <RelatedLinks {...relatedLinks} title={`Other services at ${title}`} />
         <HealthServices
           healthServices={healthServices}
           mentalHealthPhoneNumber={fieldTelephone}

src/components/vamcFacility/template.test.tsx
@@ -119,7 +119,7 @@ describe('HealthCareLocalFacility with valid data', () => {
 
     expect(
       screen.getByRole('heading', {
-        name: 'Other services at VA Boston health care',
+        name: /Other services at Framingham VA Clinic/,
       })
     ).toBeInTheDocument()
     expect(container.querySelectorAll('ul > li > p > va-link')).toHaveLength(8)

src/components/linkTeaser/__snapshots__/query.test.ts.snap
@@ -3,9 +3,6 @@
 exports[`paragraph--link_teaser formatData outputs formatted data 1`] = `
 [
   {
-    "componentParams": {
-      "sectionHeader": "",
-    },
     "entityId": 3,
     "id": "cb0c2019-0f48-448f-98ca-205d80c8f6fe",
     "options": [],
@@ -15,9 +12,6 @@ exports[`paragraph--link_teaser formatData outputs formatted data 1`] = `
     "uri": "/health-care/eligibility/",
   },
   {
-    "componentParams": {
-      "sectionHeader": "",
-    },
     "entityId": 4,
     "id": "18e73277-6d82-4391-a33e-4ddf118fb3b0",
     "options": [],
@@ -27,9 +21,6 @@ exports[`paragraph--link_teaser formatData outputs formatted data 1`] = `
     "uri": "/health-care/how-to-apply/",
   },
   {
-    "componentParams": {
-      "sectionHeader": "",
-    },
     "entityId": 5,
     "id": "42b1db7c-8ee2-4fcd-933c-df8affa95117",
     "options": [],

src/components/linkTeaser/formatted-type.ts
@@ -8,7 +8,4 @@ export type LinkTeaser = PublishedParagraph & {
   uri: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   options: { [key: string]: any }
-  componentParams: {
-    sectionHeader: string
-  }
 }

src/components/linkTeaser/query.ts
@@ -21,6 +21,5 @@ export const formatter: QueryFormatter<
     summary: entity.field_link_summary
       ? getHtmlFromDrupalContent(entity.field_link_summary)
       : null,
-    componentParams: { sectionHeader: '' },
   }
 }

commit 9cfcfc94e1e6148835fc711ee2b7040121c0f76d

Fix regression where the facility pages showed the facility name for related links
Regression was introduced in https://github.com/department-of-veterans-affairs/next-build/commit/18e1f3e7d6c994098eac3b0bf019eb363dbe773e

src/components/vamcFacility/__snapshots__/query.test.ts.snap
@@ -959,6 +959,7 @@ exports[`VamcFacility query should handle the Lovell variant page menu 1`] = `
     "type": "node--health_care_local_facility",
     "vaHealthConnectPhoneNumber": "833-933-1106",
     "vamcEhrSystem": "vista",
+    "vamcSystemTitle": "VA Boston health care",
   },
   "include": {},
 }
@@ -1934,6 +1935,7 @@ exports[`VamcFacility query should output formatted data 1`] = `
     "type": "node--health_care_local_facility",
     "vaHealthConnectPhoneNumber": "833-933-1106",
     "vamcEhrSystem": "vista",
+    "vamcSystemTitle": "VA Boston health care",
   },
   "include": {},
 }

src/components/vamcFacility/formatted-type.ts
@@ -22,6 +22,7 @@ export type VamcFacility = PublishedEntity & {
   menu: SideNavMenu | null
   path: string
   administration?: Administration
+  vamcSystemTitle: string
   vamcEhrSystem: VamcEhr['field_region_page']['field_vamc_ehr_system']
   officeHours: FieldOfficeHours[]
   address: FieldAddress

src/components/vamcFacility/query.ts
@@ -148,6 +148,7 @@ export const formatter: QueryFormatter<VamcFacilityData, VamcFacility> = ({
             getOppositeChildVariant(lovell?.variant)
           )
         : null,
+    vamcSystemTitle: entity.field_region_page.title,
     healthServices: entity.field_local_health_care_service_
       // Make sure we're only dealing with published health services. If they're
       // not published, they'll be entity references with no actual data.

src/components/vamcFacility/template.tsx
@@ -26,6 +26,7 @@ export function VamcFacility({
   menu,
   path,
   administration,
+  vamcSystemTitle,
   vamcEhrSystem,
   officeHours,
   address,
@@ -165,7 +166,10 @@ export function VamcFacility({
           </div>
         </TextWithImage>
         <LocationServices items={locationServices} />
-        <RelatedLinks {...relatedLinks} title={`Other services at ${title}`} />
+        <RelatedLinks
+          {...relatedLinks}
+          title={`Other services at ${vamcSystemTitle}`}
+        />
         <HealthServices
           healthServices={healthServices}
           mentalHealthPhoneNumber={fieldTelephone}

src/components/vamcFacility/template.test.tsx
@@ -119,7 +119,7 @@ describe('HealthCareLocalFacility with valid data', () => {
 
     expect(
       screen.getByRole('heading', {
-        name: /Other services at Framingham VA Clinic/,
+        name: /Other services at VA Boston health care/,
       })
     ).toBeInTheDocument()
     expect(container.querySelectorAll('ul > li > p > va-link')).toHaveLength(8)