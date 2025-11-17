bfc9b6c9ec9fc5dbc2929028b3deaf9c42407749
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-11-17 13:49:48 -0800

    Do the same thing for the VAMC Health Services Listing query module

--- src/components/vamcHealthServicesListing/query.test.ts
+++ src/components/vamcHealthServicesListing/query.test.ts
@@ -1,24 +1,37 @@
 /**
  * @jest-environment node
+ *
+ * If we don't have this, next-drupal-query will complain, thinking that it's running on
+ * the client: "You should not call getQueryData on the client."
  */
 
+import mockPage from './mock.json'
+import mockSystem from '@/components/vamcSystem/mock.json'
 import {
   NodeVamcHealthServicesListing,
   NodeHealthCareRegionPage,
+  NodeRegionalHealthCareServiceDes,
 } from '@/types/drupal/node'
-import { formatter, params } from './query'
+import { RESOURCE_TYPES } from '@/lib/constants/resourceTypes'
 import { queries } from '@/lib/drupal/queries'
-import mockServicesListingPartial from './mock.json'
-import mockSystem from '@/components/vamcSystem/mock.json'
-import { DrupalMenuLinkContent } from 'next-drupal'
+import { LovellStaticPropsContextProps } from '@/lib/drupal/lovell/types'
+import { params } from './query'
 import { createMockServicesForGrouping } from './mockServiceDes'
+import { Menu } from '@/types/drupal/menu'
+import { DrupalMenuLinkContent } from 'next-drupal'
 
 // Use type assertion to bypass strict type checking for test data
 const mockServicesListing = {
-  ...mockServicesListingPartial,
+  ...mockPage,
   field_office: {
     ...(mockSystem as unknown as NodeHealthCareRegionPage),
-    field_system_menu: {},
+    field_system_menu: {
+      type: 'menu_link_content--menu_link_content',
+      id: 'test-menu-link',
+      resourceIdObjMeta: {
+        drupal_internal__target_id: 1,
+      },
+    },
   },
 } as NodeVamcHealthServicesListing
 
@@ -43,16 +56,44 @@ const menuItem: DrupalMenuLinkContent = {
 const mockMenu = {
   items: [menuItem],
   tree: [menuItem],
+} as Menu
+
+const mockPageQuery = jest.fn(
+  () => mockServicesListing as NodeVamcHealthServicesListing
+)
+const mockServicesQuery = jest.fn(
+  () => createMockServicesForGrouping() as NodeRegionalHealthCareServiceDes[]
+)
+
+jest.mock('@/lib/drupal/query', () => ({
+  ...jest.requireActual('@/lib/drupal/query'),
+  fetchSingleEntityOrPreview: () => mockPageQuery(),
+  fetchAndConcatAllResourceCollectionPages: () => ({
+    data: mockServicesQuery(),
+  }),
+  getMenu: () => mockMenu,
+}))
+
+function runQuery(lovell: Partial<LovellStaticPropsContextProps> = {}) {
+  return queries.getData(RESOURCE_TYPES.VAMC_HEALTH_SERVICES_LISTING, {
+    id: mockServicesListing.id,
+    context: {
+      path: '/test-path',
+      drupalPath: '/test-drupal-path',
+      listing: {
+        isListingPage: false,
+        firstPagePath: '/test-first-page-path',
+        page: 1,
+      },
+      lovell: {
+        isLovellVariantPage: false,
+        variant: null,
+        ...lovell,
+      },
+    },
+  })
 }
 
-const mockDataWrapper = {
-  entity: mockServicesListing,
-  services: createMockServicesForGrouping(),
-  menu: mockMenu,
-  lovell: undefined,
-}
-
-// remove if this component does not have a data fetch
 describe('DrupalJsonApiParams configuration', () => {
   test('params function sets the correct include fields', () => {
     const paramsInstance = params()
@@ -61,49 +102,23 @@ describe('DrupalJsonApiParams configuration', () => {
   })
 })
 
-describe('VamcHealthServicesListing formatData', () => {
-  test('outputs formatted data', () => {
-    expect(
-      queries.formatData('node--health_services_listing', mockDataWrapper)
-    ).toMatchSnapshot()
-  })
-
-  test('handles empty description correctly', () => {
-    const mockWithEmptyDescription = {
-      ...mockServicesListing,
-      field_description: '',
-      field_intro_text: '',
-    }
-
-    const mockDataWrapperWithEmptyDescription = {
-      ...mockDataWrapper,
-      entity: mockWithEmptyDescription,
-    }
-
-    const result = queries.formatData(
-      'node--health_services_listing',
-      mockDataWrapperWithEmptyDescription
+describe('VamcHealthServicesListing formatter', () => {
+  beforeEach(() => {
+    // Reset to default mock data before each test
+    mockPageQuery.mockReturnValue(
+      mockServicesListing as NodeVamcHealthServicesListing
     )
-
-    expect(result.introText).toBe('')
+    mockServicesQuery.mockReturnValue(
+      createMockServicesForGrouping() as NodeRegionalHealthCareServiceDes[]
+    )
   })
 
-  test('includes path, administration, and vamcEhrSystem fields', () => {
-    const result = formatter(mockDataWrapper)
-    expect(result.path).toBeDefined()
-    expect(result.administration).toBeDefined()
-    expect(result.vamcEhrSystem).toBeDefined()
+  test('outputs formatted data', async () => {
+    expect(await runQuery()).toMatchSnapshot()
   })
 
-  test('correctly formats menu data', () => {
-    const result = formatter(mockDataWrapper)
-    expect(result.menu).toBeDefined()
-    expect(result.menu).toHaveProperty('rootPath')
-    expect(result.menu).toHaveProperty('data')
-  })
-
-  test('groups health services by type of care', () => {
-    const result = formatter(mockDataWrapper)
+  test('groups health services by type of care', async () => {
+    const result = await runQuery()
 
     expect(result.healthServiceGroups).toBeDefined()
     expect(result.healthServiceGroups).toHaveLength(5) // 5 different types of care
