e2b7d4bd0d743d34c9048dca0a7bc7ef822f8196
Chris Kim <chriskim@Chriss-MBP-2.attlocal.net>
2025-11-21 14:04:47 -0800

    merge

--- src/components/benefitsHub/template.tsx
+++ src/components/benefitsHub/template.tsx
@@ -16,127 +16,34 @@ export function BenefitsHub({
   const iconConfig = getHubIcon(titleIcon)
 
   return (
-    <div className="usa-grid usa-grid-full">
-      <article className="usa-width-two-thirds">
-        {iconConfig ? (
-          <div className="tablet:vads-u-display--flex vads-u-margin-y--1 vads-u-align-items--flex-start">
-            <span className="vads-u-margin-top--1">
-              <va-icon
-                icon={iconConfig.icon}
-                size="3"
-                class={iconConfig.className}
-              />
-            </span>
-            <h1 className="vads-u-margin-top--1 tablet:vads-u-margin-left--1 tablet:vads-u-margin-y--0">
-              {title}
-            </h1>
-          </div>
-        ) : (
-          <h1>{title}</h1>
-        )}
-        {intro && (
-          <p
-            className="va-introtext"
-            dangerouslySetInnerHTML={{ __html: intro }}
-          />
-        )}
-        {spokes?.map((spokeSection) => (
-          <div key={spokeSection.id}>
-            <section className="usa-grid">
-              <div className="va-h-ruled--stars"></div>
-            </section>
-            <ListOfLinkTeasers {...spokeSection} isHubPage={true} />
-          </div>
-        ))}
-        <ContentFooter lastUpdated={lastUpdated} />
-      </article>
-      <div className="usa-width-one-third" id="hub-rail">
-        <va-accordion bordered uswds>
-          <va-accordion-item
-            className="va-accordion-item"
-            level="2"
-            open="true"
-            header="Ask questions"
-            id="ask-questions"
-            bordered
-            uswds
-          >
-            <section>
-              <h3 className="vads-u-font-size--h4">Message us</h3>
-              <ul className="va-nav-linkslist-list social">
-                <li data-widget-type="ask-va-widget"></li>
-              </ul>
-            </section>
-
-            {supportServices && supportServices.length > 0 && (
-              <section>
-                <h3 className="vads-u-font-size--h4">Call us</h3>
-                <ul className="va-nav-linkslist-list social">
-                  {supportServices.map((service, index) => {
-                    const handleClick = () => {
-                      recordEvent({
-                        event: 'nav-hub-rail',
-                        'nav-path': 'Ask questions',
-                      })
-                    }
-
-                    const renderServiceContent = () => {
-                      if (service?.number) {
-                        return (
-                          <a href={service.link?.url} onClick={handleClick}>
-                            <span>{service.title}</span>
-                            <br />
-                            <span>{service.number}</span>
-                          </a>
-                        )
-                      }
-
-                      // TTY special case
-                      // It was requested that we hardcode in the aria-label and href for the TTY service
-                      // see: https://github.com/department-of-veterans-affairs/va.gov-team/issues/18151#issuecomment-879993959
-                      if (service?.title?.includes('TTY: 711')) {
-                        return (
-                          <a
-                            aria-label="TTY: 7 1 1."
-                            href="tel:711"
-                            onClick={handleClick}
-                          >
-                            <span>{service.title}</span>
-                            <br />
-                          </a>
-                        )
-                      }
-
-                      if (service?.link?.url) {
-                        return (
-                          <a href={service.link.url} onClick={handleClick}>
-                            <span>{service.title}</span>
-                          </a>
-                        )
-                      }
-
-                      return service?.title
-                    }
-
-                    return (
-                      <li key={service.id || index}>
-                        {renderServiceContent()}
-                      </li>
-                    )
-                  })}
-                </ul>
-              </section>
-            )}
-          </va-accordion-item>
-          {fieldLinks && fieldLinks.length > 0 && (
-            <va-accordion-item
-              className="va-accordion-item"
-              level="2"
-              open="true"
-              header="Not a Veteran or family member?"
-              id="get-information-for"
-              bordered
-            >
+    <div className="vads-grid-container">
+      <div className="vads-grid-row">
+        <article className="vads-grid-col-12 vads-u-margin-bottom--3 tablet:vads-grid-col-8 tablet:vads-u-padding-right--4">
+          {iconConfig ? (
+            <div className="tablet:vads-u-display--flex vads-u-margin-y--1 vads-u-align-items--flex-start">
+              <span className="vads-u-margin-top--1">
+                <va-icon
+                  icon={iconConfig.icon}
+                  size="3"
+                  class={iconConfig.className}
+                />
+              </span>
+              <h1 className="vads-u-margin-top--1 tablet:vads-u-margin-left--1 tablet:vads-u-margin-y--0">
+                {title}
+              </h1>
+            </div>
+          ) : (
+            <h1>{title}</h1>
+          )}
+          {intro && (
+            <p
+              className="va-introtext"
+              dangerouslySetInnerHTML={{ __html: intro }}
+            />
+          )}
+          {spokes && spokes.length > 0 && <va-on-this-page></va-on-this-page>}
+          {spokes?.map((spokeSection) => (
+            <div key={spokeSection.id}>
               <section>
                 <div className="va-h-ruled--stars"></div>
               </section>
@@ -147,6 +54,82 @@ export function BenefitsHub({
         </article>
         <div className="vads-grid-col-12 tablet:vads-grid-col-4" id="hub-rail">
           <va-accordion bordered uswds>
+            <va-accordion-item
+              className="va-accordion-item"
+              level="2"
+              open="true"
+              header="Ask questions"
+              id="ask-questions"
+              bordered
+              uswds
+            >
+              <section>
+                <h3 className="vads-u-font-size--h4">Message us</h3>
+                <ul className="va-nav-linkslist-list social">
+                  <li data-widget-type="ask-va-widget"></li>
+                </ul>
+              </section>
+
+              {supportServices && supportServices.length > 0 && (
+                <section>
+                  <h3 className="vads-u-font-size--h4">Call us</h3>
+                  <ul className="va-nav-linkslist-list social">
+                    {supportServices.map((service, index) => {
+                      const handleClick = () => {
+                        recordEvent({
+                          event: 'nav-hub-rail',
+                          'nav-path': 'Ask questions',
+                        })
+                      }
+
+                      const renderServiceContent = () => {
+                        if (service?.number) {
+                          return (
+                            <a href={service.link?.url} onClick={handleClick}>
+                              <span>{service.title}</span>
+                              <br />
+                              <span>{service.number}</span>
+                            </a>
+                          )
+                        }
+
+                        // TTY special case
+                        // It was requested that we hardcode in the aria-label and href for the TTY service
+                        // see: https://github.com/department-of-veterans-affairs/va.gov-team/issues/18151#issuecomment-879993959
+                        if (service?.title?.includes('TTY: 711')) {
+                          return (
+                            <a
+                              aria-label="TTY: 7 1 1."
+                              href="tel:711"
+                              onClick={handleClick}
+                            >
+                              <span>{service.title}</span>
+                              <br />
+                            </a>
+                          )
+                        }
+
+                        if (service?.link?.url) {
+                          return (
+                            <a href={service.link.url} onClick={handleClick}>
+                              <span>{service.title}</span>
+                            </a>
+                          )
+                        }
+
+                        return service?.title
+                      }
+
+                      return (
+                        <li key={service.id || index}>
+                          {renderServiceContent()}
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </section>
+              )}
+            </va-accordion-item>
             {fieldLinks && fieldLinks.length > 0 && (
               <va-accordion-item
                 class="va-accordion-item"
