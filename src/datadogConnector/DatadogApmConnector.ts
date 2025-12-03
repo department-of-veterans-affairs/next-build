3bc251e258edf9e31a3a6febb17bb4403f1cfe1e
Jeff Keene <jkeene@metrostar.com>
2025-12-03 13:37:37 -0500

    added DD Trace settings with env vars

--- src/datadogConnector/DatadogApmConnector.ts
+++ src/datadogConnector/DatadogApmConnector.ts
@@ -1,5 +1,15 @@
 import tracer from 'dd-trace'
 
-tracer.init()
+tracer.init({
+  logInjection: process.env.DD_LOGS_INJECTION === 'true',
+  runtimeMetrics: process.env.DD_RUNTIME_METRICS === 'true',
+  profiling: process.env.DD_PROFILING === 'true',
+  env: process.env.APP_ENV || 'development',
+  service: process.env.DD_SERVICE || 'next-build',
+  version: process.env.GIT_HASH || 'unknown',
+  sampleRate: process.env.DD_SAMPLE_RATE
+    ? parseFloat(process.env.DD_SAMPLE_RATE)
+    : 0.1,
+})
 
 export default tracer
