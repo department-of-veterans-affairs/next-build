9b87e4a855e0b1f3446fb295ef711e91c2bd49e8
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-12-04 20:35:01 -0800

    Can reduce the indentation without this return keyword

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,64 +16,62 @@ const HomePage = ({
   commonTasksData,
   newsSpotlightData,
   benefitsData,
-}) => {
-  return (
-    <>
-      <Head>
-        <title>VA.gov | Veterans Affairs</title>
-      </Head>
-      <PageLayout
-        bannerData={bannerData}
-        footerData={footerData}
-        megaMenuData={megaMenuData}
-      >
-        <main data-template="layouts/home" id="content">
-          <HomePageHero {...heroData} />
-          <HomePageCommonTasks {...commonTasksData} />
-          <HomePageNewsSpotlight {...newsSpotlightData} />
-          <HomePageBenefits {...benefitsData} />
-          <div className="usa-grid usa-grid-full">
-            <ContentFooter />
-          </div>
-          <div className="homepage-email-update-wrapper vads-u-background-color--primary-alt-lightest vads-u-padding-x--2p5 vads-u-padding-top--2p5">
-            <div data-widget-type="homepage-email-signup"></div>
-            <div
-              id="vets-banner-1"
-              className="vads-u-display--none medium-screen:vads-u-display--block"
-            >
-              <div className="veteran-banner-container vads-u-margin-y--0 vads-u-margin-x--auto">
-                <picture>
-                  <source
-                    srcSet="https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-mobile-1.png 640w, https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-mobile-2.png 920w, https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-mobile-3.png 1316w"
-                    media="(max-width: 767px)"
-                  />
-                  <source
-                    srcSet="https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-tablet-1.png 1008w, https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-tablet-2.png 1887w"
-                    media="(max-width: 1008px)"
-                  />
-                  <img
-                    className="vads-u-width--full"
-                    src="https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-desktop-1.png"
-                    srcSet="https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-desktop-1.png 1280w, https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-desktop-2.png 2494w"
-                    loading="lazy"
-                    alt="Veteran portraits"
-                  />
-                </picture>
-              </div>
+}) => (
+  <>
+    <Head>
+      <title>VA.gov | Veterans Affairs</title>
+    </Head>
+    <PageLayout
+      bannerData={bannerData}
+      footerData={footerData}
+      megaMenuData={megaMenuData}
+    >
+      <main data-template="layouts/home" id="content">
+        <HomePageHero {...heroData} />
+        <HomePageCommonTasks {...commonTasksData} />
+        <HomePageNewsSpotlight {...newsSpotlightData} />
+        <HomePageBenefits {...benefitsData} />
+        <div className="usa-grid usa-grid-full">
+          <ContentFooter />
+        </div>
+        <div className="homepage-email-update-wrapper vads-u-background-color--primary-alt-lightest vads-u-padding-x--2p5 vads-u-padding-top--2p5">
+          <div data-widget-type="homepage-email-signup"></div>
+          <div
+            id="vets-banner-1"
+            className="vads-u-display--none medium-screen:vads-u-display--block"
+          >
+            <div className="veteran-banner-container vads-u-margin-y--0 vads-u-margin-x--auto">
+              <picture>
+                <source
+                  srcSet="https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-mobile-1.png 640w, https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-mobile-2.png 920w, https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-mobile-3.png 1316w"
+                  media="(max-width: 767px)"
+                />
+                <source
+                  srcSet="https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-tablet-1.png 1008w, https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-tablet-2.png 1887w"
+                  media="(max-width: 1008px)"
+                />
+                <img
+                  className="vads-u-width--full"
+                  src="https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-desktop-1.png"
+                  srcSet="https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-desktop-1.png 1280w, https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/homepage/veterans-banner-desktop-2.png 2494w"
+                  loading="lazy"
+                  alt="Veteran portraits"
+                />
+              </picture>
             </div>
           </div>
-        </main>
+        </div>
+      </main>
 
-        {/* Loads widgets built from vets-website after data has been added to window */}
-        <Script
-          id="staticPages"
-          strategy="afterInteractive"
-          src={`${process.env.NEXT_PUBLIC_ASSETS_URL}static-pages.entry.js`}
-        />
-      </PageLayout>
-    </>
-  )
-}
+      {/* Loads widgets built from vets-website after data has been added to window */}
+      <Script
+        id="staticPages"
+        strategy="afterInteractive"
+        src={`${process.env.NEXT_PUBLIC_ASSETS_URL}static-pages.entry.js`}
+      />
+    </PageLayout>
+  </>
+)
 
 export async function getStaticProps() {
   if (
