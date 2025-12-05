05b2ae602f6146f98165c7807c6c92cfb0569f7f
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-12-05 12:22:36 -0800

    News spotlight section: Can achieve the full-width color split with custom css
    We're not using the old classes anymore because they don't match our new breakpoints.
    
    This also seems like a more straightforward solution to me than using a background gradient...though now that I think about it, I end up throwing out the custom styles on the smaller screens anyway and apply the background color directly to the content elements, which is the part that I wanted to be more straightforward anyway. I might follow up with another commit to further simplify this

--- src/components/homePageCommonTasks/template.tsx
+++ src/components/homePageCommonTasks/template.tsx
@@ -6,15 +6,22 @@ export function HomePageCommonTasks({
 }: CommonTasksData) {
   return (
     <div
-      className="homepage-common-tasks__wrapper"
+      className="homepage-common-tasks"
       data-e2e="common"
       data-testid="common-tasks"
     >
-      <div className="vads-l-grid-container vads-u-padding-x--0 homepage-common-tasks">
-        <div className="vads-l-row">
+      {/* Full-width background sections */}
+      <div className="homepage-common-tasks__backgrounds">
+        <div className="homepage-common-tasks__background-left vads-u-background-color--white"></div>
+        <div className="homepage-common-tasks__background-right vads-u-background-color--primary-alt-lightest"></div>
+      </div>
+
+      {/* Content container */}
+      <div className="vads-grid-container vads-u-padding-x--0 homepage-common-tasks">
+        <div className="vads-grid-row">
           {/* start first column */}
-          <div className="vads-l-col--12 medium-screen:vads-l-col--6 vads-u-background-color--white">
-            <div className="vads-u-margin-x--2 medium-desktop-screen:vads-u-margin-x--0 desktop:vads-u-padding-right--9 vads-u-padding-bottom--5">
+          <div className="vads-grid-col-12 tablet:vads-grid-col-6 homepage-common-tasks__column-left">
+            <div className="vads-u-margin-x--2 desktop:vads-u-margin-x--0 desktop:vads-u-padding-right--9 vads-u-padding-bottom--5">
               <h2
                 className="vads-u-color--gray-dark vads-u-font-family--serif"
                 id="search-tools-header"
@@ -46,8 +53,8 @@ export function HomePageCommonTasks({
           {/* end first column */}
 
           {/* start second column */}
-          <div className="vads-l-col--12 medium-screen:vads-l-col--6">
-            <div className="vads-u-padding-left--2p5 medium-screen:vads-u-padding-right--2p5 vads-u-padding-bottom--5">
+          <div className="vads-grid-col-12 tablet:vads-grid-col-6 homepage-common-tasks__column-right">
+            <div className="vads-u-padding-left--2p5 tablet:vads-u-padding-right--2p5 vads-u-padding-bottom--5">
               <h2 className="vads-u-color--gray-dark vads-u-font-family--serif">
                 Top pages
               </h2>
@@ -66,6 +73,54 @@ export function HomePageCommonTasks({
           {/* end second column */}
         </div>
       </div>
+
+      <style>{`
+        .homepage-common-tasks {
+          position: relative;
+        }
+
+        .homepage-common-tasks__backgrounds {
+          position: absolute;
+          top: 0;
+          left: 50%;
+          transform: translateX(-50%);
+          width: 100vw;
+          height: 100%;
+          display: flex;
+          z-index: 0;
+        }
+
+        .homepage-common-tasks__background-left,
+        .homepage-common-tasks__background-right {
+          flex: 1 1 50%;
+        }
+
+        @media (max-width: 40em) {
+          .homepage-common-tasks__backgrounds {
+            display: none;
+          }
+
+          .homepage-common-tasks__column-left {
+            background-color: white;
+          }
+
+          .homepage-common-tasks__column-right {
+            background-color: var(--vads-color-primary-alt-lightest);
+          }
+        }
+
+        @media (min-width: 40.0625em) {
+          .homepage-common-tasks__column-left,
+          .homepage-common-tasks__column-right {
+            background-color: transparent;
+          }
+        }
+
+        .homepage-common-tasks {
+          position: relative;
+          z-index: 1;
+        }
+      `}</style>
     </div>
   )
 }
