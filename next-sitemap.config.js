b5cbacb3b57363dcad38479ca430969db631044c
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-12-09 11:46:45 -0800

    Update comments

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -20,9 +20,11 @@ const sitemapConfig = {
   transform: async (config, path) => {
     // Transform found paths to add lastmod date https://github.com/iamvishnusankar/next-sitemap?tab=readme-ov-file#custom-transformation-function
     const fs = await import('fs/promises')
-    const pathToFile = `./out${path}/index.html` // Construct the path to the static file in the `out` directory
+    const pathToFile = `./out${path}/index.html`
+    // A possible future optimization could be to read the file as a stream to avoid
+    // loading the entire file into memory, especially since this meta tag is usually
+    // near the beginning of the file.
     const html = await fs.readFile(pathToFile, 'utf-8')
-    // Extract date from meta tag using regex: <meta name="DC.Date" content="YYYY-MM-DD">
     const dateMatch = html.match(RE_META_DATE)
     const date = dateMatch ? dateMatch[1] : null
     return {
