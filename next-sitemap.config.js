5e434c9cee9ac6128a0a77f9db5670e02a9e79cc
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-12-09 11:05:43 -0800

    Don't parse each HTML file with JSDOM just to get the date

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -15,16 +15,17 @@ const sitemapConfig = {
   generateRobotsTxt: false, // (optional)
   transform: async (config, path) => {
     // Transform found paths to add lastmod date https://github.com/iamvishnusankar/next-sitemap?tab=readme-ov-file#custom-transformation-function
-    const { JSDOM } = await import('jsdom') // Import JSDOM to parse HTML files
+    const fs = await import('fs/promises')
     const pathToFile = `./out${path}/index.html` // Construct the path to the static file in the `out` directory
-    const date = JSDOM.fromFile(pathToFile).then((dom) => {
-      return dom.window.document
-        .querySelector("meta[name='DC.Date']")
-        .getAttribute('content')
-    }) // get the date from the DC.Date meta tag in the HTML of the static file for each path
+    const html = await fs.readFile(pathToFile, 'utf-8')
+    // Extract date from meta tag using regex: <meta name="DC.Date" content="YYYY-MM-DD">
+    const dateMatch = html.match(
+      /<meta\s+name\s*=\s*["']DC\.Date["'][^>]*content\s*=\s*["'](\d{4}-\d{2}-\d{2})["']/i
+    )
+    const date = dateMatch ? dateMatch[1] : null
     return {
       loc: path, // Required to be returned in sitemap => this will be exported as http(s)://<config.siteUrl>/<path>
-      lastmod: await date, // The parsed date from the HTML file
+      lastmod: date, // The parsed date from the HTML file
     }
   },
   // todo: migrate to server side sitemap to include last edited date from content for lastmod
