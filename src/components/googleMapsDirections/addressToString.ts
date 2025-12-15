b6429cbb44de422986e068401c4cfb13fd43c727
Christian Burk <omahane@gmail.com>
2025-12-15 10:33:40 -0600

    VACMS-22976: Adds test change and query change to handle null addresses

--- src/components/googleMapsDirections/addressToString.ts
+++ src/components/googleMapsDirections/addressToString.ts
@@ -1,6 +1,9 @@
 import { FieldAddress } from '@/types/drupal/field_type'
 
-export const addressToString = (address: FieldAddress) =>
-  [address.address_line1, address.locality, address.administrative_area]
+export const addressToString = (address: FieldAddress | null | undefined) => {
+  if (!address) return ''
+
+  return [address.address_line1, address.locality, address.administrative_area]
     .filter(Boolean)
     .join(', ')
+}
