0abc01ee0cba0765f7922032804cc7ce63826fe8
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-12-19 12:55:30 -0800

    These classes aren't even used

--- src/components/outreachMaterials/AssetCard.tsx
+++ src/components/outreachMaterials/AssetCard.tsx
@@ -63,15 +63,7 @@ export function AssetCard({ asset, index, topics }: AssetCardProps) {
       className="vads-grid-col-12 tablet:vads-grid-col-6 vads-u-margin-bottom--3 vads-u-display--flex vads-u-align-items--stretch"
     >
       <div className="vads-u-padding--3 vads-u-width--full vads-u-background-color--gray-lightest">
-        <div
-          className={`mobile-lg:vads-grid-col-4 tablet:vads-grid-col-12 ${
-            asset.media.type === MediaResourceType.Document
-              ? 'document-asset-wrap'
-              : asset.media.type === MediaResourceType.Image
-                ? 'image-asset-wrap'
-                : 'video-asset-wrap'
-          }`}
-        >
+        <div className="mobile-lg:vads-grid-col-4 tablet:vads-grid-col-12">
           {renderMediaImage()}
         </div>
 
