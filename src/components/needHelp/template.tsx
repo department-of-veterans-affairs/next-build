deff3af54bae2efbba8a0b1aae425ca1cf75df1c
Chris Kim <chriskim@Chriss-MacBook-Pro-2.local>
2026-01-07 11:30:35 -0800

    updates to add need help to outreach materials

---
+++ src/components/needHelp/template.tsx
@@ -0,0 +1,22 @@
+import {
+  VaNeedHelp,
+  VaTelephone,
+} from '@department-of-veterans-affairs/component-library/dist/react-bindings'
+
+export const NeedHelp = () => {
+  return (
+    <div className="row need-help-padding">
+      <div className="usa-width-one-whole columns">
+        <VaNeedHelp>
+          <div slot="content">
+            <p>
+              Call us at <VaTelephone contact="8008271000" />. We’re here Monday
+              through Friday, 8:00 a.m to 9:00 p.m ET. If you have hearing loss,
+              call <VaTelephone contact="711" tty="true" />.
+            </p>
+          </div>
+        </VaNeedHelp>
+      </div>
+    </div>
+  )
+}
