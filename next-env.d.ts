4c53eb3c06b57cc9a5a0ac605824012671fe7dc2
Patrick Wolfert <patrickrwolfert@gmail.com>
2026-01-20 15:59:32 -0800

    Reference both docs

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,4 +4,5 @@
 /// <reference path="./.next/types/routes.d.ts" />
 
 // NOTE: This file should not be edited
+// see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
