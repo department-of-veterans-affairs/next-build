152f2fc3357a60419f44ab55f75cfb6487ba6881
Patrick Wolfert <patrickrwolfert@gmail.com>
2026-02-17 12:58:51 -0800

    Revert "Temporarily revert the refactor so we can see the old json"
    This reverts commit 2d37c6948cb276e2aa5c0ab5c0facea3171dac6f.

--- src/components/vetCenterOutstation/template.tsx
+++ src/components/vetCenterOutstation/template.tsx
@@ -8,6 +8,7 @@ import { AccordionItem } from '@/components/accordion/formatted-type'
 import { PhoneNumber } from '@/components/phoneNumber/template'
 import { TextWithImage } from '@/components/textWithImage/template'
 import { MediaImage } from '@/components/mediaImage/template'
+import { SchemaScript } from './SchemaScript'
 
 const PrepareForVisitComponent = ({
   visitItems,
@@ -31,120 +32,25 @@ const PrepareForVisitComponent = ({
   )
 }
 
-export function VetCenterOutstation({
-  address,
-  ccNonTraditionalHours,
-  ccVetCenterCallCenter,
-  ccVetCenterFaqs,
-  geolocation,
-  featuredContent,
-  introText,
-  officeHours,
-  officialName,
-  phoneNumber,
-  healthServices,
-  image,
-  prepareForVisit,
-  title,
-  fieldFacilityLocatorApiId,
-  path,
-}: FormattedVetCenterOutstation) {
-  const structuredSchemaData = {
-    '@context': 'https://schema.org',
-    '@type': 'Place',
-    address: {
-      '@type': 'PostalAddress',
-      streetAddress: `${address.address_line1}${
-        address.address_line2 ? `, ${address.address_line2}` : ''
-      }`,
-      addressLocality: address.locality,
-      addressRegion: address.administrative_area,
-      postalCode: address.postal_code,
-    },
-    name: title,
-    telephone: phoneNumber,
-    openingHoursSpecification: officeHours.map((hours) => ({
-      '@type': 'OpeningHoursSpecification',
-      dayOfWeek: `https://schema.org/${hours.day}`,
-      opens: hours.starthours,
-      closes: hours.endhours,
-    })),
-    hasMap: `https://maps.google.com?saddr=Current+Location&daddr=${encodeURIComponent(
-      `${address.address_line1}, ${address.locality}, ${address.postal_code}`
-    )}`,
-    image: image?.links?.['2_1_large']?.href,
-    branchCode: fieldFacilityLocatorApiId,
-    geo: {
-      '@type': 'GeoCoordinates',
-      latitude: geolocation.lat.toString(),
-      longitude: geolocation.lon.toString(),
-    },
-  }
-
-  const generateStructuredDataForHealthServices = (
-    healthServices,
+export function VetCenterOutstation(
+  vetCenterOutstationProps: FormattedVetCenterOutstation
+) {
+  const {
+    address,
+    ccNonTraditionalHours,
+    ccVetCenterCallCenter,
+    ccVetCenterFaqs,
+    featuredContent,
+    introText,
+    officeHours,
+    officialName,
+    phoneNumber,
+    image,
+    prepareForVisit,
     title,
-    fieldAddress
-  ) => {
-    return healthServices.map((service) => ({
-      '@context': 'https://schema.org',
-      '@type': 'GovernmentService',
-      name: title,
-      alternateName: service?.fieldVetCenterFriendlyName || null,
-      serviceType: service?.vetCenterTypeOfCare || null,
-      serviceOperator: {
-        '@type': 'GovernmentOrganization',
-        name: 'US Department of Veterans Affairs',
-      },
-      areaServed: {
-        '@type': 'AdministrativeArea',
-        name: fieldAddress?.administrative_area || null,
-      },
-      audience: {
-        '@type': 'Audience',
-        audienceType: 'Veteran',
-      },
-      availableChannel: {
-        '@type': 'ServiceChannel',
-        serviceUrl: 'https://www.va.gov',
-        servicePhone: {
-          '@type': 'ContactPoint',
-          telephone: service?.phoneNumber || null,
-        },
-      },
-      provider: {
-        '@type': 'GovernmentOrganization',
-        name: 'Veterans Affairs',
-        url: 'https://www.va.gov',
-      },
-      serviceLocation: {
-        '@type': 'Place',
-        name: title,
-        address: {
-          '@type': 'PostalAddress',
-          streetAddress:
-            fieldAddress.address_line1 +
-            (address.address_line2
-              ? `${address.address_line1}${address.address_line2}`
-              : address.address_line1),
-          addressLocality: fieldAddress?.locality,
-          addressRegion: fieldAddress?.administrative_area,
-          postalCode: fieldAddress?.postal_code,
-        },
-        geo: {
-          '@type': 'GeoCoordinates',
-          latitude: geolocation.lat.toString(),
-          longitude: geolocation.lon.toString(),
-        },
-      },
-    }))
-  }
-
-  const structuredDataHealthServices = generateStructuredDataForHealthServices(
-    healthServices,
-    title,
-    address
-  )
+    fieldFacilityLocatorApiId,
+    path,
+  } = vetCenterOutstationProps
 
   return (
     <div className="usa-grid usa-grid-full">
@@ -274,20 +180,7 @@ export function VetCenterOutstation({
 
           <va-back-to-top></va-back-to-top>
 
-          {/* Embedding structured data scripts for schema.org */}
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{
-              __html: JSON.stringify(structuredSchemaData),
-            }}
-          />
-          {structuredDataHealthServices.map((service, index) => (
-            <script
-              key={index}
-              type="application/ld+json"
-              dangerouslySetInnerHTML={{ __html: JSON.stringify(service) }}
-            />
-          ))}
+          <SchemaScript vetCenterOutstation={vetCenterOutstationProps} />
         </article>
       </div>
     </div>
