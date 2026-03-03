5a9e08893fb61382aee02b0a0d0089580d49c366
Patrick Wolfert <patrickrwolfert@gmail.com>
2026-03-03 11:59:46 -0800

    Fix assumption about sample rate being 0 to 1 when docs actually say it's 0 to 100

--- src/datadogConnector/DatadogApmConnector.ts
+++ src/datadogConnector/DatadogApmConnector.ts
@@ -1,5 +1,13 @@
 import tracer from 'dd-trace'
 
+function parseSampleRate(rateString: string | undefined): number | undefined {
+  const rate = parseInt(rateString)
+  if (isNaN(rate)) {
+    return undefined
+  }
+  return rate
+}
+
 tracer.init({
   logInjection: process.env.DD_LOGS_INJECTION === 'true',
   runtimeMetrics: process.env.DD_RUNTIME_METRICS === 'true',
@@ -7,12 +15,7 @@ tracer.init({
   env: process.env.APP_ENV || 'development',
   service: process.env.DD_SERVICE || 'next-build',
   version: process.env.GIT_HASH || 'unknown',
-  sampleRate: process.env.DD_SAMPLE_RATE
-    ? (() => {
-        const rate = parseFloat(process.env.DD_SAMPLE_RATE as string)
-        return !isNaN(rate) && rate >= 0 && rate <= 1 ? rate : 0.1
-      })()
-    : 0.1,
+  sampleRate: parseSampleRate(process.env.DD_SAMPLE_RATE) ?? 10,
 })
 
 export default tracer
